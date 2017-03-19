8640a1f21dc135c98e146ba2b1307ed0fcd3b3af
Joongi Kim <joongi@lablup.com>
2017-03-19 18:34:30 +0900

    Improve packaging and update README.

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+import pip
 
-here = path.abspath(path.dirname(__file__))
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ""
+
+
+requires = []
+links = []
+requirements = pip.req.parse_requirements(
+    'requirements.txt', session=pip.download.PipSession()
+)
+for item in requirements:
+    if getattr(item, 'url', None):  # older pip has url
+        links.append(str(item.url))
+    if getattr(item, 'link', None):  # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req))  # always the package name
+
 
 setup(
     name='sorna',
@@ -12,9 +28,9 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.7.0',
+    version='0.9.1',
     description='Sorna meta-package',
-    long_description='',
+    long_description=long_description,
     url='https://github.com/lablup/sorna',
     author='Lablup Inc.',
     author_email='joongi@lablup.com',
@@ -26,7 +42,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Operating System :: POSIX',
         'Operating System :: MacOS :: MacOS X',
         'Environment :: No Input/Output (Daemon)',
@@ -37,16 +53,12 @@ setup(
     packages=['sorna'],
     namespace_packages=['sorna'],
 
-    install_requires=['sorna-common', 'sorna-manager', 'sorna-agent'],
+    python_requires='>=3.6',
+    install_requires=requires,
+    dependency_links=links,
     extras_require={
         'dev': [],
         'test': [],
     },
-    package_data={
-    },
     data_files=[],
-
-    entry_points={
-        'console_scripts': ['sorna_agent=sorna.agent:main'],
-    },
 )
