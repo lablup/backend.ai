5a2f626c646f5100a9cf41f09cbc7c653a797831
Joongi Kim <joongi@lablup.com>
2018-01-30 11:44:01 +0900

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 setup(
     name='backend.ai',
-    version='1.1.0',
+    version='1.2.0',
     description='Lablup Backend.AI Meta-package',
     long_description=Path('README.rst').read_text(),
     url='https://github.com/lablup/backend.ai',
@@ -28,15 +28,15 @@ setup(
     packages=['ai.backend.meta'],
     python_requires='>=3.6',
     install_requires=[
-        'backend.ai-common~=1.1.0',
-        'backend.ai-client~=1.1.5',
+        'backend.ai-common~=1.1.2',
+        'backend.ai-client~=1.1.7',
     ],
     extras_require={
         'manager': [
-            'backend.ai-manager~=1.1.0',
+            'backend.ai-manager~=1.2.0',
         ],
         'agent': [
-            'backend.ai-agent~=1.1.0',
+            'backend.ai-agent~=1.2.0',
         ],
         'dev': [
             'pytest',
