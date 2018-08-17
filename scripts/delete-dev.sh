d1bd7c0c21dedc7976d02880a27372be1c2a91c1
Joongi Kim <joongi@lablup.com>
2018-08-17 17:04:19 +0900

    Fix up the delete script (#20)

--- scripts/delete-dev.sh
+++ scripts/delete-dev.sh
@@ -4,19 +4,19 @@ ENV_ID=$1
 INSTALL_PATH="$(pwd)/backend.ai-dev"
 REMOVE_VENVS="yes"
 REMOVE_CONTAINERS="yes"
-REMOVE_SOURCE="no"
+REMOVE_SOURCE="yes"
 
-if [ "$REMOVE_VENVS" -eq "yes" ]; then
+if [ "$REMOVE_VENVS" = "yes" ]; then
   echo "Removing Python virtual environments..."
-  pyenv uninstall "venv-${ENV_ID}-agent"
-  pyenv uninstall "venv-${ENV_ID}-client"
-  pyenv uninstall "venv-${ENV_ID}-common"
-  pyenv uninstall "venv-${ENV_ID}-manager"
+  pyenv uninstall -f "venv-${ENV_ID}-agent"
+  pyenv uninstall -f "venv-${ENV_ID}-client"
+  pyenv uninstall -f "venv-${ENV_ID}-common"
+  pyenv uninstall -f "venv-${ENV_ID}-manager"
 else
   echo "Skipped removal of Python virtual environments."
 fi
 
-if [ "$REMOVE_CONTAINERS" -eq "yes" ]; then
+if [ "$REMOVE_CONTAINERS" = "yes" ]; then
   echo "Removing Docker containers..."
   cd "${INSTALL_PATH}/backend.ai"
   docker-compose -p "${ENV_ID}" -f docker-compose.halfstack.yml down
@@ -24,7 +24,7 @@ else
   echo "Skipped removal of Docker containers."
 fi
 
-if [ "$REMOVE_SOURCE" -eq "yes" ]; then
+if [ "$REMOVE_SOURCE" = "yes" ]; then
   echo "Removing cloned source files..."
   rm -rf "${INSTALL_PATH}"
 else
