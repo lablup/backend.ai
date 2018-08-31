90a166a9b757c4506fe298f009ff7297b1183b62
Jeongkyu Shin <inureyes@gmail.com>
2018-08-31 09:00:53 +0900

    refs #21 : modify - remove installation chapter for production page

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,8 @@ templates_path = ['_templates']
 source_parsers = {
     '.md': CommonMarkParser,
 }
-source_suffix = ['.rst', '.md']
+#source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
