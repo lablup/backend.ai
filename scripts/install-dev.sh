401b7375ba86fdbe03dc5bc95c94d9f1bc42e9ae
Joongi Kim <joongi@lablup.com>
2018-12-21 17:34:56 +0900

    Update the shell command for example-keypairs fixture population

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -377,7 +377,8 @@ show_info "Setting up databases..."
 cd "${INSTALL_PATH}/manager"
 cp alembic.ini.sample alembic.ini
 python -m ai.backend.manager.cli schema oneshot head
-python -m ai.backend.manager.cli --db-addr=localhost:8100 --db-user=postgres --db-password=develove --db-name=backend fixture populate example_keypair
+python -m ai.backend.manager.cli --db-addr=localhost:8100 --db-user=postgres --db-password=develove --db-name=backend \
+       fixture populate sample-configs/example-keypairs.json
 
 show_info "Installing Python client SDK/CLI source..."
 cd "${INSTALL_PATH}"
