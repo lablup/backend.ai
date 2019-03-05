629c52cb7256e9095cb7bea5ce3eaa23a58a9e33
Mario Cho <hephaex@gmail.com>
2019-03-05 13:23:57 +0900

    add shell script to update pip

--- scripts/update-to-latest-repo.sh
+++ scripts/update-to-latest-repo.sh
@@ -3,8 +3,13 @@ INSTALL_PATH="./backend.ai-dev"
 cd ${INSTALL_PATH}
 for d in agent backend.ai client-py common manager
 do
-  cd ${d}
-  git fetch
-  git pull
-  cd ..
+    cd ${d}
+    pip install --upgrade pip
+    git fetch
+    git pull
+    pip install -U -r ./requirements-dev.txt
+    pip install -e . -e ../common
+    cd ..
 done
+cd manager
+pip install ../common
