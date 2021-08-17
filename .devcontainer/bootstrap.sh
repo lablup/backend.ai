55c3676e2177956750408a060aa69daef2c2f0e7
Kyujin Cho <thy2134@me.com>
2021-08-17 14:23:26 +0000

    Fix updated GITHUB_TOKEN env var not persisting

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 export GITHUB_TOKEN=$MY_GITHUB_TOKEN
+for PROFILE_FILE in "zshrc" "bashrc" "profile" "bash_profile"
+do
+  if [ -e "${HOME}/.${PROFILE_FILE}" ]
+  then
+    echo "export GITHUB_TOKEN=$MY_GITHUB_TOKEN" >> "${HOME}/.${PROFILE_FILE}"
+  fi
+done
+
 git config --global credential.helper=/.codespaces/bin/gitcredential_github.sh
 
+export
+
+sed -i '/pyenv init --path/ a  eval "$(pyenv init -)"' /workspaces/backend.ai/scripts/install-dev.sh
 sed -i 's/^git clone.*backend\.ai$//g' /workspaces/backend.ai/scripts/install-dev.sh
-bash /workspaces/backend.ai/scripts/install-dev.sh --install-path /workspaces > install.log
+
+bash /workspaces/backend.ai/scripts/install-dev.sh --install-path /workspaces >install.log 2>&1 
