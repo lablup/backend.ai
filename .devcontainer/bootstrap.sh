5978f16afc6aefafe385b95d2fc48d0d10ce718a
Kyujin Cho <kyujin.cho@samkrion.com>
2021-08-18 11:23:41 +0900

    Skip bootstrap process when halfstack is already initialized

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+if [ -e "/workspaces/manager"] then 
+  exit 0
+fi
+
 GITHUB_TOKEN=${$CUSTOM_GITHUB_TOKEN:-$GITHUB_TOKEN}
 for PROFILE_FILE in "zshrc" "bashrc" "profile" "bash_profile"
 do
