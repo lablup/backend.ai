26deac0e8962a60cb542c0a50b0ba9b7eb0cb38f
DaeHyun Sung <dhsung@lablup.com>
2022-01-26 16:59:21 +0900

    Change python default version(3.9.10) description
    on install-dev.sh, help message describe that python default version is `3.9.5`.
    However, on the file, already set ` PYTHON_VERSION="3.9.9"`
    
    As a result, change python default version description
    `3.9.5` to `3.9.10`
    
    close https://github.com/lablup/backend.ai/issues/356

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -51,7 +51,7 @@ usage() {
   echo ""
   echo "  ${LWHITE}--python-version VERSION${NC}"
   echo "                       Set the Python version to install via pyenv"
-  echo "                       (default: 3.9.5)"
+  echo "                       (default: 3.9.10)"
   echo ""
   echo "  ${LWHITE}--install-path PATH${NC}  Set the target directory"
   echo "                       (default: ./backend.ai-dev)"
@@ -177,7 +177,7 @@ fi
 
 ROOT_PATH=$(pwd)
 ENV_ID=""
-PYTHON_VERSION="3.9.9"
+PYTHON_VERSION="3.9.10"
 SERVER_BRANCH="main"
 CLIENT_BRANCH="main"
 INSTALL_PATH="./backend.ai-dev"
