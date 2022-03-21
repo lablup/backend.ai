e9d646b251b6a6ac09573e3d26052557c8ef1c91
Joongi Kim <me@daybreaker.info>
2022-03-21 11:38:10 +0900

    installer: Fix up grpcio install issue on M1 and Python 3.10 (#391)
    * fix: Explicitly clear GRPC build flags in Python 3.10 and Apple Silicon
      and force build of grpcio wheel by ourselves
    * fix: Update delete-dev.sh
    * fix: Merge #388 to reorder image rescan and db init
    
    Co-authored-by: Sanghun Lee <sanghun@lablup.com>

--- scripts/delete-dev.sh
+++ scripts/delete-dev.sh
@@ -125,6 +125,7 @@ if [ $REMOVE_VENVS -eq 1 ]; then
   pyenv uninstall -f "venv-${ENV_ID}-webserver"
   pyenv uninstall -f "venv-${ENV_ID}-storage-proxy"
   pyenv uninstall -f "venv-${ENV_ID}-tester"
+  pyenv uninstall -f "tmp-grpcio-build"
 else
   echo "Skipped removal of Python virtual environments."
 fi
@@ -149,6 +150,8 @@ if [ $REMOVE_SOURCE -eq 1 ]; then
   $sudo rm -rf "${INSTALL_PATH}/backend.ai"
   $sudo rm -rf "${INSTALL_PATH}/vfolder"
   $sudo rm -rf "${INSTALL_PATH}/accel-cuda"
+  $sudo rm -rf "${INSTALL_PATH}/tester"
+  $sudo rm -rf "${INSTALL_PATH}/wheelhouse"
   echo "Please remove ${INSTALL_PATH} by yourself."
 else
   echo "Skipped removal of cloned source files."
