90938f353c2df2e29e2eed0f15604cf93e680056
Joongi Kim <joongi@lablup.com>
2022-05-03 13:59:38 +0000

    Implement entrypoint scan with BUILD files for CLI

--- src/ai/backend/cli/loader.py
+++ src/ai/backend/cli/loader.py
@@ -1,17 +1,13 @@
-import sys
-if sys.version_info >= (3, 8, 0):
-    from importlib.metadata import entry_points
-else:
-    from importlib_metadata import entry_points  # type: ignore
-
 import click            # noqa: E402
+
+from ai.backend.plugin.entrypoint import scan_entrypoints
 
 from .main import main  # noqa: E402
 
 
 def load_entry_points() -> click.Group:
     entry_prefix = 'backendai_cli_v10'
-    for entrypoint in entry_points().get(entry_prefix, []):
+    for entrypoint in scan_entrypoints(entry_prefix):
         if entrypoint.name == "_":
             cmd_group = entrypoint.load()
             return cmd_group
--- src/ai/backend/plugin/entrypoint.py
+++ src/ai/backend/plugin/entrypoint.py
@@ -1,9 +1,63 @@
+import ast
+import logging
+import pkg_resources
 from pathlib import Path
+from typing import Container, Iterator
 
 
-def scan_entrypoints():
-    pass
+log = logging.getLogger(__name__)
 
 
-def extract_entrypoint_from_buildscript(path: Path):
-    pass
+def scan_entrypoints(
+    group_name: str,
+    blocklist: Container[str] = None,
+) -> Iterator[pkg_resources.EntryPoint]:
+    if blocklist is None:
+        blocklist = set()
+    for entrypoint in scan_entrypoint_from_buildscript(group_name):
+        if entrypoint.name in blocklist:
+            continue
+        yield entrypoint
+    for entrypoint in scan_entrypoint_from_package_metadata(group_name):
+        if entrypoint.name in blocklist:
+            continue
+        yield entrypoint
+
+
+def scan_entrypoint_from_package_metadata(group_name: str) -> Iterator[pkg_resources.EntryPoint]:
+    for entrypoint in pkg_resources.iter_entry_points(group_name):
+        yield entrypoint
+
+
+def scan_entrypoint_from_buildscript(group_name: str) -> Iterator[pkg_resources.EntryPoint]:
+    ai_backend_ns_path = Path(__file__).parent.parent
+    print("Namespace path:", ai_backend_ns_path)
+    print("All BUILD files:\n", [*ai_backend_ns_path.glob("**/BUILD")])
+    for buildscript_path in ai_backend_ns_path.glob("**/BUILD"):
+        log.debug("reading entry points [{}] from {}", group_name, buildscript_path)
+        yield from extract_entrypoints_from_buildscript(group_name, buildscript_path)
+
+
+def extract_entrypoints_from_buildscript(
+    group_name: str,
+    buildscript_path: Path,
+) -> Iterator[pkg_resources.EntryPoint]:
+    tree = ast.parse(buildscript_path.read_bytes())
+    for node in tree.body:
+        if (
+            isinstance(node, ast.Expr) and
+            isinstance(node.value, ast.Call) and
+            isinstance(node.value.func, ast.Name) and
+            node.value.func.id == "python_distribution"
+        ):
+            for kwarg in node.value.keywords:
+                if kwarg.arg == "entry_points":
+                    raw_data = ast.literal_eval(kwarg.value)
+                    for key, raw_entry_points in raw_data.items():
+                        if key != group_name:
+                            continue
+                        for name, ref in raw_entry_points.items():
+                            try:
+                                yield pkg_resources.EntryPoint.parse(f"{name}={ref}")
+                            except ValueError:
+                                pass
