b11415c5bfe0aad307b1af6eec1601668e3ffafe
Joongi Kim <joongi@lablup.com>
2022-05-03 01:39:31 +0000

    Add a new `ai.backend.plugin` package

---
+++ src/ai/backend/plugin/entrypoint.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+def scan_entrypoints():
+    pass
+
+
+def extract_entrypoint_from_buildscript(path: Path):
+    pass
---
+++ tests/plugin/test_entrypoint.py
@@ -0,0 +1,5 @@
+from ai.backend.plugin.entrypoint import extract_entrypoint_from_buildscript
+
+
+def test_parse_build():
+    pass
