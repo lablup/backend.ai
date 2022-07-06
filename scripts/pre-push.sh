6f6295f6a54556603e18b4c3f86d8cf79743c7ef
Joongi Kim <joongi@lablup.com>
2022-07-06 14:23:26 +0000

    fix: detection of release branch
    - We need to quote the operand when using "-n" operator in bash

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,18 +1,18 @@
 # backend.ai monorepo standard pre-push hook
 BASE_PATH=$(cd "$(dirname "$0")"/../.. && pwd)
 if [ -f "$BASE_PATH/pants-local" ]; then
-    PANTS=$BASE_PATH/pants-local
+  PANTS="$BASE_PATH/pants-local"
 else
-    PANTS=$BASE_PATH/pants
+  PANTS="$BASE_PATH/pants"
 fi
 CURRENT_COMMIT=$(git rev-parse --short HEAD)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [ -n $(echo "$CURRENT_BRANCH" | sed -n '/^[0-9]\+\.[0-9]\+$/p') ]; then
-  # if we are on a release branch, use it as the base branch.
+if [ -n "$(echo "$CURRENT_BRANCH" | sed -n '/^[0-9]\+\.[0-9]\+$/p')" ]; then
+  # if we are on the release branch, use it as the base branch.
   BASE_BRANCH="$CURRENT_BRANCH"
 else
   BASE_BRANCH="main"
 fi
-echo "Performing formatting and lint checks on $1/$BASE_BRANCH..HEAD@$CURRENT_COMMIT"
+echo "Performing formatting and lint checks on $1/${BASE_BRANCH}..HEAD@${CURRENT_COMMIT}"
 "$PANTS" tailor --check update-build-files --check
-"$PANTS" fmt lint check --changed-since=$1/$BASE_BRANCH
+"$PANTS" fmt lint check --changed-since="$1/${BASE_BRANCH}..HEAD"
