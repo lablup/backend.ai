aafb5041f11f9e97bf13240a334ef25431e55d9a
Sanghun Lee <sanghun@lablup.com>
2022-07-14 02:45:05 +0000

    merge git and alembic branch

---
+++ src/ai/backend/manager/models/alembic/versions/c2d76ca9fd29_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c2d76ca9fd29
+Revises: 40f0912bff53, 35923972eddb
+Create Date: 2022-07-14 02:44:13.577084
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c2d76ca9fd29'
+down_revision = ('40f0912bff53', '35923972eddb')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
