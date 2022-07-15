96de010ddf423d978acaa1adfd12a78ea77e1d3b
DaeHyun Sung <dhsung@lablup.com>
2022-07-15 10:50:45 +0900

    rollback entrypoint.py
    rollback entrypoint.py from main branch

--- src/ai/backend/plugin/entrypoint.py
+++ src/ai/backend/plugin/entrypoint.py
@@ -120,26 +120,25 @@ def extract_entrypoints_from_buildscript(
     group_name: str,
     buildscript_path: Path,
 ) -> Iterator[EntryPoint]:
-    if buildscript_path.is_file():
-        tree = ast.parse(buildscript_path.read_bytes())
-        for node in tree.body:
-            if (
-                isinstance(node, ast.Expr) and
-                isinstance(node.value, ast.Call) and
-                isinstance(node.value.func, ast.Name) and
-                node.value.func.id == "python_distribution"
-            ):
-                for kwarg in node.value.keywords:
-                    if kwarg.arg == "entry_points":
-                        raw_data = ast.literal_eval(kwarg.value)
-                        for key, raw_entry_points in raw_data.items():
-                            if key != group_name:
-                                continue
-                            for name, ref in raw_entry_points.items():
-                                try:
-                                    yield EntryPoint(name=name, value=ref, group=group_name)
-                                except ValueError:
-                                    pass
+    tree = ast.parse(buildscript_path.read_bytes())
+    for node in tree.body:
+        if (
+            isinstance(node, ast.Expr) and
+            isinstance(node.value, ast.Call) and
+            isinstance(node.value.func, ast.Name) and
+            node.value.func.id == "python_distribution"
+        ):
+            for kwarg in node.value.keywords:
+                if kwarg.arg == "entry_points":
+                    raw_data = ast.literal_eval(kwarg.value)
+                    for key, raw_entry_points in raw_data.items():
+                        if key != group_name:
+                            continue
+                        for name, ref in raw_entry_points.items():
+                            try:
+                                yield EntryPoint(name=name, value=ref, group=group_name)
+                            except ValueError:
+                                pass
 
 
 def extract_entrypoints_from_setup_cfg(
