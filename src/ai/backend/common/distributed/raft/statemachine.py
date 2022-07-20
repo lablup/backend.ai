fc89ef16a157763e6b9ad0a590577b1dfefaacb4
rapsealk <jskang@lablup.com>
2022-07-20 16:12:03 +0900

    feat: Add redis-based state machine prototype

--- src/ai/backend/common/distributed/raft/statemachine.py
+++ src/ai/backend/common/distributed/raft/statemachine.py
@@ -1,25 +1,42 @@
 import abc
+from typing import Optional
+
+import redis
+from redis.asyncio import Redis
 
 from ...types import aobject
 
 
 class AbstractStateMachine(abc.ABC):
     @abc.abstractmethod
-    async def execute(self, command: str) -> None:
+    async def apply(self, command: str) -> None:
         raise NotImplementedError()
 
-
-class ExecStateMachine(AbstractStateMachine):
-    async def execute(self, command: str) -> None:
-        exec(command)
+    @abc.abstractmethod
+    async def get(self, key: str) -> Optional[bytes]:
+        raise NotImplementedError()
 
 
 class RedisStateMachine(aobject, AbstractStateMachine):
     def __init__(self, *args, **kwargs) -> None:
-        pass
+        self._redis = Redis.from_url("redis://127.0.0.1:8111")
 
-    async def __ainit__(self, *args, **kwargs) -> None:
-        pass
+    async def apply(self, command: str) -> None:
+        try:
+            await self._redis.execute_command(command)
+        except redis.exceptions.ResponseError:
+            pass
 
-    async def execute(self, command: str) -> None:
-        pass
+    async def get(self, key: str) -> Optional[bytes]:
+        return await self._redis.get(key)
+
+    """
+    async def get(self, key: str) -> Optional[Union[bytes, float, int, str]]:
+        if value := await self._redis.get(key):
+            for dtype in (int, float, str):
+                try:
+                    return dtype(value)
+                except ValueError:
+                    pass
+        return value
+    """
