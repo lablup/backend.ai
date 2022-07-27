c801fc647d260b7cbe8127f7135a01c2b6cad474
Joongi Kim <joongi@lablup.com>
2022-07-27 13:40:46 +0000

    fix: Allow arbitrary expr in the field names
    - This allows dynamic field generation, e.g., iterating over a dict's keys, etc.

--- src/ai/backend/web/template.py
+++ src/ai/backend/web/template.py
@@ -1,7 +1,7 @@
 import json
-from typing import Any, List
+from typing import Any, List, Optional
 
-from jinja2 import lexer, nodes
+from jinja2 import nodes
 from jinja2.ext import Extension
 from jinja2.parser import Parser
 
@@ -13,27 +13,36 @@ class TOMLField(Extension):
     def parse(self, parser: Parser) -> nodes.Node | List[nodes.Node]:
         tag_name = list(self.tags)[0]
         lineno = parser.stream.expect(f"name:{tag_name}").lineno
-        field_name: lexer.Token = parser.stream.expect(lexer.TOKEN_STRING)
+        field_name: nodes.Expr = parser.parse_expression()
         field_value: nodes.Expr = parser.parse_expression()
         return nodes.Output(
             [
                 nodes.CondExpr(
                     nodes.Test(field_value, "none", [], [], None, None),
                     # TOML does not have "null" syntax so let's comment out the field.
-                    nodes.TemplateData(f"# {field_name.value} ="),
                     nodes.Concat(
                         [
-                            nodes.TemplateData(f"{field_name.value} = "),
-                            self._transform(field_value),
-                        ]
+                            nodes.TemplateData("# "),
+                            field_name,
+                            nodes.TemplateData(" = "),
+                        ],
+                        lineno=lineno,
+                    ),
+                    nodes.Concat(
+                        [
+                            field_name,
+                            nodes.TemplateData(" = "),
+                            self._transform(field_value, lineno=lineno),
+                        ],
+                        lineno=lineno,
                     ),
                 ),
             ],
             lineno=lineno,
         )
 
-    def _transform(self, field_value: nodes.Expr) -> nodes.Expr:
-        field_value = nodes.Filter(field_value, "toml_scalar", [], [], None, None)
+    def _transform(self, field_value: nodes.Expr, lineno: Optional[int] = None) -> nodes.Expr:
+        field_value = nodes.Filter(field_value, "toml_scalar", [], [], None, None, lineno=lineno)
         return field_value
 
 
@@ -41,9 +50,11 @@ class TOMLStringListField(TOMLField):
 
     tags = {"toml_strlist_field"}
 
-    def _transform(self, field_value: nodes.Expr) -> nodes.Expr:
-        field_value = nodes.Filter(field_value, "join", [nodes.Const(",")], [], None, None)
-        field_value = nodes.Filter(field_value, "toml_scalar", [], [], None, None)
+    def _transform(self, field_value: nodes.Expr, lineno: Optional[int] = None) -> nodes.Expr:
+        field_value = nodes.Filter(
+            field_value, "join", [nodes.Const(",")], [], None, None, lineno=lineno
+        )
+        field_value = nodes.Filter(field_value, "toml_scalar", [], [], None, None, lineno=lineno)
         return field_value
 
 
