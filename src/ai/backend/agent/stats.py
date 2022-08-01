1188b0f4370ff0e773ea47099a81c27375bc827f
KangJuSeong <wntjd3686@gmail.com>
2022-08-01 13:05:38 +0900

    fix: rewrite variable name

--- src/ai/backend/agent/stats.py
+++ src/ai/backend/agent/stats.py
@@ -382,18 +382,18 @@ class StatContext:
         """
         async with self._lock:
             kernel_id_map: Dict[ContainerId, KernelId] = {}
-            stale_kernel_id_list: List[KernelId] = []
+            stale_kernel_ids = []
             for kid, info in self.agent.kernel_registry.items():
                 try:
                     cid = info["container_id"]
                 except KeyError:
-                    stale_kernel_id_list.append(KernelId(kid))
+                    stale_kernel_ids.append(kid)
                     log.warning("collect_container_stat(): no container for kernel {}", kid)
                 else:
                     kernel_id_map[ContainerId(cid)] = kid
-            if stale_kernel_id_list:
-                for kernel_id in stale_kernel_id_list:
-                    self.agent.kernel_registry.pop(kernel_id, None)
+            if stale_kernel_ids:
+                for kid in stale_kernel_ids:
+                    self.agent.kernel_registry.pop(kid, None)
             unused_kernel_ids = set(self.kernel_metrics.keys()) - set(kernel_id_map.values())
             for unused_kernel_id in unused_kernel_ids:
                 log.debug("removing kernel_metric for {}", unused_kernel_id)
