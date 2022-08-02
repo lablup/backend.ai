5a06029e2232a1e9f89f90972a1eaed5d9b3e8d7
KangJuSeong <39542923+KangJuSeong@users.noreply.github.com>
2022-08-02 15:46:25 +0900

    fix(web): Force the keypair-based auth mode regardless to env-vars (#564)
    Backported-From: main
    Backported-To: 22.03

--- src/ai/backend/web/auth.py
+++ src/ai/backend/web/auth.py
@@ -54,6 +54,7 @@ async def get_api_session(
     config = APIConfig(
         domain=config["api"]["domain"],
         endpoint=config["api"]["endpoint"],
+        endpoint_type="api",
         access_key=ak,
         secret_key=sk,
         user_agent=user_agent,
@@ -73,6 +74,7 @@ async def get_anonymous_session(
     config = APIConfig(
         domain=config["api"]["domain"],
         endpoint=config["api"]["endpoint"],
+        endpoint_type="api",
         access_key="",
         secret_key="",
         user_agent=user_agent,
