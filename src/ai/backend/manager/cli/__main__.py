8a4ee34ba53aaa99488b4f5232b8e30f0c74bf71
Joongi Kim <joongi@lablup.com>
2022-08-18 14:25:04 +0900

    feat: Optimize the launch speed of CLI and daemons in the dev mode (#663)

--- src/ai/backend/manager/cli/__main__.py
+++ src/ai/backend/manager/cli/__main__.py
@@ -21,11 +21,8 @@ from ai.backend.common import redis_helper as redis_helper
 from ai.backend.common.cli import LazyGroup
 from ai.backend.common.logging import BraceStyleAdapter
 from ai.backend.common.validators import TimeDuration
-from ai.backend.manager.models import kernels
-from ai.backend.manager.models.utils import connect_database
 
 from ..config import load as load_config
-from ..models.keypair import generate_keypair as _gen_keypair
 from .context import CLIContext, init_logger, redis_ctx
 
 log = BraceStyleAdapter(logging.getLogger("ai.backend.manager.cli"))
@@ -146,6 +143,8 @@ def generate_keypair(cli_ctx: CLIContext):
     """
     Generate a random keypair and print it out to stdout.
     """
+    from ..models.keypair import generate_keypair as _gen_keypair
+
     log.info("generating keypair...")
     ak, sk = _gen_keypair()
     print(f"Access Key: {ak} ({len(ak)} bytes)")
@@ -177,6 +176,9 @@ def clear_history(cli_ctx: CLIContext, retention, vacuum_full) -> None:
     Delete old records from the kernels table and
     invoke the PostgreSQL's vaccuum operation to clear up the actual disk space.
     """
+    from ai.backend.manager.models import kernels
+    from ai.backend.manager.models.utils import connect_database
+
     local_config = cli_ctx.local_config
     with cli_ctx.logger:
         today = datetime.now()
