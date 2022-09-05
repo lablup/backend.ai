0b7612792164d0738323c7f53eba948493cf681c
lexikovs <lexikovs@gmail.com>
2022-09-05 21:44:50 +0900

    fix: setup issues

--- src/ai/backend/storage/filebrowser/settings_test.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-import pkg_resources
-
-p = Path(pkg_resources.resource_filename(__name__, ""))
-storage_proxy_root_path_index = p.parts.index("storage-proxy")
-
-Path(*p.parts[0:storage_proxy_root_path_index]) / "config/filebrowser"
