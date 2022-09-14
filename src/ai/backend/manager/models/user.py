3343e7f9a20882e02c1c6307fae6b0391791d7df
sh <oper6909@gmail.com>
2022-09-14 12:33:05 +0900

    check list by length

--- src/ai/backend/manager/models/user.py
+++ src/ai/backend/manager/models/user.py
@@ -687,7 +687,7 @@ class ModifyUser(graphene.Mutation):
                         if row.is_admin and row.is_active:
                             kp_data["is_active"] = False
                             kp_updates.append(kp_data)
-                    if kp_updates:
+                    if len(kp_updates) > 0:
                         await conn.execute(
                             sa.update(keypairs)
                             .values(
