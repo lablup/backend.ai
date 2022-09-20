4cc3ee4cfc331f04bc447176dd12981794499b92
Sanghun Lee <oper6909@gmail.com>
2022-09-20 13:03:32 +0900

    fix: user role update not reflected to active keypair (#718)

--- src/ai/backend/manager/models/user.py
+++ src/ai/backend/manager/models/user.py
@@ -636,7 +636,6 @@ class ModifyUser(graphene.Mutation):
         async def _post_func(conn: SAConnection, result: Result) -> Row:
             nonlocal prev_domain_name, prev_role
             updated_user = result.first()
-
             if "role" in data and data["role"] != prev_role:
                 from ai.backend.manager.models import keypairs
 
@@ -646,6 +645,7 @@ class ModifyUser(graphene.Mutation):
                             keypairs.c.user,
                             keypairs.c.is_active,
                             keypairs.c.is_admin,
+                            keypairs.c.access_key,
                         ]
                     )
                     .select_from(keypairs)
@@ -655,13 +655,14 @@ class ModifyUser(graphene.Mutation):
                 )
                 if data["role"] in [UserRole.SUPERADMIN, UserRole.ADMIN]:
                     # User's becomes admin. Set the keypair as active admin.
+                    # TODO: Should we update the role of all users related to keypair?
                     kp = result.first()
                     kp_data = dict()
                     if not kp.is_admin:
                         kp_data["is_admin"] = True
                     if not kp.is_active:
                         kp_data["is_active"] = True
-                    if len(kp_data.keys()) > 0:
+                    if kp_data:
                         await conn.execute(
                             sa.update(keypairs)
                             .values(kp_data)
@@ -670,36 +671,34 @@ class ModifyUser(graphene.Mutation):
                 else:
                     # User becomes non-admin. Make the keypair non-admin as well.
                     # If there are multiple admin keypairs, inactivate them.
+                    # TODO: Should elaborate keypair inactivation policy.
                     rows = result.fetchall()
-                    cnt = 0
                     kp_updates = []
-                    for row in rows:
+                    for idx, row in enumerate(rows):
                         kp_data = {
-                            "user": row.user,
-                            "is_admin": keypairs.c.is_admin,
-                            "is_active": keypairs.c.is_active,
+                            "b_access_key": row.access_key,
+                            "is_admin": row.is_admin,
+                            "is_active": row.is_active,
                         }
-                        changed = False
-                        if cnt == 0:
+                        if idx == 0:
                             kp_data["is_admin"] = False
-                            changed = True
-                        elif row.is_admin and row.is_active:
-                            kp_data["is_active"] = False
-                            changed = True
-                        if changed:
                             kp_updates.append(kp_data)
-                        cnt += 1
-                    await conn.execute(
-                        sa.update(keypairs)
-                        .values(
-                            {
-                                "is_admin": bindparam("is_admin"),
-                                "is_active": bindparam("is_active"),
-                            }
+                            continue
+                        if row.is_admin and row.is_active:
+                            kp_data["is_active"] = False
+                            kp_updates.append(kp_data)
+                    if kp_updates:
+                        await conn.execute(
+                            sa.update(keypairs)
+                            .values(
+                                {
+                                    "is_admin": bindparam("is_admin"),
+                                    "is_active": bindparam("is_active"),
+                                }
+                            )
+                            .where(keypairs.c.access_key == bindparam("b_access_key")),
+                            kp_updates,
                         )
-                        .where(keypairs.c.user == bindparam("user")),
-                        kp_updates,
-                    )
 
             # If domain is changed and no group is associated, clear previous domain's group.
             if prev_domain_name != updated_user.domain_name and not props.group_ids:
