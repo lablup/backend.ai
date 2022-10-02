7e3320f7a9501bda837ea0b8bcf016297cf3c4e0
soheeeeP <tpfj@naver.com>
2022-10-02 06:58:22 +0000

    add: sh script creating/updating the news fragments

---
+++ scripts/update-news-fragment.py
@@ -0,0 +1,66 @@
+import os
+import re
+import sys
+import argparse
+import tomli as tomllib
+from pathlib import Path
+
+
+def get_config():
+    directory = os.path.abspath("./")
+    config_path = Path(os.path.join(directory, "pyproject.toml"))
+
+    with open(config_path, "rb") as conf:
+        config_toml = tomllib.load(conf)
+    config = config_toml["tool"]["towncrier"]
+    return config
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--content', default='',
+        help='Enter the contents for the news fragment to modify.'
+    )
+    parser.add_argument(
+        '-f', '--fragment', type=str, default='',
+        help='Enter the fragment types for the new fragment you want to modify.'
+    )
+
+    args = parser.parse_args()
+    if not args.content:
+        print("::error ::No contents given from args.")
+        sys.exit(1)
+    if not args.fragment:
+        print("::error ::No fragment types given from args.")
+        sys.exit(1)
+
+    config = get_config()
+    fragment_dir = config.get("directory")
+    try:
+        files = os.listdir(fragment_dir)
+    except FileNotFoundError as e:
+        raise Exception()
+
+    files = [basename for basename in files if len(basename.split(".")) == 3]
+    if not files:
+        # FIX: integer value to issue number linked to pull request
+        basename = '.'.join([str(0), args.fragment, 'md'])
+        output_path = Path(os.path.join(fragment_dir, basename))
+        output_path.write_text(args.content)
+        print(f'\n### {args.fragment.title()}\n * {args.content} ({basename})', file=sys.stderr)
+        print(f'Successfully created the "{args.fragment.title()}" news fragment found by towncrier.\n', file=sys.stderr)
+
+        sys.exit(0)
+
+    for basename in files:
+        parts = basename.split(".")
+        if parts[1] != args.fragment:
+            continue
+        output_path = Path(os.path.join(fragment_dir, basename))
+        output_path.write_text(args.content)
+        print(f'\n### {parts[1].title()}\n * {args.content} ({basename})', file=sys.stderr)
+        print(f'Successfully updated the "{parts[1].title()}" news fragment found by towncrier.\n', file=sys.stderr)
+
+if __name__ == '__main__':
+    main()
