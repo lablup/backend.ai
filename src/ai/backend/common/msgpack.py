0edded203750b75af1e69e41f372023097acd53d
Joongi Kim <joongi@lablup.com>
2022-11-03 14:14:27 +0900

    refactor: Add ext-type hooks to common.msgpack

--- src/ai/backend/common/msgpack.py
+++ src/ai/backend/common/msgpack.py
@@ -2,16 +2,54 @@
 Wrapper of msgpack-python with good defaults.
 """
 
+import datetime
+import enum
+import pickle
+import uuid
+from decimal import Decimal
 from typing import Any
 
 import msgpack as _msgpack
+import temporenc
+
+__all__ = ("packb", "unpackb")
+
+
+class ExtTypes(enum.IntEnum):
+    # We can define up to 255 extension type identifiers.
+    UUID = 1
+    DATETIME = 16
+    DECIMAL = 32
+    BINARY_SIZE = 33
+
+
+def _default(obj: object) -> _msgpack.ExtType:
+    match obj:
+        case uuid.UUID():
+            return _msgpack.ExtType(ExtTypes.UUID, obj.bytes)
+        case datetime.datetime():
+            return _msgpack.ExtType(ExtTypes.DATETIME, temporenc.packb(obj))
+        case Decimal():
+            return _msgpack.ExtType(ExtTypes.DECIMAL, pickle.dumps(obj, protocol=5))
+    raise TypeError("Unknown type: %r" % (obj,))
+
+
+def _ext_hook(code: int, data: bytes) -> Any:
+    match code:
+        case ExtTypes.UUID:
+            return uuid.UUID(bytes=data)
+        case ExtTypes.DATETIME:
+            return temporenc.unpackb(data).datetime()
+        case ExtTypes.DECIMAL:
+            return pickle.loads(data)
+    return _msgpack.ExtType(code, data)
 
 
 def packb(data: Any, **kwargs) -> bytes:
     opts = {"use_bin_type": True, **kwargs}
-    return _msgpack.packb(data, **opts)
+    return _msgpack.packb(data, default=_default, **opts)
 
 
 def unpackb(packed: bytes, **kwargs) -> Any:
     opts = {"raw": False, "use_list": False, **kwargs}
-    return _msgpack.unpackb(packed, **opts)
+    return _msgpack.unpackb(packed, ext_hook=_ext_hook, **opts)
--- tests/common/test_msgpack.py
+++ tests/common/test_msgpack.py
@@ -1,4 +1,11 @@
+import uuid
+from datetime import datetime
+from decimal import Decimal
+
+from dateutil.tz import tzutc
+
 from ai.backend.common import msgpack
+from ai.backend.common.types import BinarySize
 
 
 def test_msgpack_with_unicode():
@@ -27,3 +34,48 @@ def test_msgpack_kwargs():
     assert isinstance(unpacked["mem"], list)
     assert isinstance(unpacked["cuda_mem"], list)
     assert isinstance(unpacked["cuda_util"], list)
+
+
+def test_msgpack_uuid():
+    device_id = uuid.uuid4()
+    data = {"device_id": device_id}
+    packed = msgpack.packb(data)
+    unpacked = msgpack.unpackb(packed)
+    assert isinstance(unpacked["device_id"], uuid.UUID)
+    assert unpacked["device_id"] == device_id
+
+
+def test_msgpack_datetime():
+    now = datetime.now(tzutc())
+    data = {"timestamp": now}
+    packed = msgpack.packb(data)
+    unpacked = msgpack.unpackb(packed)
+    assert isinstance(unpacked["timestamp"], datetime)
+    assert unpacked["timestamp"] == now
+
+
+def test_msgpack_decimal():
+    value = Decimal(
+        "1209705197565610203801239512319273475.2350976162030923750923750961028963490861246890575"
+    )
+    data = {"value": value}
+    packed = msgpack.packb(data)
+    unpacked = msgpack.unpackb(packed)
+    assert isinstance(unpacked["value"], Decimal)
+    assert unpacked["value"] == value
+
+
+def test_msgpack_binarysize():
+    size = BinarySize.from_str("64T")
+    data = {"size": size}
+    packed = msgpack.packb(data)
+    unpacked = msgpack.unpackb(packed)
+    assert isinstance(unpacked["size"], int)
+    assert unpacked["size"] == size
+
+    size = BinarySize.from_str("Infinity")
+    data = {"size": size}
+    packed = msgpack.packb(data)
+    unpacked = msgpack.unpackb(packed)
+    assert isinstance(unpacked["size"], Decimal)
+    assert unpacked["size"] == Decimal("Infinity")
