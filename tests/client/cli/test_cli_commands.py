ef208b4ab59bf6572f7fc2ea4dbe28a7aa43949f
Joongi Kim <joongi@lablup.com>
2022-11-23 18:01:52 +0900

    fix: Add allowlist/blocklist in `cli.loader.load_entry_points()`
    * In pex-based test environments, there are no manager/agent/etc.
      CLI entrypoints importable but scanned via
      `ai.backend.plug.entrypoint`.
      We need to import only `ai.backend.client.cli` for the client test suite.

--- tests/client/cli/test_cli_commands.py
+++ tests/client/cli/test_cli_commands.py
@@ -1,10 +1,10 @@
 import re
-import sys
 
 import pytest
 from click.testing import CliRunner
 
-from ai.backend.client.cli.main import main
+from ai.backend.cli.loader import load_entry_points
+from ai.backend.cli.types import ExitCode
 from ai.backend.client.config import get_config, set_config
 
 
@@ -13,20 +13,25 @@ def runner():
     return CliRunner()
 
 
+@pytest.fixture(scope="module")
+def cli_entrypoint():
+    return load_entry_points(allowlist={"ai.backend.client.cli"})
+
+
 @pytest.mark.parametrize("help_arg", ["-h", "--help"])
-def test_print_help(runner, help_arg):
-    result = runner.invoke(main, [help_arg])
-    assert result.exit_code == 0
+def test_print_help(runner, cli_entrypoint, help_arg):
+    result = runner.invoke(cli_entrypoint, [help_arg])
+    assert result.exit_code == ExitCode.OK
     assert re.match(r"Usage: ([.\w]+) \[OPTIONS\] COMMAND \[ARGS\]", result.output)
 
 
-def test_print_help_for_unknown_command(runner):
-    result = runner.invoke(main, ["x-non-existent-command"])
-    assert result.exit_code == 2
+def test_print_help_for_unknown_command(runner, cli_entrypoint):
+    result = runner.invoke(cli_entrypoint, ["x-non-existent-command"])
+    assert result.exit_code == ExitCode.INVALID_USAGE
     assert re.match(r"Usage: ([.\w]+) \[OPTIONS\] COMMAND \[ARGS\]", result.output)
 
 
-def test_config(runner, monkeypatch, example_keypair, unused_tcp_port_factory):
+def test_config(runner, cli_entrypoint, monkeypatch, example_keypair, unused_tcp_port_factory):
     api_port = unused_tcp_port_factory()
     api_url = "http://127.0.0.1:{}".format(api_port)
     set_config(None)
@@ -34,8 +39,8 @@ def test_config(runner, monkeypatch, example_keypair, unused_tcp_port_factory):
     monkeypatch.setenv("BACKEND_SECRET_KEY", example_keypair[1])
     monkeypatch.setenv("BACKEND_ENDPOINT", api_url)
     config = get_config()
-    result = runner.invoke(main, ["config"])
-    assert result.exit_code == 0
+    result = runner.invoke(cli_entrypoint, ["config"])
+    assert result.exit_code == ExitCode.OK
     assert str(config.endpoint) in result.output
     assert config.version in result.output
     assert config.access_key in result.output
@@ -43,37 +48,39 @@ def test_config(runner, monkeypatch, example_keypair, unused_tcp_port_factory):
     assert config.hash_type in result.output
 
 
-def test_config_unset(runner, monkeypatch):
+def test_config_unset(runner, cli_entrypoint, monkeypatch):
     monkeypatch.delenv("BACKEND_ACCESS_KEY", raising=False)
     monkeypatch.delenv("BACKEND_SECRET_KEY", raising=False)
     monkeypatch.delenv("BACKEND_ENDPOINT", raising=False)
     # now this works as "anonymous" session config.
-    result = runner.invoke(main, ["config"])
-    assert result.exit_code == 0
+    result = runner.invoke(cli_entrypoint, ["config"])
+    assert result.exit_code == ExitCode.OK
 
 
-def test_compiler_shortcut(mocker):
-    mocker.patch.object(sys, "argv", ["lcc", "-h"])
-    try:
-        main()
-    except SystemExit:
-        pass
-    assert sys.argv == ["lcc", "-h"]
+# def test_compiler_shortcut(mocker):
+#     mocker.patch.object(sys, "argv", ["lcc", "-h"])
+#     try:
+#         main()
+#     except SystemExit:
+#         pass
+#     assert sys.argv == ["lcc", "-h"]
+#
+#     mocker.patch.object(sys, "argv", ["lpython", "-h"])
+#     try:
+#         main()
+#     except SystemExit:
+#         pass
+#     assert sys.argv == ["lpython", "-h"]
 
-    mocker.patch.object(sys, "argv", ["lpython", "-h"])
-    try:
-        main()
-    except SystemExit:
-        pass
-    assert sys.argv == ["lpython", "-h"]
 
-
-def test_run_file_or_code_required(runner, monkeypatch, example_keypair, unused_tcp_port_factory):
+def test_run_file_or_code_required(
+    runner, cli_entrypoint, monkeypatch, example_keypair, unused_tcp_port_factory
+):
     api_port = unused_tcp_port_factory()
     api_url = "http://127.0.0.1:{}".format(api_port)
     monkeypatch.setenv("BACKEND_ACCESS_KEY", example_keypair[0])
     monkeypatch.setenv("BACKEND_SECRET_KEY", example_keypair[1])
     monkeypatch.setenv("BACKEND_ENDPOINT", api_url)
-    result = runner.invoke(main, ["run", "python"])
-    assert result.exit_code == 1
+    result = runner.invoke(cli_entrypoint, ["run", "python"])
+    assert result.exit_code == ExitCode.INVALID_ARGUMENT
     assert "provide the command-line code snippet" in result.output
