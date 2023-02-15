4f8d7f3326809b44c3ba434b7668dcf09883d9f2
Jeongseok Kang <jskang@lablup.com>
2023-02-15 17:13:15 +0900

    fix: Update missing alembic migration

--- src/ai/backend/manager/models/alembic/versions/b5be363ab05c_.py
+++ src/ai/backend/manager/models/alembic/versions/b5be363ab05c_.py
@@ -5,8 +5,10 @@ Revises: cace152eefac
 Create Date: 2023-02-15 15:24:29.112938
 
 """
-import sqlalchemy as sa
+import textwrap
+
 from alembic import op
+from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
 revision = "b5be363ab05c"
@@ -16,8 +18,28 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    conn = op.get_bind()
+    conn.execute(
+        text(
+            textwrap.dedent(
+                """\
+            ALTER TABLE sessions
+                ALTER COLUMN session_type ADD VALUE 'INFERENCE'
+            """
+            )
+        )
+    )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    conn.execute(
+        text(
+            textwrap.dedent(
+                """\
+            ALTER TABLE sessions
+                ALTER COLUMN session_type DROP INFERENCE
+            """
+            )
+        )
+    )
