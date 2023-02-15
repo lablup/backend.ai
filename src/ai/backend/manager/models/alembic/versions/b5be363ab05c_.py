a6d270f68f1b484d9f466c5837ae0a16ba92c0cf
Jeongseok Kang <jskang@lablup.com>
2023-02-15 19:48:44 +0900

    Revert "fix: Update alembic migration script for `SessionType.INFERENCE`"
    This reverts commit 0d530c2bd5b293f86f33c0a7ed058e0966844871.

--- src/ai/backend/manager/models/alembic/versions/b5be363ab05c_.py
+++ src/ai/backend/manager/models/alembic/versions/b5be363ab05c_.py
@@ -1,10 +1,14 @@
-"""
+"""Define a new SessionType: INFERENCE.
 
 Revision ID: b5be363ab05c
 Revises: cace152eefac
 Create Date: 2023-02-15 15:24:29.112938
 
 """
+import textwrap
+
+from alembic import op
+from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
 revision = "b5be363ab05c"
@@ -14,8 +18,28 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    conn = op.get_bind()
+    conn.execute(
+        text(
+            textwrap.dedent(
+                """\
+            ALTER TABLE sessions
+                ALTER COLUMN session_type ADD VALUE 'INFERENCE'
+            """
+            )
+        )
+    )
 
 
 def downgrade():
-    pass
+    conn = op.get_bind()
+    conn.execute(
+        text(
+            textwrap.dedent(
+                """\
+            ALTER TABLE sessions
+                ALTER COLUMN session_type DROP INFERENCE
+            """
+            )
+        )
+    )
