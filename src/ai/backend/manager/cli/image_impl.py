974fd50f7baee640446e327d55dc6aedfd5f6e2b
Joongi Kim <joongi@lablup.com>
2023-02-24 08:46:46 +0000

    refactor: Make nested async-with blocks more concise

--- src/ai/backend/manager/cli/image_impl.py
+++ src/ai/backend/manager/cli/image_impl.py
@@ -46,72 +46,76 @@ async def etcd_ctx(cli_ctx) -> AsyncIterator[AsyncEtcd]:
 
 async def list_images(cli_ctx, short, installed_flag):
     # Connect to postgreSQL DB
-    async with connect_database(cli_ctx.local_config) as db:
-        async with db.begin_readonly_session() as session:
-            displayed_items = []
-            try:
-                items = await ImageRow.list(session)
-                # NOTE: installed/installed_agents fields are no longer provided in CLI,
-                #       until we finish the epic refactoring of image metadata db.
-                async with redis_ctx(cli_ctx) as redis_conn_set:
-                    for item in items:
-                        installed = (
-                            await redis_helper.execute(
-                                redis_conn_set.image, lambda r: r.scard(item.name)
-                            )
-                        ) > 0
-                        installed_agents = await redis_helper.execute(
-                            redis_conn_set.image, lambda r: r.smembers(item.name)
-                        )
+    async with (
+        connect_database(cli_ctx.local_config) as db,
+        db.begin_readonly_session() as session,
+        redis_ctx(cli_ctx) as redis_conn_set,
+    ):
+        displayed_items = []
+        try:
+            items = await ImageRow.list(session)
+            # NOTE: installed/installed_agents fields are no longer provided in CLI,
+            #       until we finish the epic refactoring of image metadata db.
+            for item in items:
+                installed = (
+                    await redis_helper.execute(redis_conn_set.image, lambda r: r.scard(item.name))
+                ) > 0
+                installed_agents = await redis_helper.execute(
+                    redis_conn_set.image, lambda r: r.smembers(item.name)
+                )
 
-                        if installed_flag and not installed:
-                            continue
-                        if short:
-                            displayed_items.append((item.image_ref.canonical, item.config_digest))
-                        elif installed_flag:
-                            pprint(f"{item} @ {installed_agents}")
-                        else:
-                            pprint(item)
-                    if short:
-                        print(tabulate(displayed_items, tablefmt="plain"))
-            except Exception:
-                log.exception("An error occurred.")
+                if installed_flag and not installed:
+                    continue
+                if short:
+                    displayed_items.append((item.image_ref.canonical, item.config_digest))
+                elif installed_flag:
+                    pprint(f"{item} @ {installed_agents}")
+                else:
+                    pprint(item)
+            if short:
+                print(tabulate(displayed_items, tablefmt="plain"))
+        except Exception:
+            log.exception("An error occurred.")
 
 
 async def inspect_image(cli_ctx, canonical_or_alias, architecture):
-    async with connect_database(cli_ctx.local_config) as db:
-        async with db.begin_readonly_session() as session:
-            try:
-                image_row = await ImageRow.resolve(
-                    session,
-                    [
-                        ImageRef(canonical_or_alias, ["*"], architecture),
-                        canonical_or_alias,
-                    ],
-                )
-                pprint(await image_row.inspect())
-            except UnknownImageReference:
-                log.exception("Image not found.")
-            except Exception:
-                log.exception("An error occurred.")
+    async with (
+        connect_database(cli_ctx.local_config) as db,
+        db.begin_readonly_session() as session,
+    ):
+        try:
+            image_row = await ImageRow.resolve(
+                session,
+                [
+                    ImageRef(canonical_or_alias, ["*"], architecture),
+                    canonical_or_alias,
+                ],
+            )
+            pprint(await image_row.inspect())
+        except UnknownImageReference:
+            log.exception("Image not found.")
+        except Exception:
+            log.exception("An error occurred.")
 
 
 async def forget_image(cli_ctx, canonical_or_alias, architecture):
-    async with connect_database(cli_ctx.local_config) as db:
-        async with db.begin_session() as session:
-            try:
-                image_row = await ImageRow.resolve(
-                    session,
-                    [
-                        ImageRef(canonical_or_alias, ["*"], architecture),
-                        canonical_or_alias,
-                    ],
-                )
-                await session.delete(image_row)
-            except UnknownImageReference:
-                log.exception("Image not found.")
-            except Exception:
-                log.exception("An error occurred.")
+    async with (
+        connect_database(cli_ctx.local_config) as db,
+        db.begin_session() as session,
+    ):
+        try:
+            image_row = await ImageRow.resolve(
+                session,
+                [
+                    ImageRef(canonical_or_alias, ["*"], architecture),
+                    canonical_or_alias,
+                ],
+            )
+            await session.delete(image_row)
+        except UnknownImageReference:
+            log.exception("Image not found.")
+        except Exception:
+            log.exception("An error occurred.")
 
 
 async def set_image_resource_limit(
@@ -121,56 +125,64 @@ async def set_image_resource_limit(
     range_value,
     architecture,
 ):
-    async with connect_database(cli_ctx.local_config) as db:
-        async with db.begin_session() as session:
-            try:
-                image_row = await ImageRow.resolve(
-                    session,
-                    [
-                        ImageRef(canonical_or_alias, ["*"], architecture),
-                        canonical_or_alias,
-                    ],
-                )
-                await image_row.set_resource_limit(slot_type, range_value)
-            except UnknownImageReference:
-                log.exception("Image not found.")
-            except Exception:
-                log.exception("An error occurred.")
+    async with (
+        connect_database(cli_ctx.local_config) as db,
+        db.begin_session() as session,
+    ):
+        try:
+            image_row = await ImageRow.resolve(
+                session,
+                [
+                    ImageRef(canonical_or_alias, ["*"], architecture),
+                    canonical_or_alias,
+                ],
+            )
+            await image_row.set_resource_limit(slot_type, range_value)
+        except UnknownImageReference:
+            log.exception("Image not found.")
+        except Exception:
+            log.exception("An error occurred.")
 
 
 async def rescan_images(cli_ctx, registry):
-    async with connect_database(cli_ctx.local_config) as db:
-        async with etcd_ctx(cli_ctx) as etcd:
-            try:
-                await rescan_images_func(etcd, db, registry=registry)
-            except Exception:
-                log.exception("An error occurred.")
+    async with (
+        connect_database(cli_ctx.local_config) as db,
+        etcd_ctx(cli_ctx) as etcd,
+    ):
+        try:
+            await rescan_images_func(etcd, db, registry=registry)
+        except Exception:
+            log.exception("An error occurred.")
 
 
 async def alias(cli_ctx, alias, target, architecture):
-    async with connect_database(cli_ctx.local_config) as db:
-        async with db.begin_session() as session:
-            try:
-                image_row = await ImageRow.resolve(
-                    session,
-                    [
-                        ImageRef(target, ["*"], architecture),
-                    ],
-                )
-                await ImageAliasRow.create(session, alias, image_row)
-            except UnknownImageReference:
-                log.exception("Image not found.")
-            except Exception:
-                log.exception("An error occurred.")
+    async with (
+        connect_database(cli_ctx.local_config) as db,
+        db.begin_session() as session,
+    ):
+        try:
+            image_row = await ImageRow.resolve(
+                session,
+                [
+                    ImageRef(target, ["*"], architecture),
+                ],
+            )
+            await ImageAliasRow.create(session, alias, image_row)
+        except UnknownImageReference:
+            log.exception("Image not found.")
+        except Exception:
+            log.exception("An error occurred.")
 
 
 async def dealias(cli_ctx, alias):
-    async with connect_database(cli_ctx.local_config) as db:
-        async with db.begin_session() as session:
-            alias_row = await session.scalar(
-                sa.select(ImageAliasRow).where(ImageAliasRow.alias == alias),
-            )
-            if alias_row is None:
-                log.exception("Alias not found.")
-                return
-            await session.delete(alias_row)
+    async with (
+        connect_database(cli_ctx.local_config) as db,
+        db.begin_session() as session,
+    ):
+        alias_row = await session.scalar(
+            sa.select(ImageAliasRow).where(ImageAliasRow.alias == alias),
+        )
+        if alias_row is None:
+            log.exception("Alias not found.")
+            return
+        await session.delete(alias_row)
