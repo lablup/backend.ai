f1159509b848f1977490a2cbbb69ae671737f35e
Joongi Kim <joongi@lablup.com>
2023-02-28 05:55:06 +0000

    refactor: Remove duplication

--- scripts/check-cross-imports.sh
+++ scripts/check-cross-imports.sh
@@ -3,36 +3,40 @@
 detect_count=0
 ok_count=0
 
+check_output() {
+  echo "$1"|grep -v '//:'|xargs dirname|uniq|grep "src/ai/backend/$2\$" && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
+}
+
 manager_deps=$(./pants dependencies src/ai/backend/manager:: 2>/dev/null)
-echo -n "manager -> agent: ";   echo "$manager_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/agent$'   && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "manager -> client: ";  echo "$manager_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/client$'  && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "manager -> storage: "; echo "$manager_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/storage$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "manager -> web: ";     echo "$manager_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/web$'     && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
+echo -n "manager -> agent: ";   check_output "$manager_deps" "agent"
+echo -n "manager -> client: ";  check_output "$manager_deps" "client"
+echo -n "manager -> storage: "; check_output "$manager_deps" "storage"
+echo -n "manager -> web: ";     check_output "$manager_deps" "web"
 agent_deps=$(./pants dependencies src/ai/backend/agent:: 2>/dev/null)
-echo -n "agent -> manager: "; echo "$agent_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/manager$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "agent -> client: ";  echo "$agent_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/client$'  && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "agent -> storage: "; echo "$agent_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/storage$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "agent -> web: ";     echo "$agent_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/web$'     && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
+echo -n "agent -> manager: "; check_output "$agent_deps" "manager"
+echo -n "agent -> client: ";  check_output "$agent_deps" "client"
+echo -n "agent -> storage: "; check_output "$agent_deps" "storage"
+echo -n "agent -> web: ";     check_output "$agent_deps" "web"
 client_deps=$(./pants dependencies src/ai/backend/client:: 2>/dev/null)
-echo -n "client -> manager: "; echo "$client_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/manager$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "client -> agent: ";   echo "$client_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/agent$'   && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "client -> storage: "; echo "$client_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/storage$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "client -> web: ";     echo "$client_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/web$    ' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
+echo -n "client -> manager: "; check_output "$client_deps" "manager"
+echo -n "client -> agent: ";   check_output "$client_deps" "agent"
+echo -n "client -> storage: "; check_output "$client_deps" "storage"
+echo -n "client -> web: ";     check_output "$client_deps" "web"
 storage_deps=$(./pants dependencies src/ai/backend/storage:: 2>/dev/null)
-echo -n "storage -> manager: "; echo "$storage_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/manager$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "storage -> agent: ";   echo "$storage_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/agent$'   && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "storage -> client: ";  echo "$storage_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/client$'  && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "storage -> web: ";     echo "$storage_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/web$'     && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
+echo -n "storage -> manager: "; check_output "$storage_deps" "manager"
+echo -n "storage -> agent: ";   check_output "$storage_deps" "agent"
+echo -n "storage -> client: ";  check_output "$storage_deps" "client"
+echo -n "storage -> web: ";     check_output "$storage_deps" "web"
 web_deps=$(./pants dependencies src/ai/backend/web:: 2>/dev/null)
-echo -n "web -> manager: "; echo "$web_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/manager$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "web -> agent: ";   echo "$web_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/agent$'   && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "web -> storage: "; echo "$web_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/storage$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
+echo -n "web -> manager: "; check_output "$web_deps" "manager"
+echo -n "web -> agent: ";   check_output "$web_deps" "agent"
+echo -n "web -> storage: "; check_output "$web_deps" "storage"
 common_deps=$(./pants dependencies src/ai/backend/common:: 2>/dev/null)
-echo -n "common -> manager: "; echo "$common_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/manager$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "common -> agent: ";   echo "$common_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/agent$'   && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "common -> storage: "; echo "$common_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/storage$' && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "common -> web: ";     echo "$common_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/web$'     && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
-echo -n "common -> client: ";  echo "$common_deps"|grep -v '//:'|xargs dirname|uniq|grep 'src/ai/backend/client$'  && { echo "detected cross imports"; detect_count=$((detect_count + 1)); } || { echo "clean"; ok_count=$((ok_count + 1)); }
+echo -n "common -> manager: "; check_output "$common_deps" "manager"
+echo -n "common -> agent: ";   check_output "$common_deps" "agent"
+echo -n "common -> storage: "; check_output "$common_deps" "storage"
+echo -n "common -> web: ";     check_output "$common_deps" "web"
+echo -n "common -> client: ";  check_output "$common_deps" "client"
 
 if [ "$detect_count" -gt 0 ]; then
   echo "Detected invalid cross imports!"
