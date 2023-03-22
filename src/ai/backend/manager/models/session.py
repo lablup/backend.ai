3cba32ededca5a9bdb111ded6e27bb82fa45e0e0
Sanghun Lee <sanghun@lablup.com>
2023-03-23 00:54:17 +0900

    feat: make idle checker report leftover time and utilization (#1160)

--- src/ai/backend/manager/models/session.py
+++ src/ai/backend/manager/models/session.py
@@ -30,6 +30,7 @@ from sqlalchemy.orm import noload, relationship, selectinload
 
 from ai.backend.common.types import (
     AccessKey,
+    BinarySize,
     ClusterMode,
     ResourceSlot,
     SessionId,
@@ -1071,6 +1072,7 @@ class ComputeSession(graphene.ObjectType):
     result = graphene.String()
     commit_status = graphene.String()
     abusing_reports = graphene.List(lambda: graphene.JSONString)
+    idle_checks = graphene.JSONString()
 
     # resources
     resource_opts = graphene.JSONString()
@@ -1227,6 +1229,14 @@ class ComputeSession(graphene.ObjectType):
         if containers is None:
             return []
         return [(await con.resolve_abusing_report(info, self.access_key)) for con in containers]
+
+    async def resolve_idle_checks(self, info: graphene.ResolveInfo) -> Mapping[str, Any]:
+        graph_ctx: GraphQueryContext = info.context
+        idle_report = await graph_ctx.idle_checker_host.get_idle_check_report(self.session_id)
+        avg_utils = idle_report["utilization"]
+        if avg_utils is not None and "mem" in avg_utils:
+            avg_utils["mem"] = BinarySize.from_str(str(int(avg_utils["mem"])))
+        return idle_report
 
     _queryfilter_fieldspec = {
         "id": ("sessions_id", None),
