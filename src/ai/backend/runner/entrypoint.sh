d28c1cd2581cca0c7720365d7e63574f5fb126a1
Jonghyun Park <jpark@lablup.com>
2023-03-26 10:37:09 +0900

    revert: linuxbrew does not recognize symlink

--- src/ai/backend/runner/entrypoint.sh
+++ src/ai/backend/runner/entrypoint.sh
@@ -3,14 +3,6 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-9001}
 
-create_linuxbrew_symlink() {
-  mkdir -p /home/linuxbrew
-  if [ -d "/home/work/.linuxbrew" ]; then
-    ln -s /home/work/.linuxbrew /home/linuxbrew/.linuxbrew
-  fi
-  chown $USER_ID:$GROUP_ID /home/linuxbrew
-}
-
 echo "Kernel started at: $(date -Iseconds -u)"
 
 if [ $USER_ID -eq 0 ]; then
@@ -49,8 +41,6 @@ if [ $USER_ID -eq 0 ]; then
     chmod 0644 "$HOME/.password"
     echo "work:$ALPHA_NUMERIC_VAL" | chpasswd
   fi
-
-  create_linuxbrew_symlink
 
   echo "Executing the main program..."
   exec "$@"
@@ -123,8 +113,6 @@ else
     chmod 0644 "$HOME/.password"
     echo "$USER_NAME:$ALPHA_NUMERIC_VAL" | chpasswd
   fi
-
-  create_linuxbrew_symlink
 
   # The gid 42 is a reserved gid for "shadow" to allow passwrd-based SSH login. (lablup/backend.ai#751)
   # Note that we also need to use our own patched version of su-exec to support multiple gids.
