32236252ae00bcc2888878fe825dd834ed512574
Joongi Kim <joongi@lablup.com>
2023-04-02 16:34:18 +0100

    fix: Add missing new module

---
+++ src/ai/backend/storage/subproc.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+import shlex
+import subprocess
+from collections.abc import AsyncGenerator, Sequence
+
+
+async def spawn_and_watch(
+    cmdargs: Sequence[str | bytes],
+    *,
+    cwd: os.PathLike | None = None,
+    tail_length: int = 50,
+) -> AsyncGenerator[bytes, None]:
+    last_lines: list[bytes] = []
+    proc = await asyncio.create_subprocess_exec(
+        *cmdargs,
+        cwd=cwd,
+        stdin=asyncio.subprocess.DEVNULL,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.STDOUT,
+    )
+    try:
+        assert proc.stdout is not None
+        while True:
+            line = await proc.stdout.readline()
+            if not line:
+                break
+            if len(last_lines) > tail_length:
+                last_lines.pop(0)
+            last_lines.append(line)
+            yield line.rstrip()
+    finally:
+        exit_code = await proc.wait()
+    if exit_code != 0:
+        raise subprocess.CalledProcessError(
+            exit_code, shlex.join(map(str, cmdargs)), b"".join(last_lines)
+        )
