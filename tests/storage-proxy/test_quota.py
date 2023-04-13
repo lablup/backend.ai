9a15b43b458c0cbfafcf58847364102f9c3fab77
Joongi Kim <joongi@lablup.com>
2023-04-13 08:04:27 +0100

    test: Make quota usage change detection more robust

--- tests/storage-proxy/test_quota.py
+++ tests/storage-proxy/test_quota.py
@@ -1,16 +1,34 @@
-import asyncio
 import secrets
 
 import pytest
+from tenacity import AsyncRetrying, stop_after_delay, wait_exponential
 
-from ai.backend.storage.abc import CAP_QUOTA, AbstractVolume
-from ai.backend.storage.types import QuotaConfig
+from ai.backend.storage.abc import CAP_QUOTA, AbstractQuotaModel, AbstractVolume
+from ai.backend.storage.types import QuotaConfig, QuotaUsage
 
 
 @pytest.mark.asyncio
 async def test_quota_capability(volume: AbstractVolume) -> None:
     caps = await volume.get_capabilities()
     print(caps)
+
+
+async def wait_until_quota_changed(
+    quota_model: AbstractQuotaModel,
+    quota_scope_id: str,
+    prev_usage: QuotaUsage,
+) -> QuotaUsage:
+    wait_config = wait_exponential(multiplier=1, min=1, max=5)
+    stop_config = stop_after_delay(max_delay=30.0)
+    qusage = prev_usage
+    async for attempt in AsyncRetrying(wait=wait_config, stop=stop_config):
+        with attempt:
+            qusage = await quota_model.describe_quota_scope(quota_scope_id)
+            if qusage != prev_usage:
+                break
+            prev_usage = qusage
+            raise RuntimeError("The result of describe_quota_scope() did not change.")
+    return qusage
 
 
 @pytest.mark.asyncio
@@ -32,8 +50,7 @@ async def test_quota_limit(test_id: str, volume: AbstractVolume) -> None:
 
     (qspath / "test.txt").write_bytes(secrets.token_bytes(8192))
 
-    await asyncio.sleep(2)  # the filesystem may have some delay to update this info.
-    qusage = await volume.quota_model.describe_quota_scope(qsid)
+    qusage = await wait_until_quota_changed(volume.quota_model, qsid, qusage)
     assert qusage.used_bytes >= 8192
 
     (qspath / "test.txt").unlink()
