1e852472ecf62ecb367d143174ea589d4d92f488
Sanghun Lee <sanghun@lablup.com>
2023-04-18 19:07:06 +0900

    remove possible duplicate code

--- src/ai/backend/storage/api/client.py
+++ src/ai/backend/storage/api/client.py
@@ -275,10 +275,9 @@ async def tus_upload_part(request: web.Request) -> web.Response:
                 parent_dir = vfpath
                 if (dst_dir := params["dst_dir"]) is not None:
                     parent_dir = vfpath / dst_dir
-                if not parent_dir.exists():
-                    parent_dir.mkdir(parents=True, exist_ok=True)
                 target_path: Path = parent_dir / token_data["relpath"]
-                target_path.parent.mkdir(parents=True, exist_ok=True)
+                if not target_path.parent.exists():
+                    target_path.parent.mkdir(parents=True, exist_ok=True)
                 upload_temp_path.rename(target_path)
                 try:
                     loop = asyncio.get_running_loop()
