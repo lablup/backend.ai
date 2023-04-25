1b7b9e5caf4f7825d8e3d335dc9b3f02a0c89dfe
Sanghun Lee <sanghun@lablup.com>
2023-04-25 20:14:21 +0900

    rename method and use snake case

--- src/ai/backend/manager/api/session.py
+++ src/ai/backend/manager/api/session.py
@@ -2039,7 +2039,7 @@ async def match_sessions(request: web.Request, params: Any) -> web.Response:
 
 @server_status_required(READ_ALLOWED)
 @admin_required
-async def get_direct_access_host(request: web.Request) -> web.Response:
+async def get_direct_access_info(request: web.Request) -> web.Response:
     root_ctx: RootContext = request.app["_root.context"]
     session_name = request.match_info["session_name"]
     _, owner_access_key = await get_access_key_scopes(request)
@@ -2058,9 +2058,9 @@ async def get_direct_access_host(request: web.Request) -> web.Response:
                 sshd_ports = sport["host_ports"]
                 break
         resp = {
-            "kernelRole": kernel_role.name,
-            "publicHost": public_host,
-            "sshdPorts": sshd_ports,
+            "kernel_role": kernel_role.name,
+            "public_host": public_host,
+            "sshd_ports": sshd_ports,
         }
     return web.json_response(resp)
 
@@ -2805,7 +2805,7 @@ def create_app(
     cors.add(task_log_resource.add_route("HEAD", get_task_logs))
     cors.add(task_log_resource.add_route("GET", get_task_logs))
     cors.add(
-        app.router.add_route("GET", "/{session_name}/direct-access-host", get_direct_access_host)
+        app.router.add_route("GET", "/{session_name}/direct-access-host", get_direct_access_info)
     )
     cors.add(app.router.add_route("GET", "/{session_name}/logs", get_container_logs))
     cors.add(app.router.add_route("POST", "/{session_name}/rename", rename_session))
