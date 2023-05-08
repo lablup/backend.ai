cffd17782717c41af2eae99833893a84e0d390b7
Kyujin Cho <kyujin.cho@lablup.com>
2023-05-08 18:07:03 +0900

    fix: skip execution of `pr-number-assign.py` script
    * skip execution of `pr-number-assign.py` script, which is triggered
      by update-news-fragments action job, when github.event.pull_request
      event is not defined

--- scripts/pr-number-assign.py
+++ scripts/pr-number-assign.py
@@ -61,5 +61,8 @@ def main(pr_number: str) -> None:
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: python ${sys.argv[0]} <PR Number>")
+        exit(0)
     pr_number = sys.argv[1]
     main(pr_number)
