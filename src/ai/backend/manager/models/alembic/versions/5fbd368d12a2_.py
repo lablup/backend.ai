50bf2bc2eab7e552f2d70dd6ace269ce33a0b052
Joongi Kim <joongi@lablup.com>
2023-05-08 02:49:15 +0100

    chore: Add the alembic merge revision

---
+++ src/ai/backend/manager/models/alembic/versions/5fbd368d12a2_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 5fbd368d12a2
+Revises: fdc9d6ac49b4, 565293b7395a
+Create Date: 2023-05-08 02:48:35.876775
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "5fbd368d12a2"
+down_revision = ("fdc9d6ac49b4", "565293b7395a")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
