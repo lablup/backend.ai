1e0dd464306969bc71a80331da8c3de79a352c96
Joongi Kim <joongi@lablup.com>
2023-05-15 11:01:22 +0900

    fix: Mistake in the download URL and ensure checksum validation

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -154,12 +154,22 @@ has_python() {
 }
 
 install_static_python() {
-  url="https://github.com/indygreg/python-build-standalone/releases/download/20230116/cpython-3.11.1+20230116-$STANDALONE_PYTHON_ARCH-$STANDALONE_PYTHON_ARCH-install_only.tar.gz"
+  local build_date="20230507"
+  local build_version="3.11.3"
+  local build_tag="cpython-${build_version}+${build_date}-${STANDALONE_PYTHON_ARCH}-${STANDALONE_PYTHON_PLATFORM}"
+  dist_url="https://github.com/indygreg/python-build-standalone/releases/download/${build_date}/${build_tag}-install_only.tar.gz"
+  checksum_url="https://github.com/indygreg/python-build-standalone/releases/download/${build_date}/${build_tag}-install_only.tar.gz.sha256"
   cwd=$(pwd)
   mkdir -p .python-runtime && cd .python-runtime
-  show_info "Downloading and installing static Python 3.11.1..."
-  curl -L $url | tar xz
-  mv python/* .
+  show_info "Downloading and installing static Python (${build_tag}) for bootstrapping..."
+  curl -o dist.tar.gz -L "$dist_url"
+  echo "$(curl -sL $checksum_url) dist.tar.gz" | sha256sum --check --status
+  if [ $? -ne 0 ]; then
+    echo "Failed to validate the downloaded static build of Python binary!"
+    exit 1
+  fi
+  tar xzf dist.tar.gz && rm dist.tar.gz
+  mv python/* . && rmdir python
   cd $cwd
 }
 
@@ -184,19 +194,19 @@ DISTRO=$(lsb_release -d 2>/dev/null | grep -Eo $KNOWN_DISTRO  || grep -Eo $KNOWN
 
 if [ $DISTRO = "Darwin" ]; then
   DISTRO="Darwin"
-  STANDALONE_PYTHON_ARCH="apple-darwin"
+  STANDALONE_PYTHON_PLATFORM="apple-darwin"
 elif [ -f /etc/debian_version -o "$DISTRO" == "Debian" -o "$DISTRO" == "Ubuntu" ]; then
   DISTRO="Debian"
-  STANDALONE_PYTHON_ARCH="unknown-linux-gnu"
+  STANDALONE_PYTHON_PLATFORM="unknown-linux-gnu"
 elif [ -f /etc/redhat-release -o "$DISTRO" == "RedHat" -o "$DISTRO" == "CentOS" -o "$DISTRO" == "Amazon" ]; then
   DISTRO="RedHat"
-  STANDALONE_PYTHON_ARCH="unknown-linux-gnu"
+  STANDALONE_PYTHON_PLATFORM="unknown-linux-gnu"
 elif [ -f /etc/system-release -o "$DISTRO" == "Amazon" ]; then
   DISTRO="RedHat"
-  STANDALONE_PYTHON_ARCH="unknown-linux-gnu"
+  STANDALONE_PYTHON_PLATFORM="unknown-linux-gnu"
 elif [ -f /usr/lib/os-release -o "$DISTRO" == "SUSE" ]; then
   DISTRO="SUSE"
-  STANDALONE_PYTHON_ARCH="unknown-linux-gnu"
+  STANDALONE_PYTHON_PLATFORM="unknown-linux-gnu"
 else
   show_error "Sorry, your host OS distribution is not supported by this script."
   show_info "Please send us a pull request or file an issue to support your environment!"
@@ -219,6 +229,8 @@ bpython="$PYTHONPATH/bin/python3"
 if [ $(has_python $bpython) -eq 0 ]; then
   install_static_python
 fi
+$bpython -c 'import sys;print(sys.version_info)'
+exit 0
 
 ROOT_PATH="$(pwd)"
 if [ ! -f "${ROOT_PATH}/BUILD_ROOT" ]; then
