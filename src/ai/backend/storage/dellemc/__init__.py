f231535370c7ffdd206e81b6a9f71a3e84452e73
kmkwon94 <kangmin@lablup.com>
2023-05-18 12:28:23 +0900

    fix: modify the error handling

--- src/ai/backend/storage/dellemc/__init__.py
+++ src/ai/backend/storage/dellemc/__init__.py
@@ -8,11 +8,11 @@ from uuid import UUID
 from ai.backend.common.types import BinarySize, HardwareMetadata
 
 from ..abc import CAP_METRIC, CAP_VFHOST_QUOTA, CAP_VFOLDER
-from ..exception import ExecutionError
 from ..types import FSPerfMetric, FSUsage
 from ..vfs import BaseVolume
 from .dellemc_client import DellEMCClient
 from .dellemc_quota_manager import QuotaManager
+from .exceptions import DellNoMetricError
 
 
 class DellEMCVolume(BaseVolume):
@@ -73,67 +73,48 @@ class DellEMCVolume(BaseVolume):
         raise NotImplementedError
 
     async def get_drive_stats(self):
-        resp = await self.dellEMC_client.get_drive_stats()
-
-        if "errors" in resp:
-            raise ExecutionError("api error")
-        return resp
+        try:
+            resp = await self.dellEMC_client.get_drive_stats()
+            return resp
+        except KeyError:
+            raise DellNoMetricError
 
     async def get_protocol_stats(self):
-        resp = await self.dellEMC_client.get_protocol_stats()
-
-        if "errors" in resp:
-            raise ExecutionError("api error")
-        return resp
+        try:
+            resp = await self.dellEMC_client.get_protocol_stats()
+            return resp
+        except KeyError:
+            raise DellNoMetricError
 
     async def get_system_stats(self):
-        resp = await self.dellEMC_client.get_system_stats()
-
-        if "errors" in resp:
-            raise ExecutionError("api error")
-        return resp
+        try:
+            resp = await self.dellEMC_client.get_system_stats()
+            return resp
+        except KeyError:
+            raise DellNoMetricError
 
     async def get_workload_stats(self):
-        resp = await self.dellEMC_client.get_workload_stats()
-
-        if "errors" in resp:
-            raise ExecutionError("api error")
-        return resp
+        try:
+            resp = await self.dellEMC_client.get_workload_stats()
+            return resp
+        except KeyError:
+            raise DellNoMetricError
 
     async def get_performance_metric(self) -> FSPerfMetric:
-        protocol_stats = await self.get_protocol_stats()
-        workload = await self.get_workload_stats()
+        try:
+            protocol_stats = await self.get_protocol_stats()
+            workload = await self.get_workload_stats()
+        except (KeyError, IndexError):
+            raise DellNoMetricError
         return FSPerfMetric(
-            iops_read=protocol_stats["disk"]["iops"],
+            iops_read=protocol_stats["disk"]["iops"] or 0,
             iops_write=0,  # Dell does not support IOPS Read/Write, They support only IOPS.
-            # io_bytes_read=workload["bytes_out"],
-            # io_bytes_write=workload["bytes_in"],
-            io_bytes_read=protocol_stats["onefs"]["out"],
-            io_bytes_write=protocol_stats["onefs"]["in"],
-            io_usec_read=workload["latency_write"],
-            io_usec_write=workload["latency_read"],
+            io_bytes_read=protocol_stats["onefs"]["out"] or 0,
+            io_bytes_write=protocol_stats["onefs"]["in"] or 0,
+            io_usec_read=workload["latency_write"] or 0,
+            io_usec_write=workload["latency_read"] or 0,
         )
 
     async def create_quota(self, path, type):
-        # path would be start like '/ifs'
-        # type should be selected from the below string.
-        # "directory",
-        # "user",
-        # "group",
-        # "default-directory",
-        # "default-user",
-        # "default-group"
         quota_id = self.quota_manager.create_quota(path, type)
         return quota_id
-
-    """
-    async def get_usage(
-        self,
-        vfid: UUID,
-        relpath: PurePosixPath = None,
-    ) -> VFolderUsage:
-        target_path = self.sanitize_vfpath(vfid, relpath)
-        total_size = 0
-        total_count = 0
-        raw_target_path = bytes(target_path)
-    """
