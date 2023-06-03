0dcf5c1dc88ec3ceda2ed1f53476a8679c49f0fa
Sanghun Lee <sanghun@lablup.com>
2023-06-03 18:13:12 +0900

    impl dummy agent

--- src/ai/backend/agent/dummy/agent.py
+++ src/ai/backend/agent/dummy/agent.py
@@ -1,76 +1,133 @@
+import asyncio
+import random
+from decimal import Decimal
+from pathlib import Path
+from typing import Any, FrozenSet, Literal, Mapping, Optional, Sequence, Tuple
 
-from ..agent import AbstractAgent, AbstractKernelCreationContext
+from ai.backend.common.docker import ImageRef
+from ai.backend.common.types import (
+    AutoPullBehavior,
+    ClusterInfo,
+    ClusterSSHPortMapping,
+    ContainerId,
+    DeviceId,
+    DeviceName,
+    ImageRegistry,
+    KernelCreationConfig,
+    KernelId,
+    MountTypes,
+    SessionId,
+    SlotName,
+)
+
+from ..agent import ACTIVE_STATUS_SET, AbstractAgent, AbstractKernelCreationContext
+from ..kernel import AbstractKernel
+from ..resources import AbstractComputePlugin, KernelResourceSpec, Mount
+from ..types import Container, ContainerStatus, MountInfo
 from .kernel import DummyKernel
+
 
 class DummyKernelCreationContext(AbstractKernelCreationContext[DummyKernel]):
     async def get_extra_envs(self) -> Mapping[str, str]:
         return {}
-    
+
     async def prepare_resource_spec(
         self,
     ) -> Tuple[KernelResourceSpec, Optional[Mapping[str, Any]]]:
-        pass
+        return (
+            KernelResourceSpec(
+                container_id="",
+                allocations={},
+                slots={},
+                mounts=[],
+                scratch_disk_size=0,
+            ),
+            {},
+        )
 
     async def prepare_scratch(self) -> None:
-        pass
+        await asyncio.sleep(2)
 
     async def get_intrinsic_mounts(self) -> Sequence[Mount]:
         return []
-    
+
     async def apply_network(self, cluster_info: ClusterInfo) -> None:
-        pass
+        return
 
     async def prepare_ssh(self, cluster_info: ClusterInfo) -> None:
-        pass
+        await asyncio.sleep(0.1)
 
     async def process_mounts(self, mounts: Sequence[Mount]):
-        pass
+        return
 
     async def apply_accelerator_allocation(
         self,
         computer: AbstractComputePlugin,
         device_alloc: Mapping[SlotName, Mapping[DeviceId, Decimal]],
     ) -> None:
-        pass
+        return
 
     async def generate_accelerator_mounts(
         self,
         computer: AbstractComputePlugin,
         device_alloc: Mapping[SlotName, Mapping[DeviceId, Decimal]],
-    ) -> List[MountInfo]:
-        pass
+    ) -> list[MountInfo]:
+        return []
 
     def resolve_krunner_filepath(self, filename) -> Path:
-        pass
+        return Path()
 
     def get_runner_mount(
         self,
         type: MountTypes,
-        src: Union[str, Path],
-        target: Union[str, Path],
+        src: str | Path,
+        target: str | Path,
         perm: Literal["ro", "rw"] = "ro",
         opts: Mapping[str, Any] = None,
     ):
-        pass
+        return Mount(MountTypes.BIND, Path(), Path())
 
     async def spawn(
         self,
         resource_spec: KernelResourceSpec,
         environ: Mapping[str, str],
         service_ports,
-    ) -> KernelObjectType:
-        pass
+    ) -> DummyKernel:
+        await asyncio.sleep(0.1)
+        return DummyKernel(
+            self.kernel_id,
+            self.session_id,
+            self.agent_id,
+            self.image_ref,
+            self.kspec_version,
+            agent_config=self.local_config,
+            service_ports=service_ports,
+            resource_spec=resource_spec,
+            environ=environ,
+            data={},
+        )
 
     async def start_container(
         self,
         kernel_obj: AbstractKernel,
-        cmdargs: List[str],
+        cmdargs: list[str],
         resource_opts,
         preopen_ports,
     ) -> Mapping[str, Any]:
-        pass
-
-    
+        container_bind_host = self.local_config["container"]["bind-host"]
+        advertised_kernel_host = self.local_config["container"].get("advertised-host")
+        await asyncio.sleep(5)
+        return {
+            "container_id": "",
+            "kernel_host": advertised_kernel_host or container_bind_host,
+            "repl_in_port": 2000,
+            "repl_out_port": 2001,
+            "stdin_port": 2002,  # legacy
+            "stdout_port": 2003,  # legacy
+            "host_ports": [2000, 2001, 2002, 2003],
+            "domain_socket_proxies": [],
+            "block_service_ports": self.internal_data.get("block_service_ports", False),
+        }
 
 
 class DummyAgent(
@@ -80,23 +137,25 @@ class DummyAgent(
         self,
         status_filter: FrozenSet[ContainerStatus] = ACTIVE_STATUS_SET,
     ) -> Sequence[Tuple[KernelId, Container]]:
-        pass
+        return []
 
     async def detect_resources(
         self,
     ) -> Tuple[Mapping[DeviceName, AbstractComputePlugin], Mapping[SlotName, Decimal]]:
-        pass
+        await asyncio.sleep(0.1)
+        return {}, {}
 
     async def scan_images(self) -> Mapping[str, str]:
-        pass
+        await asyncio.sleep(0.1)
+        return {}
 
     async def pull_image(self, image_ref: ImageRef, registry_conf: ImageRegistry) -> None:
-        pass
+        await asyncio.sleep(random.uniform(0.5, 10))
 
     async def check_image(
         self, image_ref: ImageRef, image_id: str, auto_pull: AutoPullBehavior
     ) -> bool:
-        pass
+        return True
 
     async def init_kernel_context(
         self,
@@ -106,15 +165,23 @@ class DummyAgent(
         *,
         restarting: bool = False,
         cluster_ssh_port_mapping: Optional[ClusterSSHPortMapping] = None,
-    ) -> AbstractKernelCreationContext:
-        pass
+    ) -> DummyKernelCreationContext:
+        return DummyKernelCreationContext(
+            kernel_id,
+            session_id,
+            self.id,
+            kernel_config,
+            self.local_config,
+            self.computers,
+            restarting=restarting,
+        )
 
     async def destroy_kernel(
         self,
         kernel_id: KernelId,
         container_id: Optional[ContainerId],
     ) -> None:
-        pass
+        await asyncio.sleep(random.uniform(0.2, 5))
 
     async def clean_kernel(
         self,
@@ -122,20 +189,21 @@ class DummyAgent(
         container_id: Optional[ContainerId],
         restarting: bool,
     ) -> None:
-        pass
+        await asyncio.sleep(random.uniform(0.5, 100))
 
     async def create_local_network(self, network_name: str) -> None:
-        pass
+        await asyncio.sleep(2)
 
     async def destroy_local_network(self, network_name: str) -> None:
-        pass
+        await asyncio.sleep(2)
 
     async def restart_kernel__load_config(
         self,
         kernel_id: KernelId,
         name: str,
     ) -> bytes:
-        pass
+        await asyncio.sleep(0.1)
+        return b""
 
     async def restart_kernel__store_config(
         self,
@@ -143,6 +211,4 @@ class DummyAgent(
         name: str,
         data: bytes,
     ) -> None:
-        pass
-
-
+        await asyncio.sleep(0.1)
--- src/ai/backend/agent/dummy/kernel.py
+++ src/ai/backend/agent/dummy/kernel.py
@@ -1,8 +1,42 @@
+import asyncio
+from typing import Any, Dict, FrozenSet, Mapping, Sequence
+
+from ai.backend.common.docker import ImageRef
+from ai.backend.common.types import AgentId, CommitStatus, KernelId, SessionId
 
 from ..kernel import AbstractCodeRunner, AbstractKernel
+from ..resources import KernelResourceSpec
 
 
 class DummyKernel(AbstractKernel):
+    def __init__(
+        self,
+        kernel_id: KernelId,
+        session_id: SessionId,
+        agent_id: AgentId,
+        image: ImageRef,
+        version: int,
+        *,
+        agent_config: Mapping[str, Any],
+        resource_spec: KernelResourceSpec,
+        service_ports: Any,  # TODO: type-annotation
+        environ: Mapping[str, Any],
+        data: Dict[str, Any],
+    ) -> None:
+        super().__init__(
+            kernel_id,
+            session_id,
+            agent_id,
+            image,
+            version,
+            agent_config=agent_config,
+            resource_spec=resource_spec,
+            service_ports=service_ports,
+            data=data,
+            environ=environ,
+        )
+        self.is_commiting = False
+
     async def close(self) -> None:
         pass
 
@@ -12,61 +46,108 @@ class DummyKernel(AbstractKernel):
         client_features: FrozenSet[str],
         api_version: int,
     ) -> "AbstractCodeRunner":
-        pass
+        return await DummyCodeRunner.new(
+            self.kernel_id,
+            kernel_host=self.data["kernel_host"],
+            repl_in_port=self.data["repl_in_port"],
+            repl_out_port=self.data["repl_out_port"],
+            exec_timeout=0,
+            client_features=client_features,
+        )
 
     async def check_status(self):
-        pass
+        await asyncio.sleep(1)
+        return {}
 
     async def get_completions(self, text, opts):
-        pass
+        await asyncio.sleep(0.5)
+        return {"status": "finished", "completions": []}
 
     async def get_logs(self):
-        pass
+        await asyncio.sleep(0.5)
+        return {"logs": "my logs"}
 
     async def interrupt_kernel(self):
-        pass
-
+        await asyncio.sleep(1)
 
     async def start_service(self, service, opts):
-        pass
+        await asyncio.sleep(10)
 
+    async def start_model_service(self, model_service: Mapping[str, Any]):
+        await asyncio.sleep(50)
+        return {}
 
     async def shutdown_service(self, service):
-        pass
-
+        await asyncio.sleep(3)
 
     async def check_duplicate_commit(self, kernel_id, subdir) -> CommitStatus:
-        pass
-
+        if self.is_commiting:
+            return CommitStatus.ONGOING
+        return CommitStatus.READY
 
     async def commit(self, kernel_id, subdir, filename):
-        pass
-
+        self.is_commiting = True
+        await asyncio.sleep(200)
+        self.is_commiting = False
 
     async def get_service_apps(self):
-        pass
-
+        await asyncio.sleep(1)
+        return {
+            "status": "done",
+            "data": [
+                {"name": "jupyter"},
+                {"name": "vscode", "allowed_arguments": ["--auth"], "allowed_envs": ["PASSWORD"]},
+                {"name": "nni"},
+                {"name": "ipython"},
+                {"name": "digits"},
+                {"name": "jupyterlab"},
+                {"name": "sftp"},
+                {"name": "tensorboard", "allowed_arguments": ["--logdir"]},
+            ],
+        }
 
     async def accept_file(self, filename, filedata):
-        pass
-
+        await asyncio.sleep(3)
 
     async def download_file(self, filepath):
-        pass
-
+        await asyncio.sleep(30)
+        return b""
 
     async def download_single(self, filepath):
-        pass
-
+        await asyncio.sleep(30)
+        return b""
 
     async def list_files(self, path: str):
-        pass
+        await asyncio.sleep(30)
+        return {"files": "", "errors": "", "abspath": ""}
+
 
 class DummyCodeRunner(AbstractCodeRunner):
+    kernel_host: str
+    repl_in_port: int
+    repl_out_port: int
+
+    def __init__(
+        self,
+        kernel_id,
+        *,
+        kernel_host,
+        repl_in_port,
+        repl_out_port,
+        exec_timeout=0,
+        client_features=None,
+    ) -> None:
+        super().__init__(kernel_id, exec_timeout=exec_timeout, client_features=client_features)
+        self.kernel_host = kernel_host
+        self.repl_in_port = repl_in_port
+        self.repl_out_port = repl_out_port
+
     async def get_repl_in_addr(self) -> str:
-        pass
+        return f"tcp://{self.kernel_host}:{self.repl_in_port}"
 
     async def get_repl_out_addr(self) -> str:
-        pass
+        return f"tcp://{self.kernel_host}:{self.repl_out_port}"
 
-    
+
+async def prepare_krunner_env(local_config: Mapping[str, Any]) -> Mapping[str, Sequence[str]]:
+    return {}
