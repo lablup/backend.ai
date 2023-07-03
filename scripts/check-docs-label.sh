66640b313c4324931ed51b1aaec235218937e5d0
Joongi Kim <joongi@lablup.com>
2023-07-03 19:32:46 +0900

    ci: Clean up

--- scripts/check-docs-label.sh
+++ scripts/check-docs-label.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-wget -O ./jq https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64
+# This script is executed inside the RTD's build environment.
+wget -qO ./jq https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64
 chmod +x ./jq
-([ "${READTHEDOCS_VERSION_TYPE}" = "external" ] && curl -sL https://api.github.com/repos/lablup/backend.ai/issues/${READTHEDOCS_VERSION}/labels | ./jq -e '.[] | select(.name == "area:docs")') && echo "continue" || exit 183
+([ "${READTHEDOCS_VERSION_TYPE}" = "external" ] && curl -sL https://api.github.com/repos/lablup/backend.ai/issues/${READTHEDOCS_VERSION}/labels | ./jq -e '.[] | select(.name == "area:docs")' > /dev/null) && echo "continue" || exit 183
