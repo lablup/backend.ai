67e1147aac4e581e7bb7cb776409e22682e8e4a2
Joongi Kim <joongi@lablup.com>
2023-07-03 19:30:53 +0900

    ci: Install jq by myself

--- scripts/check-docs-label.sh
+++ scripts/check-docs-label.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-([ "${READTHEDOCS_VERSION_TYPE}" = "external" ] && curl -sL https://api.github.com/repos/lablup/backend.ai/issues/${READTHEDOCS_VERSION}/labels | jq -e '.[] | select(.name == "area:docs")') && echo "continue" || exit 183
+wget -O ./jq https://github.com/jqlang/jq/releases/download/jq-1.6/jq-linux64
+chmod +x ./jq
+([ "${READTHEDOCS_VERSION_TYPE}" = "external" ] && curl -sL https://api.github.com/repos/lablup/backend.ai/issues/${READTHEDOCS_VERSION}/labels | ./jq -e '.[] | select(.name == "area:docs")') && echo "continue" || exit 183
