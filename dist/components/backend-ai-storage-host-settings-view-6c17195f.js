70aea9667cc0be2ff0318ebad38e46b004626746
Kyujin Cho <kyujin.cho@lablup.com>
2023-07-17 23:29:49 +0900

    Squashed 'src/ai/backend/web/static/' changes from aa694b48..26003b86
    26003b86 Updated to v23.03.3
    
    git-subtree-dir: src/ai/backend/web/static
    git-subtree-split: 26003b86f742ceca16206df1482b34767940c2b8

--- dist/components/backend-ai-storage-host-settings-view-6c17195f.js
+++
@@ -1,8 +0,0 @@
-import{B as a,d as t,I as e,b as s,i,y as o,a1 as n,a2 as r,_ as d,a as c}from"./backend-ai-webui-ab578ea5.js";
-/**
- @license
-Copyright (c) 2015-2023 Lablup Inc. All rights reserved.
-*/let h=class extends a{static get styles(){return[t,e,s,i`
-      `]}async _viewStateChanged(a){await this.updateComplete}render(){return o`
-      <backend-ai-react-storage-host-settings value="${window.location.pathname.split("/")[2]}"  @moveTo="${a=>{const t=a.detail.path;globalThis.history.pushState({},"",t),n.dispatch(r(decodeURIComponent(t),{}))}}"></backend-ai-react-storage-host-settings>
-    `}};h=d([c("backend-ai-storage-host-settings-view")],h);var l=h;export{l as default};
