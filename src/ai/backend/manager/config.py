af69f346ae1a74627a2da1cd36e0c9f634166dbb
JinMyeong Kim <84405002+kimjinmyeong@users.noreply.github.com>
2023-08-06 13:26:25 +0900

    fix: Add `empty_str_as_empty_list=True` option in StringList (#1451)
    Co-authored-by: Joongi Kim <joongi@lablup.com>
    Backported-from: main
    Backported-to: 22.09

--- src/ai/backend/manager/config.py
+++ src/ai/backend/manager/config.py
@@ -325,7 +325,9 @@ container_registry_iv = t.Dict(
         t.Key("type", default="docker"): t.String,
         t.Key("username", default=None): t.Null | t.String,
         t.Key("password", default=None): t.Null | t.String,
-        t.Key("project", default=None): t.Null | tx.StringList | t.List(t.String),
+        t.Key("project", default=None): t.Null | tx.StringList(
+            allow_blank=True, empty_str_as_empty_list=True
+        ),
         t.Key("ssl-verify", default=True): t.ToBool,
     }
 ).allow_extra("*")
