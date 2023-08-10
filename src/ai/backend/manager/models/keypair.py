1675bca4da27c06bf2d8dffa7c809010e30da776
Sanghun Lee <sanghun@lablup.com>
2023-08-10 16:55:58 +0900

    remove not needed joins

--- src/ai/backend/manager/models/keypair.py
+++ src/ai/backend/manager/models/keypair.py
@@ -321,18 +321,9 @@ class KeyPair(graphene.ObjectType):
         is_active: bool = None,
         filter: str = None,
     ) -> int:
-        from .group import association_groups_users, groups
         from .user import users
 
-        j = (
-            sa.join(keypairs, users, keypairs.c.user == users.c.uuid, isouter=True)
-            .join(
-                association_groups_users,
-                users.c.uuid == association_groups_users.c.user_id,
-                isouter=True,
-            )
-            .join(groups, association_groups_users.c.group_id == groups.c.id)
-        )
+        j = sa.join(keypairs, users, keypairs.c.user == users.c.uuid)
         query = sa.select([sa.func.count(sa.distinct(keypairs.c.user_id))]).select_from(j)
         if domain_name is not None:
             query = query.where(users.c.domain_name == domain_name)
@@ -364,7 +355,7 @@ class KeyPair(graphene.ObjectType):
         from .user import users
 
         j = (
-            sa.join(keypairs, users, keypairs.c.user == users.c.uuid, isouter=True)
+            sa.join(keypairs, users, keypairs.c.user == users.c.uuid)
             .join(
                 association_groups_users,
                 users.c.uuid == association_groups_users.c.user_id,
