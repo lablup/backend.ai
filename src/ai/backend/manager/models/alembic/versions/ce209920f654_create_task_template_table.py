6e276c2df59c4eb2cd7dff02f406d8a2b206bc5d
Joongi Kim <joongi@lablup.com>
2023-08-12 13:02:35 +0900

    lint: Auto-fix by ruff (remove unused imports)

--- src/ai/backend/manager/models/alembic/versions/ce209920f654_create_task_template_table.py
+++ src/ai/backend/manager/models/alembic/versions/ce209920f654_create_task_template_table.py
@@ -7,10 +7,9 @@ Create Date: 2019-12-16 13:39:13.210996
 """
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy.dialects import postgresql as pgsql
 from sqlalchemy.sql import text
 
-from ai.backend.manager.models.base import GUID, ForeignKeyIDColumn, IDColumn
+from ai.backend.manager.models.base import GUID, IDColumn
 
 # revision identifiers, used by Alembic.
 revision = "ce209920f654"
