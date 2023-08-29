5d1221084d84d230d42782d036833f4f5a573ed7
Dongjin Park <capospi9903@naver.com>
2023-08-29 12:09:40 +0900

    fix: avoid checking `max_vfolder_count` (#1397)
    Backported-from: main
    Backported-to: 23.03

--- src/ai/backend/manager/api/vfolder.py
+++ src/ai/backend/manager/api/vfolder.py
@@ -459,7 +459,7 @@ async def create(request: web.Request, params: Any) -> web.Response:
         if keypair_resource_policy["max_vfolder_count"] > 0:
             query = sa.select([sa.func.count()]).where(vfolders.c.user == user_uuid)
             result = await conn.scalar(query)
-            if result >= keypair_resource_policy["max_vfolder_count"]:
+            if result >= keypair_resource_policy["max_vfolder_count"] and ownership_type == "user":
                 raise InvalidAPIParameters("You cannot create more vfolders.")
 
         # DEPRECATED: Limit vfolder size quota if it is larger than max_vfolder_size of the resource policy.
