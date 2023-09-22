c5089d750b5521b77f15357330ec7d5a8b4d2c80
Jeongseok Kang <jskang@lablup.com>
2023-09-22 17:22:33 +0900

    fix: Add `pipeline.endpoint` default value to webserver halfstack.conf (#1592)

--- src/ai/backend/manager/config.py
+++ src/ai/backend/manager/config.py
@@ -289,11 +289,6 @@ manager_local_config_iv = (
                     t.Key("aiomonitor-webui-port", default=49100): t.ToInt[1:65535],
                 }
             ).allow_extra("*"),
-            t.Key("pipeline", default=None): t.Null | t.Dict(
-                {
-                    t.Key("event-queue", default=None): t.Null | tx.HostPortPair,
-                },
-            ).allow_extra("*"),
             t.Key("docker-registry"): t.Dict(
                 {  # deprecated in v20.09
                     t.Key("ssl-verify", default=True): t.ToBool,
