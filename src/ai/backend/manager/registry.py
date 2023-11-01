c46b49b8ed0b7a1f05e0accbb62a8fbe2c4e4ebd
Kyujin Cho <kyujin.cho@lablup.com>
2023-10-31 21:07:12 -0700

    fix: inference session not created when service name is too long (#1642)

--- src/ai/backend/manager/registry.py
+++ src/ai/backend/manager/registry.py
@@ -3689,7 +3689,7 @@ async def handle_route_creation(
             )
 
             await context.create_session(
-                f"{endpoint.name}-{uuid.uuid4()}",
+                f"{endpoint.name}-{str(event.route_id)}",
                 endpoint.image_row.name,
                 endpoint.image_row.architecture,
                 UserScope(
