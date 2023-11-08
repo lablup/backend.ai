a2a424c61fd28401a4388eefce4fe60544ba7463
Joongi Kim <joongi@lablup.com>
2023-11-08 12:46:32 +0900

    ci: `pants filter` can be replaced with `pants list`

--- scripts/build-scies.sh
+++ scripts/build-scies.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 pants --tag='-wheel' package '::'
 # NOTE: 'pants run' does not support parallelization
-pants filter --filter-tag-regex='checksum' '::' | xargs -n 1 pants run
+pants list --filter-tag-regex='checksum' '::' | xargs -n 1 pants run
