74a3ef71a822c093f2456903ac3b59476f9ae920
mirageoasis <hyunwoo@lablup.com>
2023-12-06 17:16:14 +0900

    docs: fixed docstring to sync with new feature

--- src/ai/backend/test/cli/__main__.py
+++ src/ai/backend/test/cli/__main__.py
@@ -26,7 +26,7 @@ def main(ctx: click.Context) -> None:
         "allow_extra_args": True,
         "allow_interspersed_args": True,
     },
-    usage_args="[PKGS] [PYTEST_ARGS]",
+    usage_args="[PKGS] [PYTEST_ARGS] --user-file [PATH] --user2-file [PATH] --admin-file [PATH]",
 )
 @click.argument(
     "pkgs",
@@ -42,16 +42,19 @@ def main(ctx: click.Context) -> None:
     "--user-file",
     type=click.Path(exists=True, dir_okay=False, resolve_path=True),
     default="env-local-user-api.sh",
+    help="The path to the environment file for the first user",
 )
 @click.option(
     "--user2-file",
     type=click.Path(exists=True, dir_okay=False, resolve_path=True),
     default="env-local-user2-api.sh",
+    help="The path to the environment file for the second user",
 )
 @click.option(
     "--admin-file",
     type=click.Path(exists=True, dir_okay=False, resolve_path=True),
     default="env-local-admin-api.sh",
+    help="The path to the environment file for the admin user",
 )
 @click.pass_context
 def run_cli(
@@ -68,10 +71,24 @@ def run_cli(
     and forwards all other extra arguments and options (PYTEST_ARGS) to
     the underlying pytest command.
 
+
     \b
-    Available CLI-based integration test package names:
-      admin
-      user
+    Parameters:
+        pkgs: A comma-separated list of package names to test. Available options are 'admin' and 'user'.
+        user_file: Path to the environment file for the 'user' profile. Defaults to 'env-local-user-api.sh'.
+        user2_file: Path to the environment file for the 'user2' profile. Defaults to 'env-local-user2-api.sh'.
+        admin_file: Path to the environment file for the 'admin' profile. Defaults to 'env-local-admin-api.sh'.
+
+    \b
+    Usage:
+        run_cli [PKGS] [PYTEST_ARGS] --user-file [PATH] --user2-file [PATH] --admin-file [PATH]
+
+    \b
+    Examples:
+        run_cli admin,user --some-pytest-option
+        run_cli user --user-file custom-user-env.sh --pytest-arg1 --pytest-arg2
+
+    The command simplifies the process of running CLI-based integration tests for specific packages, allowing customization of the environment and passing additional arguments to pytest.
     """
     pytest_args = ctx.args
     result = subprocess.run(
