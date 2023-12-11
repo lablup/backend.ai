39c32749ed6d676da7a788b548507a8a21afd00c
mirageoasis <hyunwoo@lablup.com>
2023-12-11 22:31:36 +0900

    test: added test for deleting keypair for running session

--- src/ai/backend/test/cli_integration/admin/test_keypair.py
+++ src/ai/backend/test/cli_integration/admin/test_keypair.py
@@ -221,6 +221,115 @@ def test_list_keypair(run_admin: ClientRunnerFunc):
         assert isinstance(keypair_list, list), "List not printed properly!"
 
 
+def test_delete_keypair_on_running_session(run_admin: ClientRunnerFunc):
+    admin_find_command = [
+        "--output=json",
+        "admin",
+        "user",
+        "list",
+        "--filter",
+        'username == "admin"',
+    ]
+    with closing(run_admin(admin_find_command)) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        item = loaded.get("items")
+        assert len(item) == 1, "admin user not found"
+        admin_user_info = item[0]
+        assert admin_user_info.get("username") == "admin"
+        assert admin_user_info.get("email")
+
+    # create keypair
+    keypair_add_arguments = [
+        "--output=json",
+        "admin",
+        "keypair",
+        "add",
+        admin_user_info.get("email"),
+        "default",
+        "--admin",
+    ]
+    with closing(run_admin(keypair_add_arguments)) as p:
+        p.expect(EOF)
+        response = json.loads(p.before.decode())
+        access_key = response.get("access_key")
+        assert access_key
+        assert response.get("ok") is True, "Admin keypair add error"
+
+    # find image
+    with closing(run_admin(["--output=json", "admin", "image", "list"])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        image_list = loaded.get("items")
+        assert isinstance(image_list, list), "Image list not printed properly"
+        assert len(image_list) > 0, "No image found"
+
+        target_image = image_list[0]
+        assert target_image.get("name")
+        assert target_image.get("registry")
+        assert target_image.get("tag")
+
+    image_name = target_image.get("name")
+    image_registry = target_image.get("registry")
+    image_tag = target_image.get("tag")
+
+    session_image_name = f"{image_registry}/{image_name}:{image_tag}"
+
+    # create session
+    session_name = f"test_session_{access_key}"
+    create_session_command = [
+        "--output=json",
+        "session",
+        "create",
+        session_image_name,
+        "-o",
+        access_key,
+        "--enqueue-only",
+        "--name",
+        session_name,
+    ]
+
+    with closing(run_admin(create_session_command)) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        assert "is enqueued for scheduling." in decoded, "Session creation failed"
+
+    with closing(run_admin(["--output=json", "admin", "session", "list"])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        session_list = loaded.get("items")
+        assert isinstance(session_list, list), "Session list not printed properly"
+        assert len(session_list) > 0, "No session found"
+        session_name_list = [session.get("name") for session in session_list]
+        assert session_name in session_name_list, f"Session name {session_name} creation failed"
+
+    # delete keypair
+    with closing(run_admin(["--output=json", "admin", "keypair", "delete", access_key])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        print("delete keypair response", loaded)
+        assert loaded.get("ok") is False, "Keypair should not be deleted"
+
+    # delete session
+    with closing(
+        run_admin(["--output=json", "session", "rm", session_name, "-o", access_key])
+    ) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        assert "Done" in decoded, "Session deletion failed"
+
+    # delete keypair
+    with closing(run_admin(["--output=json", "admin", "keypair", "delete", access_key])) as p:
+        p.expect(EOF)
+        decoded = p.before.decode()
+        loaded = json.loads(decoded)
+        assert loaded.get("ok") is True, "Keypair deletion failed"
+
+
 def get_keypair_from_list(keypairs: list, userid: str) -> dict:
     for keypair in keypairs:
         if keypair.get("user_id", "") == userid:
