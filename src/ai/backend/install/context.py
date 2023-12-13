2733b373ee7bb8ccda874b82d599bf6073b4eba6
Kyujin Cho <kyujin.cho@lablup.com>
2023-12-13 09:02:29 -0600

    reflect changes to installer

--- src/ai/backend/install/context.py
+++ src/ai/backend/install/context.py
@@ -509,6 +509,7 @@ class Context(metaclass=ABCMeta):
                 if halfstack.redis_password:
                     redis_table["password"] = halfstack.redis_password
             data["session"]["redis"] = redis_table  # type: ignore
+            data["ui"]["menu_inactivelist"] = ",".join(service.webui_menu_inactivelist)  # type: ignore
         with conf_path.open("w") as fp:
             tomlkit.dump(data, fp)
 
@@ -743,6 +744,7 @@ class DevContext(Context):
             webserver_addr=ServerAddr(HostPortPair("127.0.0.1", 8090)),
             webserver_ipc_base_path="ipc/webserver",
             webserver_var_base_path="var/webserver",
+            webui_menu_inactivelist=["statistics"],
             manager_addr=ServerAddr(HostPortPair("127.0.0.1", 8091)),
             storage_proxy_manager_auth_key=secrets.token_hex(32),
             manager_ipc_base_path="ipc/manager",
@@ -828,6 +830,7 @@ class PackageContext(Context):
             webserver_addr=ServerAddr(HostPortPair("127.0.0.1", 8090)),
             webserver_ipc_base_path="ipc/webserver",
             webserver_var_base_path="var/webserver",
+            webui_menu_inactivelist=["statistics"],
             manager_addr=ServerAddr(HostPortPair("127.0.0.1", 8091)),
             storage_proxy_manager_auth_key=secrets.token_urlsafe(32),
             manager_ipc_base_path="ipc/manager",
