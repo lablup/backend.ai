a2368408fed890d5e4ace2cd3a49a771e0c3475e
Sanghun Lee <sanghun@lablup.com>
2024-01-11 15:37:57 +0900

    update alembic migration

--- src/ai/backend/manager/models/alembic/versions/c5ed277b7f7b_change_main_access_key_ondelete_to_set_.py
+++ src/ai/backend/manager/models/alembic/versions/c5ed277b7f7b_change_main_access_key_ondelete_to_set_.py
@@ -1,16 +1,16 @@
-"""change_main_access_key_ondelete
+"""change_main_access_key_ondelete_to_set_null
 
-Revision ID: 1107ea592571
-Revises: d3f8c74bf148
-Create Date: 2023-12-11 20:54:36.573832
+Revision ID: c5ed277b7f7b
+Revises: 8b2ec7e3d22a
+Create Date: 2024-01-11 15:37:24.097596
 
 """
 
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "1107ea592571"
-down_revision = "d3f8c74bf148"
+revision = "c5ed277b7f7b"
+down_revision = "8b2ec7e3d22a"
 branch_labels = None
 depends_on = None
 
