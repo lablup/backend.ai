603a1a7fde2dfeb3ffc19b82e10adbecab3c3c20
Sanghun Lee <sanghun@lablup.com>
2024-02-01 20:18:17 +0900

    fix: drop association tables existing primary key

--- src/ai/backend/manager/models/alembic/versions/caf54fcc17ab_add_id_columns_to_association_tables.py
+++ src/ai/backend/manager/models/alembic/versions/caf54fcc17ab_add_id_columns_to_association_tables.py
@@ -36,6 +36,18 @@ def upgrade():
         sa.Column("id", GUID(), server_default=sa.text("uuid_generate_v4()"), nullable=False),
     )
 
+    def drop_existing_pk(idx: str, table: str):
+        try:
+            op.drop_constraint(idx, table, type_="primary")
+        except sa.exc.ProgrammingError:
+            # Skip dropping if the table has no primary key
+            pass
+
+    drop_existing_pk("pk_association_groups_users", "association_groups_users")
+    drop_existing_pk("pk_sgroups_for_domains", "sgroups_for_domains")
+    drop_existing_pk("pk_sgroups_for_groups", "sgroups_for_groups")
+    drop_existing_pk("pk_sgroups_for_keypairs", "sgroups_for_keypairs")
+
     op.create_primary_key("pk_association_groups_users", "association_groups_users", ["id"])
     op.create_primary_key("pk_sgroups_for_domains", "sgroups_for_domains", ["id"])
     op.create_primary_key("pk_sgroups_for_groups", "sgroups_for_groups", ["id"])
@@ -52,3 +64,14 @@ def downgrade():
     op.drop_column("sgroups_for_groups", "id")
     op.drop_column("sgroups_for_domains", "id")
     op.drop_column("association_groups_users", "id")
+
+    op.create_primary_key(
+        "pk_association_groups_users", "association_groups_users", ["user_id", "group_id"]
+    )
+    op.create_primary_key(
+        "pk_sgroups_for_domains", "sgroups_for_domains", ["scaling_group", "domain"]
+    )
+    op.create_primary_key("pk_sgroups_for_groups", "sgroups_for_groups", ["scaling_group", "group"])
+    op.create_primary_key(
+        "pk_sgroups_for_keypairs", "sgroups_for_keypairs", ["scaling_group", "access_key"]
+    )
