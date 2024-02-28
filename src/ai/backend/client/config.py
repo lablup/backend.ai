714c1191c4634bcd2c9d21ba3fc764f40a5a1f36
Joongi Kim <joongi@lablup.com>
2024-02-28 18:48:05 +0900

    hotfix: A typo in #1938

--- src/ai/backend/client/config.py
+++ src/ai/backend/client/config.py
@@ -39,7 +39,7 @@ class Undefined(enum.Enum):
 _config = None
 _undefined = Undefined.token
 
-API_VERSION = (8, "20240615")
+API_VERSION = (8, "20240315")
 MIN_API_VERSION = (7, "20230615")
 
 DEFAULT_CHUNK_SIZE = 16 * (2**20)  # 16 MiB
