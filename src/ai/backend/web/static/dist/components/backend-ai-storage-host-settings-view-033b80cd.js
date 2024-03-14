1814813df4481b7039cbf9bd82ad719d3e4021cd
Joongi Kim <joongi@lablup.com>
2024-03-14 14:51:00 +0900

    deps: Update the prebuilt webui to latest (24.03.0-beta.1)

--- src/ai/backend/web/static/dist/components/backend-ai-storage-host-settings-view-033b80cd.js
+++
@@ -1,6 +0,0 @@
-import{_ as t,e as a,B as e,c as s,I as i,a as o,i as n,x as c,j as r,k as d}from"./backend-ai-webui-7bb27bb8.js";let h=class extends e{static get styles(){return[s,i,o,n``]}async _viewStateChanged(t){await this.updateComplete}render(){return c`
-      <backend-ai-react-storage-host-settings
-        value="${window.location.pathname.split("/")[2]}"
-        @moveTo="${t=>{const a=t.detail.path;globalThis.history.pushState({},"",a),r.dispatch(d(decodeURIComponent(a),{}))}}"
-      ></backend-ai-react-storage-host-settings>
-    `}};h=t([a("backend-ai-storage-host-settings-view")],h);
