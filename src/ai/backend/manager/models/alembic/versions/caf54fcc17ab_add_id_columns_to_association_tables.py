238bac8fdf270160aaea6fe17077515b4e89576b
Sanghun Lee <sanghun@lablup.com>
2024-03-25 08:40:25 +0900

    fix: early check before drop pk and add missing args in table

--- src/ai/backend/manager/models/alembic/versions/caf54fcc17ab_add_id_columns_to_association_tables.py
+++ src/ai/backend/manager/models/alembic/versions/caf54fcc17ab_add_id_columns_to_association_tables.py
@@ -9,7 +9,7 @@ Create Date: 2024-01-03 21:39:50.558724
 import sqlalchemy as sa
 from alembic import op
 
-from ai.backend.manager.models.base import GUID
+from ai.backend.manager.models.base import GUID, metadata
 
 # revision identifiers, used by Alembic.
 revision = "caf54fcc17ab"
@@ -36,17 +36,112 @@ def upgrade():
         sa.Column("id", GUID(), server_default=sa.text("uuid_generate_v4()"), nullable=False),
     )
 
-    def drop_existing_pk(idx: str, table: str):
-        try:
-            op.drop_constraint(idx, table, type_="primary")
-        except sa.exc.ProgrammingError:
-            # Skip dropping if the table has no primary key
-            pass
+    association_groups_users = sa.Table(
+        "association_groups_users",
+        metadata,
+        sa.Column(
+            "id",
+            GUID,
+            nullable=False,
+        ),
+        sa.Column(
+            "user_id",
+            GUID,
+            sa.ForeignKey("users.uuid", onupdate="CASCADE", ondelete="CASCADE"),
+            nullable=False,
+        ),
+        sa.Column(
+            "group_id",
+            GUID,
+            sa.ForeignKey("groups.id", onupdate="CASCADE", ondelete="CASCADE"),
+            nullable=False,
+        ),
+        sa.UniqueConstraint("user_id", "group_id", name="uq_association_user_id_group_id"),
+        extend_existing=True,
+    )
 
-    drop_existing_pk("pk_association_groups_users", "association_groups_users")
-    drop_existing_pk("pk_sgroups_for_domains", "sgroups_for_domains")
-    drop_existing_pk("pk_sgroups_for_groups", "sgroups_for_groups")
-    drop_existing_pk("pk_sgroups_for_keypairs", "sgroups_for_keypairs")
+    sgroups_for_domains = sa.Table(
+        "sgroups_for_domains",
+        metadata,
+        sa.Column(
+            "id",
+            GUID,
+            nullable=False,
+        ),
+        sa.Column(
+            "scaling_group",
+            sa.ForeignKey("scaling_groups.name", onupdate="CASCADE", ondelete="CASCADE"),
+            index=True,
+            nullable=False,
+        ),
+        sa.Column(
+            "domain",
+            sa.ForeignKey("domains.name", onupdate="CASCADE", ondelete="CASCADE"),
+            index=True,
+            nullable=False,
+        ),
+        sa.UniqueConstraint("scaling_group", "domain", name="uq_sgroup_domain"),
+        extend_existing=True,
+    )
+
+    sgroups_for_groups = sa.Table(
+        "sgroups_for_groups",
+        metadata,
+        sa.Column(
+            "id",
+            GUID,
+            nullable=False,
+        ),
+        sa.Column(
+            "scaling_group",
+            sa.ForeignKey("scaling_groups.name", onupdate="CASCADE", ondelete="CASCADE"),
+            index=True,
+            nullable=False,
+        ),
+        sa.Column(
+            "group",
+            sa.ForeignKey("groups.id", onupdate="CASCADE", ondelete="CASCADE"),
+            index=True,
+            nullable=False,
+        ),
+        sa.UniqueConstraint("scaling_group", "group", name="uq_sgroup_ugroup"),
+        extend_existing=True,
+    )
+
+    sgroups_for_keypairs = sa.Table(
+        "sgroups_for_keypairs",
+        metadata,
+        sa.Column(
+            "id",
+            GUID,
+            nullable=False,
+        ),
+        sa.Column(
+            "scaling_group",
+            sa.ForeignKey("scaling_groups.name", onupdate="CASCADE", ondelete="CASCADE"),
+            index=True,
+            nullable=False,
+        ),
+        sa.Column(
+            "access_key",
+            sa.ForeignKey("keypairs.access_key", onupdate="CASCADE", ondelete="CASCADE"),
+            index=True,
+            nullable=False,
+        ),
+        sa.UniqueConstraint("scaling_group", "access_key", name="uq_sgroup_akey"),
+        extend_existing=True,
+    )
+
+    def drop_existing_pk(table: sa.sql.schema.Table) -> None:
+        for const in table.constraints:
+            if isinstance(const, sa.PrimaryKeyConstraint):
+                op.drop_constraint(const.name, table.name, type_="primary")
+                break
+
+    drop_existing_pk(association_groups_users)
+    drop_existing_pk(sgroups_for_domains)
+    drop_existing_pk(sgroups_for_groups)
+    drop_existing_pk(sgroups_for_keypairs)
 
     op.create_primary_key("pk_association_groups_users", "association_groups_users", ["id"])
     op.create_primary_key("pk_sgroups_for_domains", "sgroups_for_domains", ["id"])
