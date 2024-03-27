278bc06cf2e4794ed412953e9d1ec33d5b193717
Kyujin Cho <kyujin.cho@lablup.com>
2024-03-27 09:49:01 +0900

    utilize DROP CONSTRAINT IF EXISTS

--- src/ai/backend/manager/models/alembic/versions/caf54fcc17ab_add_id_columns_to_association_tables.py
+++ src/ai/backend/manager/models/alembic/versions/caf54fcc17ab_add_id_columns_to_association_tables.py
@@ -1,5 +1,7 @@
 """add_id_columns_to_association_tables
 
+Adds ID column to select tables and replaces its Primary Key from pair of data columns to newly created ID column
+
 Revision ID: caf54fcc17ab
 Revises: 8b2ec7e3d22a
 Create Date: 2024-01-03 21:39:50.558724
@@ -9,7 +11,7 @@ Create Date: 2024-01-03 21:39:50.558724
 import sqlalchemy as sa
 from alembic import op
 
-from ai.backend.manager.models.base import GUID, metadata
+from ai.backend.manager.models.base import GUID
 
 # revision identifiers, used by Alembic.
 revision = "caf54fcc17ab"
@@ -36,88 +38,19 @@ def upgrade():
         sa.Column("id", GUID(), server_default=sa.text("uuid_generate_v4()"), nullable=False),
     )
 
-    association_groups_users = sa.Table(
-        "association_groups_users",
-        metadata,
-        sa.Column(
-            "user_id",
-            GUID,
-            sa.ForeignKey("users.uuid", onupdate="CASCADE", ondelete="CASCADE"),
-            nullable=False,
-        ),
-        sa.Column(
-            "group_id",
-            GUID,
-            sa.ForeignKey("groups.id", onupdate="CASCADE", ondelete="CASCADE"),
-            nullable=False,
-        ),
-        extend_existing=True,
-    )
+    def drop_existing_pk(table: str) -> None:
+        try:
+            # based on age of our constraint naming convention (ai.backend.manager.models.base)
+            # it is safe to assume that every primary key has pk_{table} as its name
+            op.execute(f"ALTER TABLE {table} DROP CONSTRAINT IF EXISTS pk_{table}")
+        except sa.exc.ProgrammingError:
+            # Skip dropping if the table has no primary key
+            pass
 
-    sgroups_for_domains = sa.Table(
-        "sgroups_for_domains",
-        metadata,
-        sa.Column(
-            "scaling_group",
-            sa.ForeignKey("scaling_groups.name", onupdate="CASCADE", ondelete="CASCADE"),
-            index=True,
-            nullable=False,
-        ),
-        sa.Column(
-            "domain",
-            sa.ForeignKey("domains.name", onupdate="CASCADE", ondelete="CASCADE"),
-            index=True,
-            nullable=False,
-        ),
-        extend_existing=True,
-    )
-
-    sgroups_for_groups = sa.Table(
-        "sgroups_for_groups",
-        metadata,
-        sa.Column(
-            "scaling_group",
-            sa.ForeignKey("scaling_groups.name", onupdate="CASCADE", ondelete="CASCADE"),
-            index=True,
-            nullable=False,
-        ),
-        sa.Column(
-            "group",
-            sa.ForeignKey("groups.id", onupdate="CASCADE", ondelete="CASCADE"),
-            index=True,
-            nullable=False,
-        ),
-        extend_existing=True,
-    )
-
-    sgroups_for_keypairs = sa.Table(
-        "sgroups_for_keypairs",
-        metadata,
-        sa.Column(
-            "scaling_group",
-            sa.ForeignKey("scaling_groups.name", onupdate="CASCADE", ondelete="CASCADE"),
-            index=True,
-            nullable=False,
-        ),
-        sa.Column(
-            "access_key",
-            sa.ForeignKey("keypairs.access_key", onupdate="CASCADE", ondelete="CASCADE"),
-            index=True,
-            nullable=False,
-        ),
-        extend_existing=True,
-    )
-
-    def drop_existing_pk(table: sa.sql.schema.Table) -> None:
-        for const in table.constraints:
-            if isinstance(const, sa.PrimaryKeyConstraint):
-                op.drop_constraint(const.name, table.name, type_="primary")
-                break
-
-    drop_existing_pk(association_groups_users)
-    drop_existing_pk(sgroups_for_domains)
-    drop_existing_pk(sgroups_for_groups)
-    drop_existing_pk(sgroups_for_keypairs)
+    drop_existing_pk("association_groups_users")
+    drop_existing_pk("sgroups_for_domains")
+    drop_existing_pk("sgroups_for_groups")
+    drop_existing_pk("sgroups_for_keypairs")
 
     op.create_primary_key("pk_association_groups_users", "association_groups_users", ["id"])
     op.create_primary_key("pk_sgroups_for_domains", "sgroups_for_domains", ["id"])
