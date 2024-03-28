2cfb57daa725ebee15f4f75a004bd262c7449a15
Sanghun Lee <sanghun@lablup.com>
2024-03-28 14:21:59 +0900

    add comment and update news fragment

--- src/ai/backend/manager/registry.py
+++ src/ai/backend/manager/registry.py
@@ -1124,6 +1124,7 @@ class AgentRegistry:
                         )
 
             # Shared memory.
+            # Do not including the shared memory when comparing the memory slot with image requiring resource slot.
             shmem = BinarySize.from_str(
                 resource_opts.get("shmem")
                 or labels.get("ai.backend.resource.preferred.shmem")
