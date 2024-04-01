1e1312bda44d8f4c010af37ed59d5821b519baa2
Sanghun Lee <sanghun@lablup.com>
2024-04-01 18:50:43 +0900

    add field description

--- src/ai/backend/manager/models/session.py
+++ src/ai/backend/manager/models/session.py
@@ -1191,7 +1191,7 @@ class ComputeSession(graphene.ObjectType):
     vfolder_mounts = graphene.List(lambda: graphene.String)
     occupying_slots = graphene.JSONString()
     occupied_slots = graphene.JSONString()  # legacy
-    requested_slots = graphene.JSONString()
+    requested_slots = graphene.JSONString(description="Added in 24.03.0")
 
     # statistics
     num_queries = BigInt()
