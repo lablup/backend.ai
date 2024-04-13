23b238a71c4ece2ecf21eaf7fb9ff14a2e36a1ae
Sanghun Lee <sanghun@lablup.com>
2024-04-13 15:41:08 +0900

    remove unused config schema

--- src/ai/backend/agent/dummy/config.py
+++ src/ai/backend/agent/dummy/config.py
@@ -1,26 +1,10 @@
 import trafaret as t
 
 from ai.backend.common import validators as tx
-from ai.backend.common.types import SlotTypes
-
-from .defs import AllocationModes
 
 RandomRange = t.Tuple(t.ToFloat, t.ToFloat)
 core_idx = {0, 1, 2, 3, 4}
 
-
-dummy_device_config = t.Dict({
-    t.Key("device-id"): t.String(),
-    t.Key("device-name"): t.String(),
-    t.Key("slot-type", default=SlotTypes.COUNT): tx.Enum(SlotTypes),
-    t.Key("allocation-mode", default=AllocationModes.DISCRETE): tx.Enum(AllocationModes),
-    t.Key("hw-location", default="hw-location"): t.String(),
-    t.Key("memory-size", default=10): t.ToInt(),
-    t.Key("processing-units", default=1): t.ToInt(),
-    t.Key("numa-node", default=None): t.Null | t.ToInt(),
-    t.Key("model-name", default=None): t.Null | t.String(),
-    t.Key("mother-uuid", default=None): t.Null | t.String(),
-})
 
 dummy_local_config = t.Dict({
     t.Key("agent"): t.Dict({
