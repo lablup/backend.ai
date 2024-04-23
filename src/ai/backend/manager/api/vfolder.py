65cd119289410130baac221d86744154824a3371
Sanghun Lee <sanghun@lablup.com>
2024-04-23 22:11:15 +0900

    revert import format

--- src/ai/backend/manager/api/vfolder.py
+++ src/ai/backend/manager/api/vfolder.py
@@ -34,7 +34,11 @@ import attrs
 import sqlalchemy as sa
 import trafaret as t
 from aiohttp import web
-from pydantic import AliasChoices, BaseModel, Field
+from pydantic import (
+    AliasChoices,
+    BaseModel,
+    Field,
+)
 from sqlalchemy.orm import load_only, selectinload
 
 from ai.backend.common import msgpack, redis_helper
@@ -113,7 +117,12 @@ from .exceptions import (
 )
 from .manager import ALL_ALLOWED, READ_ALLOWED, server_status_required
 from .resource import get_watcher_info
-from .utils import BaseResponseModel, check_api_params, get_user_scopes, pydantic_params_api_handler
+from .utils import (
+    BaseResponseModel,
+    check_api_params,
+    get_user_scopes,
+    pydantic_params_api_handler,
+)
 
 if TYPE_CHECKING:
     from .context import RootContext
