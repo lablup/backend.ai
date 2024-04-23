bd83825708d22aec792c6bcff52ab9664553e396
achimnol <555156+achimnol@users.noreply.github.com>
2024-04-23 17:34:29 +0000

    feat: Support encrypted requests in the client SDK (#493)
    This is a follow-up of #484.
    Let's automatically encrypt the payloads for user authentication and password change requests if the manager/webserver API endpoint uses the plain HTTP.

--- src/ai/backend/client/auth.py
+++ src/ai/backend/client/auth.py
@@ -1,10 +1,14 @@
+import base64
 import enum
 import hashlib
 import hmac
+import secrets
 from datetime import datetime
 from typing import Mapping, Tuple
 
 import attrs
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 from yarl import URL
 
 __all__ = (
@@ -12,6 +16,11 @@ __all__ = (
     "AuthTokenTypes",
     "generate_signature",
 )
+
+
+_iv_dict = [
+    bytes([char]) for char in b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+]
 
 
 class AuthTokenTypes(enum.Enum):
@@ -67,3 +76,20 @@ def generate_signature(
         ),
     }
     return headers, signature
+
+
+def encrypt_payload(endpoint: str, body: bytes) -> bytes:
+    iv = b"".join(secrets.choice(_iv_dict) for _ in range(16))
+    encoded_endpoint = base64.b64encode(endpoint.encode("utf-8"))
+    key = (encoded_endpoint + iv + iv)[:32]
+    crypt = AES.new(key, AES.MODE_CBC, iv)
+    result = base64.b64encode(crypt.encrypt(pad(body, 16)))
+    return iv + b":" + result
+
+
+def decrypt_payload(endpoint: str, payload: bytes) -> bytes:
+    iv, real_payload = payload.split(b":")
+    key = (base64.b64encode(endpoint.encode("ascii")) + iv + iv)[:32]
+    crypt = AES.new(key, AES.MODE_CBC, iv)
+    b64p = base64.b64decode(real_payload)
+    return unpad(crypt.decrypt(bytes(b64p)), 16)
