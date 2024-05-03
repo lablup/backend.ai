bac4846350f936eade6df9cea0c8838a6dafc907
Kyujin Cho <kyujin.cho@lablup.com>
2024-05-03 11:23:47 +0900

    Implement manager network plugin architecture

---
+++ src/ai/backend/manager/network/overlay.py
@@ -0,0 +1,71 @@
+
+import asyncio
+import uuid
+from typing import Any, Mapping
+
+import aiodocker
+import trafaret as t
+
+from ..plugin.network import AbstractNetworkManagerPlugin
+
+plugin_config_iv = t.Dict({
+    t.Key("mtu", default=1500): t.Null | t.ToInt,
+}).allow_extra("*")
+
+
+class OverlayNetworkError(RuntimeError):
+    pass
+
+
+class OverlayNetworkPlugin(AbstractNetworkManagerPlugin):
+    docker: aiodocker.Docker
+    def __init__(self, plugin_config: Mapping[str, Any], local_config: Mapping[str, Any]) -> None:
+        super().__init__(plugin_config, local_config)
+
+        plugin_config_iv.check(plugin_config)
+
+    async def init(self, context: Any = None) -> asyncio.Coroutine[Any, Any, None]:
+        await super().init(context)
+        self.docker = aiodocker.Docker()
+
+        info = await self.docker.system.info()
+        if info["Swarm"]["LocalNodeState"] != "active":
+            raise OverlayNetworkError("Docker swarm not enabled on system!")
+
+    async def cleanup(self) -> None:
+        await self.docker.close()
+        await super().cleanup()
+
+    async def create_network(self, session: SessionRow, *, options: dict[str, Any] = {}) -> str:
+        ident = identifier or f"{uuid.uuid4()}-nw"
+        network_name = f"bai-multinode-{ident}"
+        mtu: int | None = self.plugin_config["mtu"]
+
+        # Overlay networks can only be created at the Swarm manager.
+        create_options = {
+            "Name": network_name,
+            "Driver": "overlay",
+            "Attachable": True,
+            "Labels": {
+                "ai.backend.cluster-network": "1",
+            },
+            "Options": {},
+        }
+        if mtu:
+            create_options["Options"] = {"com.docker.network.driver.mtu": str(mtu)}
+        await self.docker.networks.create(create_options)
+
+        return {"mode": "overlay", "network_name": network_name, "network_id": str(session.id)}
+
+    async def destroy_network(self, session: SessionRow) -> None:
+        try:
+            network_name = f"bai-multinode-{session.id}"
+            await asyncio.sleep(2.0)
+            network = await self.docker.networks.get(network_name)
+            await network.delete()
+        except aiodocker.DockerError as e:
+            if e.status == 404:
+                # It may have been auto-destructed when the last container was detached.
+                pass
+            else:
+                raise
---
+++ src/ai/backend/manager/plugin/network.py
@@ -0,0 +1,31 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any
+
+from ai.backend.common.plugin import AbstractPlugin, BasePluginContext
+
+
+class AbstractNetworkManagerPlugin(AbstractPlugin, metaclass=ABCMeta):
+    @abstractmethod
+    async def create_network(self, session: SessionRow, *, options: dict[str, Any] = {}) -> str:
+        """
+        Creates a cross-container network and returns its name.
+        :param identifier: Optional network identifier. If not provided, a random identifier will be generated.
+        :param options: Network options.
+        :return: NetworkInfo object which contains network_id and options.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def destroy_network(
+        self,
+        network_name: str,
+        session: SessionRow,
+    ) -> None:
+        """
+        Destroys network which were used to bind containers.
+        """
+        raise NotImplementedError
+
+
+class NetworkPluginContext(BasePluginContext[AbstractNetworkManagerPlugin]):
+    plugin_group = "backendai_network_manager_v1"
