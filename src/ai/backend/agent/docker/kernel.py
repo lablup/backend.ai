771ea66d25993f5462e6e134a1db72fb17371dba
Gyubong Lee <jopemachine@naver.com>
2024-05-21 05:34:34 +0000

    fix: Replace alpine with busybox

--- src/ai/backend/agent/docker/kernel.py
+++ src/ai/backend/agent/docker/kernel.py
@@ -454,7 +454,7 @@ async def prepare_sudo_session_enabler() -> None:
                 f"{volume_name}:/mnt",
                 "-v",
                 f"{sudoer_filepath}:/tmp/work",
-                "alpine",
+                "busybox",
                 "sh",
                 "-c",
                 "cp /tmp/work /mnt/work",
