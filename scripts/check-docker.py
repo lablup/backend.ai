9b4722ffdce5d1b17dcd8594f831a0ebd32b6fa0
agatha197 <28584164+agatha197@users.noreply.github.com>
2024-05-27 03:35:46 +0000

    fix: `check-docker.py` script not working (#2174)
    Backported-from: main (24.09)
    Backported-to: 23.09
    Backport-of: 2174

--- scripts/check-docker.py
+++ scripts/check-docker.py
@@ -1,4 +1,5 @@
 import argparse
+import asyncio
 import base64
 import functools
 import hashlib
@@ -7,10 +8,8 @@ import re
 import subprocess
 import sys
 from pathlib import Path
-from urllib.parse import quote
 
-import requests
-import requests_unixsocket
+import aiohttp
 
 log = functools.partial(print, file=sys.stderr)
 run = subprocess.run
@@ -42,19 +41,22 @@ def simple_hash(data: bytes) -> str:
     return base64.b64encode(h.digest()[:12], altchars=b"._").decode()
 
 
-def detect_snap_docker():
+async def detect_snap_docker():
     if not Path("/run/snapd.socket").is_socket():
         return None
-    with requests.get("http+unix://%2Frun%2Fsnapd.socket/v2/snaps?names=docker") as r:
-        if r.status_code != 200:
-            raise RuntimeError("Failed to query Snapd package information")
-        response_data = r.json()
-        for pkg_data in response_data["result"]:
-            if pkg_data["name"] == "docker":
-                return pkg_data["version"]
+    async with aiohttp.ClientSession(connector=aiohttp.UnixConnector("/run/snapd.socket")) as sess:
+        async with sess.get("http://localhost/v2/snaps?names=docker") as r:
+            try:
+                r.raise_for_status()
+            except:
+                raise RuntimeError("Failed to query Snapd package information")
+            response_data = await r.json()
+            for pkg_data in response_data["result"]:
+                if pkg_data["name"] == "docker":
+                    return pkg_data["version"]
 
 
-def detect_system_docker():
+async def detect_system_docker():
     sock_paths = [
         Path("/run/docker.sock"),  # Linux default
         Path("/var/run/docker.sock"),  # macOS default
@@ -68,12 +70,14 @@ def detect_system_docker():
             break
     else:
         return None
-    encoded_sock_path = quote(bytes(sock_path), safe="")
-    with requests.get(f"http+unix://{encoded_sock_path}/version") as r:
-        if r.status_code != 200:
-            raise RuntimeError("Failed to query the Docker daemon API")
-        response_data = r.json()
-        return response_data["Version"]
+    async with aiohttp.ClientSession(connector=aiohttp.UnixConnector(sock_path.as_posix())) as sess:
+        async with sess.get("http://localhost/version") as r:
+            try:
+                r.raise_for_status()
+            except:
+                raise RuntimeError("Failed to query Snapd package information")
+            response_data = await r.json()
+            return response_data["Version"]
 
 
 def fail_with_snap_docker_refresh_request():
@@ -94,15 +98,13 @@ def fail_with_compose_install_request():
     sys.exit(1)
 
 
-def main():
-    requests_unixsocket.monkeypatch()
-
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--get-preferred-pants-local-exec-root", action="store_true", default=False)
     args = parser.parse_args()
 
     if args.get_preferred_pants_local_exec_root:
-        docker_version = detect_snap_docker()
+        docker_version = await detect_snap_docker()
         build_root_path = get_build_root()
         build_root_name = build_root_path.name
         build_root_hash = simple_hash(os.fsencode(build_root_path))
@@ -114,13 +116,13 @@ def main():
             print(f"/tmp/{build_root_name}-{build_root_hash}-pants")
         return
 
-    docker_version = detect_snap_docker()
+    docker_version = await detect_snap_docker()
     if docker_version is not None:
         log(f"Detected Docker installation: Snap package ({docker_version})")
         if parse_version(docker_version) < (20, 10, 15):
             fail_with_snap_docker_refresh_request()
     else:
-        docker_version = detect_system_docker()
+        docker_version = await detect_system_docker()
         if docker_version is not None:
             log(f"Detected Docker installation: System package ({docker_version})")
         else:
@@ -145,4 +147,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
