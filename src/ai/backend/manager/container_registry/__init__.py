e196a14fa069b4ca2d3bab14b41850ef47c4e4ea
Gyubong Lee <jopemachine@naver.com>
2024-06-26 07:37:57 +0000

    feat: Support GitHub Container Registry (WIP)

--- src/ai/backend/manager/container_registry/__init__.py
+++ src/ai/backend/manager/container_registry/__init__.py
@@ -28,6 +28,10 @@ def get_container_registry_cls(registry_info: Mapping[str, Any]) -> Type[BaseCon
         from .harbor import HarborRegistry_v2
 
         cr_cls = HarborRegistry_v2
+    elif registry_type == "github":
+        from .github import GitHubRegistry_v2
+
+        cr_cls = GitHubRegistry_v2
     elif registry_type == "local":
         from .local import LocalRegistry
 
