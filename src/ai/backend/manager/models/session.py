327d2ade7c68822c73626952b4882c16a0073efc
octodog <mu001@lablup.com>
2024-07-14 01:21:01 +0900

    fix: Add missing `commit_session_to_file` to `OP_EXC` (#2127) (#2446)
    Co-authored-by: Gyubong Lee <jopemachine@naver.com>
    Co-authored-by: Joongi Kim <joongi@lablup.com>

--- src/ai/backend/manager/models/session.py
+++ src/ai/backend/manager/models/session.py
@@ -177,6 +177,7 @@ OP_EXC = {
     "get_logs_from_agent": KernelExecutionFailed,
     "refresh_session": KernelExecutionFailed,
     "commit_session": KernelExecutionFailed,
+    "commit_session_to_file": KernelExecutionFailed,
     "trigger_batch_execution": KernelExecutionFailed,
 }
 
