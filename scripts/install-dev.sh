3b452bcfed5981b7095246149789133e26e8040b
pilmo kim <68311908+why-arong@users.noreply.github.com>
2024-07-25 13:43:41 +0900

    remove whitespace

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -692,7 +692,7 @@ read -r -d '' pyenv_init_script <<"EOS"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
-eval "$(pyenv init -)" 
+eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 EOS
 
