e3d3a4ab1c3f6b2addbe5a32ef5f7e592e28fbca
Sanghun Lee <sanghun@lablup.com>
2024-07-26 20:01:54 +0900

    fix: Init Redis connection pool with appropriate connection opts (#2574)

--- src/ai/backend/common/redis_helper.py
+++ src/ai/backend/common/redis_helper.py
@@ -529,6 +529,7 @@ def get_redis_object(
                 connection_pool=ConnectionPool.from_url(
                     str(url),
                     **conn_pool_opts,
+                    **conn_opts,
                 ),
                 **conn_opts,
                 auto_close_connection_pool=True,
