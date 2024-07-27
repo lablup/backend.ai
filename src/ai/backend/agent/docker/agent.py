f830b08a19ca8964f6a270bea4c12f0b341c236c
Gyubong Lee <jopemachine@naver.com>
2024-07-27 03:09:11 +0000

    fix: Silent failure of DockerAgent.push_image

--- src/ai/backend/agent/docker/agent.py
+++ src/ai/backend/agent/docker/agent.py
@@ -1487,7 +1487,13 @@ class DockerAgent(AbstractAgent[DockerKernel, DockerKernelCreationContext]):
             }
 
         async with closing_async(Docker()) as docker:
-            await docker.images.push(image_ref.canonical, auth=auth_config)
+            result = await docker.images.push(image_ref.canonical, auth=auth_config)
+
+            # Why is this list? It contradicts the API documentation.
+            result_ = cast(list, result)
+
+            if error := result_[-1].get("error"):
+                raise RuntimeError(f"Failed to push image: {error}")
 
     async def pull_image(
         self,
