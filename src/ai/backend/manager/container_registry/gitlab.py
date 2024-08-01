4f790d3701ff92737e69cb0fd15ed03a4e13ca64
Gyubong Lee <jopemachine@naver.com>
2024-08-01 06:42:27 +0000

    feat: Support multiple projects

--- src/ai/backend/manager/container_registry/gitlab.py
+++ src/ai/backend/manager/container_registry/gitlab.py
@@ -21,33 +21,36 @@ class GitLabRegistry_v2(BaseContainerRegistry):
             self.registry_info["gitlab_project"],
         )
 
-        encoded_project_id = urllib.parse.quote(f"{name}/{gitlab_project}", safe="")
-        repo_list_url = (
-            f"https://gitlab.com/api/v4/projects/{encoded_project_id}/registry/repositories"
-        )
+        projects = gitlab_project.split(",")
 
-        headers = {
-            "Accept": "application/json",
-            "PRIVATE-TOKEN": access_token,
-        }
-        page = 1
+        for project in projects:
+            encoded_project_id = urllib.parse.quote(f"{name}/{project}", safe="")
+            repo_list_url = (
+                f"https://gitlab.com/api/v4/projects/{encoded_project_id}/registry/repositories"
+            )
 
-        while True:
-            async with sess.get(
-                repo_list_url,
-                headers=headers,
-                params={"per_page": 30, "page": page},
-            ) as response:
-                if response.status == 200:
-                    data = await response.json()
+            headers = {
+                "Accept": "application/json",
+                "PRIVATE-TOKEN": access_token,
+            }
+            page = 1
 
-                    for repo in data:
-                        yield repo["path"]
-                    if "next" in response.headers.get("Link", ""):
-                        page += 1
+            while True:
+                async with sess.get(
+                    repo_list_url,
+                    headers=headers,
+                    params={"per_page": 30, "page": page},
+                ) as response:
+                    if response.status == 200:
+                        data = await response.json()
+
+                        for repo in data:
+                            yield repo["path"]
+                        if "next" in response.headers.get("Link", ""):
+                            page += 1
+                        else:
+                            break
                     else:
-                        break
-                else:
-                    raise RuntimeError(
-                        f"Failed to fetch repositories! {response.status} error occurred."
-                    )
+                        raise RuntimeError(
+                            f"Failed to fetch repositories for project {project}! {response.status} error occurred."
+                        )
