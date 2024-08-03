54f830f7b1291d81d3aebf1e7acce3e14bcab349
Joongi Kim <joongi@lablup.com>
2024-08-03 18:34:38 +0900

    fix: Run full-check if GitHub CLI is not installed, as a graceful fallback

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -9,30 +9,30 @@ fi
 CURRENT_COMMIT=$(git rev-parse --short HEAD)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-# Get the base branch name from GitHub.
 if ! command -v gh &> /dev/null; then
-  echo "GitHub CLI (gh) is not installed. Please install it and authenticate."
-  exit 1
-fi
-if gh pr view "$CURRENT_BRANCH" &> /dev/null; then
-  # Get the base branch name if we are on a pull request.
-  BASE_BRANCH=$(gh pr view "$CURRENT_BRANCH" --json baseRefName -q '.baseRefName')
+  echo "GitHub CLI (gh) is not installed. Since we cannot determine the base branch, running the full check before push."
+  pants tailor --check update-build-files --check ::
+  pants lint check ::
 else
-  BASE_BRANCH="main"
+  # Get the base branch name from GitHub if we are on a pull request.
+  if gh pr view "$CURRENT_BRANCH" &> /dev/null; then
+    BASE_BRANCH=$(gh pr view "$CURRENT_BRANCH" --json baseRefName -q '.baseRefName')
+  else
+    BASE_BRANCH="main"
+  fi
+  if [ "$1" != "origin" ]; then
+    # extract the owner name of the target repo
+    ORIGIN="$(echo "$1" | grep -o '://[^/]\+/[^/]\+/' | grep -o '/[^/]\+/$' | tr -d '/')"
+    cleanup_remote() {
+      git remote remove "$ORIGIN"
+    }
+    trap cleanup_remote EXIT
+    git remote add "$ORIGIN" "$1"
+    git fetch -q --depth=1 --no-tags "$ORIGIN" "$BASE_BRANCH"
+  else
+    ORIGIN="origin"
+  fi
+  echo "Performing lint and check on ${ORIGIN}/${BASE_BRANCH}..HEAD@${CURRENT_COMMIT} ..."
+  pants tailor --check update-build-files --check ::
+  pants lint check --changed-since="${ORIGIN}/${BASE_BRANCH}"
 fi
-
-if [ "$1" != "origin" ]; then
-  # extract the owner name of the target repo
-  ORIGIN="$(echo "$1" | grep -o '://[^/]\+/[^/]\+/' | grep -o '/[^/]\+/$' | tr -d '/')"
-  cleanup_remote() {
-    git remote remove "$ORIGIN"
-  }
-  trap cleanup_remote EXIT
-  git remote add "$ORIGIN" "$1"
-  git fetch -q --depth=1 --no-tags "$ORIGIN" "$BASE_BRANCH"
-else
-  ORIGIN="origin"
-fi
-echo "Performing lint and check on ${ORIGIN}/${BASE_BRANCH}..HEAD@${CURRENT_COMMIT} ..."
-pants tailor --check update-build-files --check ::
-pants lint check --changed-since="${ORIGIN}/${BASE_BRANCH}"
