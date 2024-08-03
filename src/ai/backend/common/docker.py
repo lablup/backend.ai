3f4e9067071a888acd12ff7a82011eb83752a607
Gyubong Lee <jopemachine@naver.com>
2024-08-03 07:17:56 +0000

    chore: Add comment

--- src/ai/backend/common/docker.py
+++ src/ai/backend/common/docker.py
@@ -253,6 +253,8 @@ async def login(
         return {"auth": basic_auth, "headers": {}}
     elif ping_status == 404:
         raise RuntimeError(f"Unsupported docker registry: {registry_url}! (API v2 not implemented)")
+    # Check also 400 response since the AWS ECR Public server returns a 400 response
+    # when given invalid credential authorization.
     elif ping_status in [400, 401]:
         params = {
             "scope": scope,
