d10da29a083ff89642c335e50c8f8ec492a27a89
Gyubong Lee <jopemachine@naver.com>
2024-08-03 02:10:52 +0000

    feat: Support GitLab Container Registry based on `PostgreSQL`

---
+++ src/ai/backend/manager/container_registry/gitlab.py
@@ -0,0 +1,56 @@
+import logging
+import urllib.parse
+from typing import AsyncIterator
+
+import aiohttp
+
+from ai.backend.common.logging import BraceStyleAdapter
+
+from .base import (
+    BaseContainerRegistry,
+)
+
+log = BraceStyleAdapter(logging.getLogger(__spec__.name))  # type: ignore[name-defined]
+
+
+class GitLabRegistry(BaseContainerRegistry):
+    async def fetch_repositories(self, sess: aiohttp.ClientSession) -> AsyncIterator[str]:
+        name, access_token, gitlab_project = (
+            self.registry_info.username,
+            self.registry_info.password,
+            self.registry_info.extra.get("gitlab_project"),
+        )
+
+        projects = gitlab_project.split(",")
+
+        for project in projects:
+            encoded_project_id = urllib.parse.quote(f"{name}/{project}", safe="")
+            repo_list_url = (
+                f"https://gitlab.com/api/v4/projects/{encoded_project_id}/registry/repositories"
+            )
+
+            headers = {
+                "Accept": "application/json",
+                "PRIVATE-TOKEN": access_token,
+            }
+            page = 1
+
+            while True:
+                async with sess.get(
+                    repo_list_url,
+                    headers=headers,
+                    params={"per_page": 30, "page": page},
+                ) as response:
+                    if response.status == 200:
+                        data = await response.json()
+
+                        for repo in data:
+                            yield repo["path"]
+                        if "next" in response.headers.get("Link", ""):
+                            page += 1
+                        else:
+                            break
+                    else:
+                        raise RuntimeError(
+                            f"Failed to fetch repositories for project {project}! {response.status} error occurred."
+                        )
