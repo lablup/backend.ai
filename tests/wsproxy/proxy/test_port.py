8707f1e1f4bcc684ae5c793c85b6dd5a296721d1
Pilmo Kim <pilmo@lablup.com>
2024-08-19 07:26:45 +0000

    refactor: move create_circuit func utils to test_port

--- tests/wsproxy/proxy/test_port.py
+++ tests/wsproxy/proxy/test_port.py
@@ -1,12 +1,48 @@
+from datetime import datetime
 from typing import Any, Dict
+from uuid import UUID
 
 import pytest
 from aiohttp import web
 
+from ai.backend.agent.utils import update_nested_dict
 from ai.backend.wsproxy.exceptions import GenericBadRequest
 from ai.backend.wsproxy.proxy.frontend.http.port import PortFrontend
+from ai.backend.wsproxy.types import AppMode, Circuit, FrontendMode, ProxyProtocol, RouteInfo
 
-from ..utils import create_circuit
+
+def create_circuit(**overrides) -> Circuit:
+    default_values = {
+        "id": UUID("d0e6f60c-f375-4454-b4d3-e8ee202fa372"),
+        "app": "ttyd",
+        "protocol": ProxyProtocol.HTTP,
+        "worker": UUID("00000000-0000-0000-0000-000000000000"),
+        "app_mode": AppMode.INTERACTIVE,
+        "frontend_mode": FrontendMode.PORT,
+        "envs": {},
+        "arguments": None,
+        "open_to_public": False,
+        "allowed_client_ips": None,
+        "port": 8080,
+        "user_id": UUID("f38dea23-50fa-42a0-b5ae-338f5f4693f4"),
+        "access_key": "AKIAIOSFODNN7EXAMPLE",
+        "endpoint_id": None,
+        "route_info": [
+            RouteInfo(
+                session_id=UUID("f5cd34ba-ae53-4537-a813-09f38496443d"),
+                session_name=None,
+                kernel_host="127.0.0.1",
+                kernel_port=30729,
+                protocol=ProxyProtocol.HTTP,
+                traffic_ratio=1.0,
+            )
+        ],
+        "session_ids": [UUID("f5cd34ba-ae53-4537-a813-09f38496443d")],
+        "created_at": datetime(2024, 7, 16, 5, 45, 45, 982446),
+        "updated_at": datetime(2024, 7, 16, 5, 45, 45, 982452),
+    }
+    update_nested_dict(default_values, overrides)
+    return Circuit(**default_values)  # type: ignore
 
 
 class DummyRequest:
