6316257dfa994f60583b6cb4bd7d7f8b18286bb5
Joongi Kim <joongi@lablup.com>
2024-08-24 16:52:18 +0900

    fix: Add explicit deprecation warning when import legacy common.logging modules

--- src/ai/backend/common/logging.py
+++ src/ai/backend/common/logging.py
@@ -1,5 +1,12 @@
 # Kept for backward-compatibility of the plugins relying on the old common.logging module.
+import warnings
 
 from ai.backend.logging import BraceStyleAdapter
 
 __all__ = ("BraceStyleAdapter",)
+
+warnings.warn(
+    "Please import BraceStyleAdapter from ai.backend.logging "
+    "instead of ai.backend.common.logging",
+    DeprecationWarning,
+)
--- src/ai/backend/common/logging_utils.py
+++ src/ai/backend/common/logging_utils.py
@@ -1,5 +1,12 @@
-# Kept for backward-compatibility of the plugins relying on the old common.logging module.
+# Kept for backward-compatibility of the plugins relying on the old common.logging_utils module.
+import warnings
 
 from ai.backend.logging import BraceStyleAdapter
 
 __all__ = ("BraceStyleAdapter",)
+
+warnings.warn(
+    "Please import BraceStyleAdapter from ai.backend.logging "
+    "instead of ai.backend.common.logging_utils",
+    DeprecationWarning,
+)
