eee84cc60ce3f160a9cde357805770b41e43bd46
Sanghun Lee <sanghun@lablup.com>
2024-09-18 17:33:24 +0900

    cleanup old static files

--- src/ai/backend/gateway/cli.py
+++ src/ai/backend/gateway/cli.py
@@ -3,5 +3,5 @@ import click
 
 @click.group()
 def main():
-    """The root entrypoint for unified CLI of the web server"""
+    """The root entrypoint for unified CLI of the gateway server"""
     pass
