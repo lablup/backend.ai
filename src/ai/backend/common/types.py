4e665786a17409e378f41c6f87d8d3b565b7b6f0
Joongi Kim <joongi@lablup.com>
2024-09-20 18:41:23 +0900

    fix: test failures due to typeguard updates
    - typeguard now correctly handles TypedDict.

--- src/ai/backend/common/types.py
+++ src/ai/backend/common/types.py
@@ -6,7 +6,6 @@ import ipaddress
 import itertools
 import math
 import numbers
-import sys
 import uuid
 from abc import ABCMeta, abstractmethod
 from collections import UserDict, defaultdict, namedtuple
@@ -185,30 +184,7 @@ def check_typed_tuple(value: Tuple[Any, ...], types: Tuple[Type, ...]) -> Tuple:
     return value
 
 
-TD = TypeVar("TD")
-
-
-def check_typed_dict(value: Mapping[Any, Any], expected_type: Type[TD]) -> TD:
-    """
-    Validates the given dict against the given TypedDict class,
-    and wraps the value as the given TypedDict type.
-
-    This is a shortcut to :func:`typeguard.check_typed_dict()` function to fill extra information
-
-    Currently using this function may not be able to fix type errors, due to an upstream issue:
-    python/mypy#9827
-    """
-    assert issubclass(expected_type, dict) and hasattr(
-        expected_type, "__annotations__"
-    ), f"expected_type ({type(expected_type)}) must be a TypedDict class"
-    frame = sys._getframe(1)
-    _globals = frame.f_globals
-    _locals = frame.f_locals
-    memo = typeguard._TypeCheckMemo(_globals, _locals)
-    typeguard.check_typed_dict("value", value, expected_type, memo)
-    # Here we passed the check, so return it after casting.
-    return cast(TD, value)
-
+check_typed_dict = typeguard.check_type
 
 PID = NewType("PID", int)
 HostPID = NewType("HostPID", PID)
--- tests/common/test_types.py
+++ tests/common/test_types.py
@@ -2,6 +2,7 @@ import asyncio
 from decimal import Decimal
 
 import pytest
+from typeguard import TypeCheckError
 
 from ai.backend.common.types import (
     BinarySize,
@@ -61,17 +62,17 @@ async def test_aobject():
 
 
 def test_check_typed_dict():
-    with pytest.raises(TypeError):
+    # As of 24.09, check_typed_dict() is a mere alias of typeguard.check_type().
+    with pytest.raises(TypeError):  # the second arg is not hashable
         check_typed_dict({}, {})
-    with pytest.raises(AssertionError):
-        check_typed_dict({}, dict)
-    with pytest.raises(AssertionError):
+    check_typed_dict({}, dict)
+    with pytest.raises(TypeCheckError):
         check_typed_dict({}, int)
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeCheckError):
         check_typed_dict({}, HardwareMetadata)
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeCheckError):
         check_typed_dict({"status": "oops", "status_info": None, "metadata": {}}, HardwareMetadata)
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeCheckError):
         check_typed_dict(
             {"status": "healthy", "status_info": None, "metadata": {"a": 1}}, HardwareMetadata
         )
