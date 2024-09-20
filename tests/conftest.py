d957fd872a649345eebf3bf41162431c8b422167
Joongi Kim <joongi@lablup.com>
2024-09-20 17:17:02 +0900

    ci: Make the pytest option `-m 'not integration'` default

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,22 @@
-# placeholder for global fixtures
+from pytest import Config, Parser
+
+
+def pytest_addoption(parser: Parser) -> None:
+    parser.addoption(
+        "--integration",
+        action="store_true",
+        dest="integration",
+        default=False,
+        help="enable longrundecorated tests",
+    )
+
+
+def pytest_configure(config: Config) -> None:
+    # Disable the tests marked as "integration" by default,
+    # unless the user gives the "--integration" CLI option.
+    markerexpr = getattr(config.option, "markexpr", "")
+    if not config.option.integration:
+        if markerexpr:
+            setattr(config.option, "markexpr", f"({markerexpr}) and not integration")
+        else:
+            setattr(config.option, "markexpr", "not integration")
