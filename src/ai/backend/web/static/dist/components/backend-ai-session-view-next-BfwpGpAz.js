6c75e69f24939f2b35b939e0c274203a3b587859
Kyujin Cho <kyujin.cho@lablup.com>
2024-09-27 09:36:12 +0900

    deps: Update the prebuilt webui to latest (24.03.10)

---
+++ src/ai/backend/web/static/dist/components/backend-ai-session-view-next-BfwpGpAz.js
@@ -0,0 +1,5 @@
+import{_ as s,t as i,B as t,b as a,I as e,a as r,i as o,k as n,s as c,l as m}from"./backend-ai-webui-64Hb1TBh.js";import"./backend-ai-session-launcher-DLC0dD-G.js";import"./backend-ai-session-view-ZKRrs65n.js";import"./lablup-progress-bar-WXW3UbcM.js";import"./slider-DeHZ65il.js";import"./mwc-check-list-item-8Zd0FtHH.js";import"./vaadin-grid-filter-column-BoK9W3KL.js";import"./vaadin-iconset-uUI3AAzK.js";import"./backend-ai-resource-monitor-0yCuJ-11.js";import"./mwc-switch-B8pW8CkE.js";import"./backend-ai-list-status-CmUqpj8c.js";import"./lablup-grid-sort-filter-column-D-EEtuCs.js";import"./lablup-activity-panel-B9IpMT33.js";import"./mwc-formfield-DV-8nMxX.js";import"./mwc-tab-bar-DNZ-1j5s.js";let p=class extends t{static get styles(){return[a,e,r,o``]}async _viewStateChanged(s){await this.updateComplete}render(){return n`
+      <backend-ai-react-session-list
+        @moveTo="${s=>{const i=s.detail.path;globalThis.history.pushState({},"",i),c.dispatch(m(decodeURIComponent(i),{}))}}"
+      ></backend-ai-react-session-list>
+    `}};p=s([i("backend-ai-session-view-next")],p);
--- src/ai/backend/web/static/dist/components/backend-ai-webui.js
+++ src/ai/backend/web/static/dist/components/backend-ai-webui.js
@@ -1,1 +1,1 @@
-export{aP as default}from"./backend-ai-webui-DHPXkWFV.js";
+export{aP as default}from"./backend-ai-webui-64Hb1TBh.js";
