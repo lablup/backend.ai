1bd2f3709b6a7737fe64adb57b06cdfc0b0c8d8a
Sanghun Lee <sanghun@lablup.com>
2024-10-02 14:23:22 +0900

    update alembic migration script

--- src/ai/backend/manager/models/alembic/versions/6e44ea67d26e_update_session_and_kernel_status.py
+++ src/ai/backend/manager/models/alembic/versions/6e44ea67d26e_update_session_and_kernel_status.py
@@ -1,7 +1,7 @@
 """update_session_and_kernel_status
 
 Revision ID: 6e44ea67d26e
-Revises: 20218a73401b
+Revises: 7c8501cec07b
 Create Date: 2024-08-05 14:39:11.901615
 
 """
@@ -14,7 +14,7 @@ from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
 revision = "6e44ea67d26e"
-down_revision = "20218a73401b"
+down_revision = "7c8501cec07b"
 branch_labels = None
 depends_on = None
 
