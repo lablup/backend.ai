839e73f021697940bd2ac1f496601dfd43c93f2b
Kyujin Cho <kyujin.cho@lablup.com>
2024-10-08 18:25:39 +0900

    fix invalid signature

--- src/ai/backend/agent/plugin/network.py
+++ src/ai/backend/agent/plugin/network.py
@@ -1,17 +1,21 @@
 from abc import ABCMeta, abstractmethod
-from typing import Any
+from typing import Any, Generic, TypeVar
 
 from ai.backend.agent.kernel import AbstractKernel
 from ai.backend.common.plugin import AbstractPlugin, BasePluginContext
 from ai.backend.common.types import ClusterInfo, KernelCreationConfig
 
+TKernel = TypeVar("TKernel", bound=AbstractKernel)
 
-class AbstractNetworkAgentPlugin(AbstractPlugin, metaclass=ABCMeta):
+
+class AbstractNetworkAgentPlugin(AbstractPlugin, Generic[TKernel], metaclass=ABCMeta):
     @abstractmethod
     async def join_network(
         self,
         kernel_config: KernelCreationConfig,
         cluster_info: ClusterInfo,
+        *,
+        network_name: str,
         **kwargs,
     ) -> dict[str, Any]:
         """
@@ -22,7 +26,7 @@ class AbstractNetworkAgentPlugin(AbstractPlugin, metaclass=ABCMeta):
     @abstractmethod
     async def leave_network(
         self,
-        kernel: AbstractKernel,
+        kernel: TKernel,
     ) -> None:
         """
         Performs extra step to make container leave from the network.
