44e094e9020ca1fb22ba331b7651b312f90b974b
Kyujin Cho <kyujin.cho@lablup.com>
2024-10-21 11:15:57 +0900

    Update config.py

--- src/ai/backend/common/config.py
+++ src/ai/backend/common/config.py
@@ -117,10 +117,10 @@ model_definition_iv = t.Dict({
                 t.Key("port"): t.ToInt[1:],
                 t.Key("health_check", default=None): t.Null
                 | t.Dict({
-                    t.Key("interval", default=1): t.Null | t.ToFloat[0:],
+                    t.Key("interval", default=10): t.Null | t.ToFloat[0:],
                     t.Key("path"): t.String,
                     t.Key("max_retries", default=10): t.Null | t.ToInt[1:],
-                    t.Key("max_wait_time", default=5): t.Null | t.ToFloat[0:],
+                    t.Key("max_wait_time", default=15): t.Null | t.ToFloat[0:],
                     t.Key("expected_status_code", default=200): t.Null | t.ToInt[100:],
                 }),
             }),
