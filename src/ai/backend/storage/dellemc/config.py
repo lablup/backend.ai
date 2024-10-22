7cc3a817d0db9c192fc419963d2c4c0d0cdf81de
Sanghun Lee <sanghun@lablup.com>
2024-10-22 13:42:57 +0900

    fix: PowerScale config allow ifs path not exists (#2932)

--- src/ai/backend/storage/dellemc/config.py
+++ src/ai/backend/storage/dellemc/config.py
@@ -7,6 +7,6 @@ config_iv = t.Dict({
     tx.AliasedKey(["dell_username", "dell_admin"]): t.String(),
     t.Key("dell_password"): t.String(),
     t.Key("dell_api_version"): t.String(),
-    t.Key("dell_ifs_path"): tx.Path(type="dir"),
+    t.Key("dell_ifs_path"): tx.Path(type="dir", allow_nonexisting=True),
     t.Key("dell_system_name", default=None): t.Null | t.String(),
 }).allow_extra("*")
