06222bffe5fb7abbb6a57d3661683ab750600d24
Gyubong Lee <jopemachine@naver.com>
2024-10-25 06:37:32 +0000

    fix: Remove unreleated changes

--- src/ai/backend/common/docker.py
+++ src/ai/backend/common/docker.py
@@ -568,9 +568,6 @@ class ImageRef:
     @property
     def canonical(self) -> str:
         join = functools.partial(join_non_empty, sep="/")
-        if self.is_local:
-            return f"{join(self.project, self.name)}:{self.tag}"
-
         # e.g., cr.backend.ai/stable/python:3.9-ubuntu
         return f"{join(self.registry, self.project, self.name)}:{self.tag}"
 
