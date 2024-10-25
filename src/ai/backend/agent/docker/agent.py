7a6ee6f4b51952db9d5b38b7c29174cddb27194d
Gyubong Lee <jopemachine@naver.com>
2024-10-25 06:41:33 +0000

    fix: Remove unreleated changes

--- src/ai/backend/agent/docker/agent.py
+++ src/ai/backend/agent/docker/agent.py
@@ -880,8 +880,13 @@ class DockerKernelCreationContext(AbstractKernelCreationContext[DockerKernel]):
         container_log_file_count = 5
         container_log_file_size = BinarySize(container_log_size // container_log_file_count)
 
+        if self.image_ref.is_local:
+            image = self.image_ref.short
+        else:
+            image = self.image_ref.canonical
+
         container_config: MutableMapping[str, Any] = {
-            "Image": self.image_ref.canonical,
+            "Image": image,
             "Tty": True,
             "OpenStdin": True,
             "Privileged": False,
