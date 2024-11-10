4f7d663de68fd83f6725c287cfff44aefe43aaf5
Joongi Kim <joongi@lablup.com>
2024-11-11 05:39:36 +0900

    feat: Implement session-based concurrency update func

--- src/ai/backend/manager/models/resource_policy.py
+++ src/ai/backend/manager/models/resource_policy.py
@@ -974,17 +974,29 @@ class ConcurrencyTracker:
             SessionRow,
         )
 
-        session_query = sa.select([AccessKey, SessionRow]).where(
-            # TODO: apply groupby access_key
-            (SessionRow.status.in_(USER_RESOURCE_OCCUPYING_KERNEL_STATUSES))
-            & (SessionRow.session_type.not_in(PRIVATE_SESSION_TYPES))
+        session_query = (
+            sa.select([
+                SessionRow.access_key,
+                sa.func.array_agg(SessionRow.id).label("session_ids"),
+            ])
+            .where(
+                (SessionRow.status.in_(USER_RESOURCE_OCCUPYING_KERNEL_STATUSES))
+                & (SessionRow.session_type.not_in(PRIVATE_SESSION_TYPES))
+            )
+            .groupby(SessionRow.access_key)
         )
         active_compute_sessions = (await db_sess.execute(session_query)).all()
 
-        session_query = sa.select([AccessKey, SessionRow]).where(
-            # TODO: apply groupby access_key
-            (SessionRow.status.in_(USER_RESOURCE_OCCUPYING_KERNEL_STATUSES))
-            & (SessionRow.session_type.in_(PRIVATE_SESSION_TYPES))
+        session_query = (
+            sa.select([
+                SessionRow.access_key,
+                sa.func.array_agg(SessionRow.id).label("session_ids"),
+            ])
+            .where(
+                (SessionRow.status.in_(USER_RESOURCE_OCCUPYING_KERNEL_STATUSES))
+                & (SessionRow.session_type.in_(PRIVATE_SESSION_TYPES))
+            )
+            .groupby(SessionRow.access_key)
         )
         active_system_sessions = (await db_sess.execute(session_query)).all()
 
