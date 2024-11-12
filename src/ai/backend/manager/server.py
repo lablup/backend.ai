363e36922830294367b5d67bf20f4be46a4388e1
Gyubong Lee <jopemachine@naver.com>
2024-11-12 05:15:11 +0000

    feat: Implement REST API for container_registry

--- src/ai/backend/manager/server.py
+++ src/ai/backend/manager/server.py
@@ -176,6 +176,7 @@ public_interface_objs: MutableMapping[str, Any] = {}
 
 global_subapp_pkgs: Final[list[str]] = [
     ".acl",
+    ".container_registry",
     ".etcd",
     ".events",
     ".auth",
