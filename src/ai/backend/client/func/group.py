9dbf14b3411336258d490897b612dbd99c513b6f
Gyubong Lee <jopemachine@naver.com>
2024-11-18 05:15:15 +0000

    fix: Broken CI

--- src/ai/backend/client/func/group.py
+++ src/ai/backend/client/func/group.py
@@ -1,8 +1,6 @@
 import textwrap
 from typing import Any, Iterable, Optional, Sequence
 
-from graphql_relay.utils import base64
-
 from ai.backend.client.output.fields import group_fields
 from ai.backend.client.output.types import FieldSpec
 from ai.backend.common.utils import b64encode
