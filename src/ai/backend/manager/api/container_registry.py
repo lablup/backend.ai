f071296eaec3aa23e9550bc7253a7c7ea102a417
Gyubong Lee <jopemachine@naver.com>
2024-11-19 08:28:05 +0000

    feat: Add `PUSH_ARTIFACT` event handler

--- src/ai/backend/manager/api/container_registry.py
+++ src/ai/backend/manager/api/container_registry.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING, Iterable, Tuple
 
 import aiohttp_cors
 import sqlalchemy as sa
+import trafaret as t
 from aiohttp import web
 from sqlalchemy.exc import IntegrityError
 
@@ -14,6 +15,10 @@ from ai.backend.common.container_registry import (
     PatchContainerRegistryResponseModel,
 )
 from ai.backend.logging import BraceStyleAdapter
+from ai.backend.manager.container_registry.harbor import HarborRegistry_v2
+from ai.backend.manager.models.container_registry import (
+    ContainerRegistryRow,
+)
 from ai.backend.manager.models.gql_models.container_registry_v2 import handle_allowed_groups_update
 
 from .exceptions import ContainerRegistryNotFound, GenericBadRequest, InternalServerError
@@ -24,7 +29,7 @@ if TYPE_CHECKING:
 from .auth import superadmin_required
 from .manager import ALL_ALLOWED, READ_ALLOWED, server_status_required
 from .types import CORSOptions, WebMiddleware
-from .utils import pydantic_params_api_handler
+from .utils import check_api_params, pydantic_params_api_handler
 
 log = BraceStyleAdapter(logging.getLogger(__spec__.name))
 
@@ -69,12 +74,41 @@ async def patch_container_registry(
 
 @server_status_required(ALL_ALLOWED)
 @check_api_params(t.Mapping(t.String, t.Any))
-async def webhook_handler(request: web.Request, params: Any) -> web.Response:
-    print("Received request")
+async def harbor_webhook_handler(request: web.Request, params: Any) -> web.Response:
+    event_type = params["type"]
+    resources = params["event_data"]["resources"]
+    project = params["event_data"]["repository"]["namespace"]
+    img_name = params["event_data"]["repository"]["name"]
+    log.info("HARBOR_WEBHOOK_HANDLER (type:{})", event_type)
 
-    # root_ctx: RootContext = request.app["_root.context"]
-    # async with root_ctx.db.begin_session() as db_sess:
-    #     ...
+    root_ctx: RootContext = request.app["_root.context"]
+
+    async with root_ctx.db.begin_session() as db_sess:
+        for resource in resources:
+            resource_url = resource["resource_url"]
+            registry_url = resource_url.split("/")[0]
+
+            registry_row = (
+                await db_sess.execute(
+                    sa.select([ContainerRegistryRow]).where(
+                        (ContainerRegistryRow.project == project)
+                        & (ContainerRegistryRow.url.like(f"%{registry_url}%"))
+                    )
+                )
+            ).fetchone()[0]
+
+            match event_type:
+                # Perform image rescan only for events that require it.
+                case "PUSH_ARTIFACT":
+                    scanner_cls = HarborRegistry_v2
+                    scanner = scanner_cls(root_ctx.db, project, registry_row)
+                    img_tag = resource["tag"]
+                    await scanner.scan_single_ref(project + "/" + img_name + ":" + img_tag)
+                case "DELETE_ARTIFACT":
+                    # TODO: Delete image row directly
+                    pass
+                case _:
+                    pass
 
     return web.json_response({})
 
@@ -87,5 +121,5 @@ def create_app(
     app["prefix"] = "container-registries"
     cors = aiohttp_cors.setup(app, defaults=default_cors_options)
     cors.add(app.router.add_route("PATCH", "/{registry_id}", patch_container_registry))
-    cors.add(app.router.add_route("POST", "/webhook/harbor", webhook_handler))
+    cors.add(app.router.add_route("POST", "/webhook/harbor", harbor_webhook_handler))
     return app, []
