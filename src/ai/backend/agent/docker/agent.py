33923a175d3708954e40891147d4fa3be24d4b15
Gyubong Lee <jopemachine@naver.com>
2024-11-28 02:14:21 +0000

    refactor: `Separate _apply_seccomp_profile into separate function` *(Reflect feedback)

--- src/ai/backend/agent/docker/agent.py
+++ src/ai/backend/agent/docker/agent.py
@@ -784,6 +784,31 @@ class DockerKernelCreationContext(AbstractKernelCreationContext[DockerKernel]):
             case ResourceGroupType.STORAGE:
                 return ("ttyd",)
 
+    async def _apply_seccomp_profile(self, container_config: MutableMapping[str, Any]) -> None:
+        default_seccomp_pth = self.resolve_krunner_filepath("runner/default-seccomp.json")
+
+        if not default_seccomp_pth.exists():
+            log.warning(
+                "Default seccomp profile file not found in the expected path! Skipped the application of additional syscalls."
+            )
+            return
+
+        async with aiofiles.open(default_seccomp_pth, mode="r") as fp:
+            seccomp_profile = json.loads(await fp.read())
+
+            additional_allowed_syscalls = self.additional_allowed_syscalls
+            additional_allowed_syscall_rule = {
+                "names": additional_allowed_syscalls,
+                "action": "SCMP_ACT_ALLOW",
+                "args": [],
+                "comment": "Additionally allowed syscalls by Backend.AI Agent",
+            }
+            seccomp_profile["syscalls"].append(additional_allowed_syscall_rule)
+
+            container_config["HostConfig"]["SecurityOpt"] = [
+                f"seccomp={json.dumps(seccomp_profile)}"
+            ]
+
     async def start_container(
         self,
         kernel_obj: AbstractKernel,
@@ -908,28 +933,7 @@ class DockerKernelCreationContext(AbstractKernelCreationContext[DockerKernel]):
             },
         }
 
-        default_seccomp_pth = self.resolve_krunner_filepath("runner/default-seccomp.json")
-
-        if default_seccomp_pth.exists():
-            async with aiofiles.open(default_seccomp_pth, mode="r") as fp:
-                seccomp_profile = json.loads(await fp.read())
-
-                additional_allowed_syscalls = self.additional_allowed_syscalls
-                additional_allowed_syscall_rule = {
-                    "names": additional_allowed_syscalls,
-                    "action": "SCMP_ACT_ALLOW",
-                    "args": [],
-                    "comment": "Additionally allowed syscalls by Backend.AI Agent",
-                }
-                seccomp_profile["syscalls"].append(additional_allowed_syscall_rule)
-
-                container_config["HostConfig"]["SecurityOpt"] = [
-                    f"seccomp={json.dumps(seccomp_profile)}"
-                ]
-        else:
-            log.warning(
-                "Default seccomp profile file not found. Skipped the application of additional syscall allow rule."
-            )
+        await self._apply_seccomp_profile(container_config)
 
         # merge all container configs generated during prior preparation steps
         for c in self.container_configs:
