09db69fe68c1663a4f19c15361eaaade092f1eec
Gyubong Lee <jopemachine@naver.com>
2024-11-28 20:05:41 +0900

    fix: Regression of `AgentSummary` GQL resolver (#3045)
    Backported-from: main
    Backported-to: 24.09
    Backported-of: 3045

--- src/ai/backend/manager/models/gql.py
+++ src/ai/backend/manager/models/gql.py
@@ -892,9 +892,9 @@ class Queries(graphene.ObjectType):
         if ctx.local_config["manager"]["hide-agents"]:
             raise ObjectNotFound(object_name="agent")
 
-        loader = ctx.dataloader_manager.get_loader(
+        loader = ctx.dataloader_manager.get_loader_by_func(
             ctx,
-            "Agent",
+            AgentSummary.batch_load,
             raw_status=None,
             scaling_group=scaling_group,
             domain_name=domain_name,
