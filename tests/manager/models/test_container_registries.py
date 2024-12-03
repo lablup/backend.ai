dddaf1cfd5168cf3b82052fe2ac9a8ad9c78d31e
Gyubong Lee <jopemachine@naver.com>
2024-12-03 06:42:31 +0000

    fix: Change the `test_harbor_read_project_quota` test location

--- tests/manager/models/test_container_registries.py
+++ tests/manager/models/test_container_registries.py
@@ -3,7 +3,6 @@ from aioresponses import aioresponses
 from graphene import Schema
 from graphene.test import Client
 
-from ai.backend.common.utils import b64encode
 from ai.backend.manager.api.context import RootContext
 from ai.backend.manager.defs import PASSWORD_PLACEHOLDER
 from ai.backend.manager.models.gql import GraphQueryContext, Mutations, Queries
@@ -294,58 +293,6 @@ FIXTURES_FOR_HARBOR_CRUD_TEST = [
         ],
     },
 ]
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize("extra_fixtures", FIXTURES_FOR_HARBOR_CRUD_TEST)
-async def test_harbor_read_project_quota(
-    client: Client,
-    database_fixture,
-    create_app_and_client,
-):
-    test_app, _ = await create_app_and_client(
-        [
-            database_ctx,
-        ],
-        [],
-    )
-
-    root_ctx: RootContext = test_app["_root.context"]
-    context = get_graphquery_context(root_ctx.db)
-
-    # Arbitrary values for mocking Harbor API responses
-    HARBOR_PROJECT_ID = "123"
-    HARBOR_QUOTA_ID = 456
-    HARBOR_QUOTA_VALUE = 1024
-
-    with aioresponses() as mocked:
-        get_project_id_url = "http://mock_registry/api/v2.0/projects/mock_project"
-        mocked.get(get_project_id_url, status=200, payload={"project_id": HARBOR_PROJECT_ID})
-
-        get_quota_url = f"http://mock_registry/api/v2.0/quotas?reference=project&reference_id={HARBOR_PROJECT_ID}"
-        mocked.get(
-            get_quota_url,
-            status=200,
-            payload=[{"id": HARBOR_QUOTA_ID, "hard": {"storage": HARBOR_QUOTA_VALUE}}],
-        )
-
-        groupnode_query = """
-            query ($id: String!) {
-                group_node(id: $id) {
-                    registry_quota
-                }
-            }
-        """
-
-        group_id = "00000000-0000-0000-0000-000000000000"
-        variables = {
-            "id": b64encode(f"group_node:{group_id}"),
-        }
-
-        response = await client.execute_async(
-            groupnode_query, variables=variables, context_value=context
-        )
-        assert response["data"]["group_node"]["registry_quota"] == HARBOR_QUOTA_VALUE
 
 
 @pytest.mark.asyncio
