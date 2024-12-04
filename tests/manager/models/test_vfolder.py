7fc574b3c71fb9b3d837cb8a5d2609103bc051a5
Gyubong Lee <jopemachine@naver.com>
2024-12-04 05:37:25 +0000

    fix: Remove unused variable

--- tests/manager/models/test_vfolder.py
+++ tests/manager/models/test_vfolder.py
@@ -1,20 +1,13 @@
 import uuid
 
 import pytest
-from graphene import Schema
-from graphene.test import Client
 
-from ai.backend.manager.models.gql import GraphQueryContext, Mutations, Queries
+from ai.backend.manager.models.gql import GraphQueryContext
 from ai.backend.manager.models.utils import ExtendedAsyncSAEngine
 from ai.backend.manager.models.vfolder import VirtualFolder
 from ai.backend.manager.server import (
     database_ctx,
 )
-
-
-@pytest.fixture(scope="module")
-def client() -> Client:
-    return Client(Schema(query=Queries, mutation=Mutations, auto_camelcase=False))
 
 
 def get_graphquery_context(database_engine: ExtendedAsyncSAEngine) -> GraphQueryContext:
