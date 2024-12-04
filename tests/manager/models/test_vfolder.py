2815400f4cd499f40d11b2c767835d637a337657
Gyubong Lee <jopemachine@naver.com>
2024-12-04 01:05:23 +0000

    feat: Add more `batch_load_by_id` tests

--- tests/manager/models/test_vfolder.py
+++ tests/manager/models/test_vfolder.py
@@ -41,11 +41,27 @@ def get_graphquery_context(database_engine: ExtendedAsyncSAEngine) -> GraphQuery
 
 FIXTURES_VFOLDERS = [
     {
+        "users": [
+            {
+                "uuid": "00000000-0000-0000-0000-000000000000",
+                "username": "admin",
+                "email": "admin@lablup.com",
+                "password": "wJalrXUt",
+                "need_password_change": False,
+                "full_name": "Admin Lablup",
+                "description": "Lablup's Admin Account",
+                "status": "active",
+                "status_info": "admin-requested",
+                "domain_name": "mock_domain",
+                "resource_policy": "default",
+                "role": "superadmin",
+            }
+        ],
         "vfolders": [
             {
                 "id": "00000000-0000-0000-0000-000000000001",
                 "host": "mock",
-                "domain_name": "default",
+                "domain_name": "mock_domain",
                 "name": "mock_vfolder_1",
                 "quota_scope_id": "user:00000000-0000-0000-0000-000000000000",
                 "usage_mode": "general",
@@ -55,11 +71,12 @@ FIXTURES_VFOLDERS = [
                 "cloneable": False,
                 "max_files": 1000,
                 "num_files": 100,
+                "user": "00000000-0000-0000-0000-000000000000",
             },
             {
                 "id": "00000000-0000-0000-0000-000000000002",
                 "host": "mock",
-                "domain_name": "default",
+                "domain_name": "mock_domain",
                 "name": "mock_vfolder_2",
                 "quota_scope_id": "user:00000000-0000-0000-0000-000000000000",
                 "usage_mode": "general",
@@ -69,8 +86,9 @@ FIXTURES_VFOLDERS = [
                 "cloneable": False,
                 "max_files": 1000,
                 "num_files": 100,
+                "user": "00000000-0000-0000-0000-000000000000",
             },
-        ]
+        ],
     }
 ]
 
@@ -84,6 +102,12 @@ async def test_batch_load_by_id(
     database_fixture,
     create_app_and_client,
 ):
+    mock_user = uuid.UUID("00000000-0000-0000-0000-000000000000")
+    mock_vfolders = [
+        uuid.UUID("00000000-0000-0000-0000-000000000001"),
+        uuid.UUID("00000000-0000-0000-0000-000000000002"),
+    ]
+
     test_app, _ = await create_app_and_client(
         [
             database_ctx,
@@ -96,6 +120,41 @@ async def test_batch_load_by_id(
 
     result = await VirtualFolder.batch_load_by_id(
         context,
-        [uuid.UUID("00000000-0000-0000-0000-000000000001")],
+        mock_vfolders[:1],
     )
     assert len(result) == 1
+    assert result[0].id == mock_vfolders[0]
+
+    result = await VirtualFolder.batch_load_by_id(
+        context,
+        mock_vfolders[:2],
+    )
+    assert len(result) == 2
+    assert result[0].id == mock_vfolders[0]
+    assert result[1].id == mock_vfolders[1]
+
+    result = await VirtualFolder.batch_load_by_id(
+        context,
+        mock_vfolders[:1],
+        user_id=mock_user,
+    )
+    assert len(result) == 1
+    assert result[0].id == mock_vfolders[0]
+
+    result = await VirtualFolder.batch_load_by_id(
+        context,
+        mock_vfolders[:1],
+        user_id=mock_user,
+        domain_name="mock_domain",
+    )
+    assert len(result) == 1
+    assert result[0].id == mock_vfolders[0]
+
+    result = await VirtualFolder.batch_load_by_id(
+        context,
+        mock_vfolders[:1],
+        user_id=mock_user,
+        domain_name="INVALID",
+    )
+    assert len(result) == 1
+    assert result[0] is None
