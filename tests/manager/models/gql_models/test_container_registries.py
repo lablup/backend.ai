aa4bc878c6bdc4f187ac39c4f5054ccbf76a5693
Gyubong <jopemachine@naver.com>
2024-12-05 10:22:44 +0900

    fix: Broken CI

--- tests/manager/models/gql_models/test_container_registries.py
+++ tests/manager/models/gql_models/test_container_registries.py
@@ -40,33 +40,6 @@ def get_graphquery_context(database_engine: ExtendedAsyncSAEngine) -> GraphQuery
     )
 
 
-@pytest.fixture(scope="module")
-def client() -> Client:
-    return Client(Schema(query=Queries, mutation=Mutations, auto_camelcase=False))
-
-
-def get_graphquery_context(database_engine: ExtendedAsyncSAEngine) -> GraphQueryContext:
-    return GraphQueryContext(
-        schema=None,  # type: ignore
-        dataloader_manager=None,  # type: ignore
-        local_config=None,  # type: ignore
-        shared_config=None,  # type: ignore
-        etcd=None,  # type: ignore
-        user={"domain": "default", "role": "superadmin"},
-        access_key="AKIAIOSFODNN7EXAMPLE",
-        db=database_engine,  # type: ignore
-        redis_stat=None,  # type: ignore
-        redis_image=None,  # type: ignore
-        redis_live=None,  # type: ignore
-        manager_status=None,  # type: ignore
-        known_slot_types=None,  # type: ignore
-        background_task_manager=None,  # type: ignore
-        storage_manager=None,  # type: ignore
-        registry=None,  # type: ignore
-        idle_checker_host=None,  # type: ignore
-    )
-
-
 @pytest.mark.asyncio
 @pytest.mark.parametrize("extra_fixtures", FIXTURES_FOR_HARBOR_CRUD_TEST)
 async def test_harbor_create_project_quota(
