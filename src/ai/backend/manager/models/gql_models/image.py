56a5474832ab5812c9b6b4ab794b02703794129f
Gyubong Lee <jopemachine@naver.com>
2024-12-10 08:16:04 +0000

    feat: Enable RescanImage by project

--- src/ai/backend/manager/models/gql_models/image.py
+++ src/ai/backend/manager/models/gql_models/image.py
@@ -670,6 +670,7 @@ class RescanImages(graphene.Mutation):
 
     class Arguments:
         registry = graphene.String()
+        project = graphene.String()
 
     ok = graphene.Boolean()
     msg = graphene.String()
@@ -680,6 +681,7 @@ class RescanImages(graphene.Mutation):
         root: Any,
         info: graphene.ResolveInfo,
         registry: Optional[str] = None,
+        project: Optional[str] = None,
     ) -> RescanImages:
         log.info(
             "rescanning docker registry {0} by API request",
@@ -688,7 +690,7 @@ class RescanImages(graphene.Mutation):
         ctx: GraphQueryContext = info.context
 
         async def _rescan_task(reporter: ProgressReporter) -> None:
-            await rescan_images(ctx.db, registry, reporter=reporter)
+            await rescan_images(ctx.db, registry, project, reporter=reporter)
 
         task_id = await ctx.background_task_manager.start(_rescan_task)
         return RescanImages(ok=True, msg="", task_id=task_id)
