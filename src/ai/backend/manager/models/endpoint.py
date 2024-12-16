f4a75daad0b663e30a5c4f2c412a013a09813b32
Kyujin Cho <kyujin.cho@lablup.com>
2024-12-16 18:11:34 +0900

    consider cases when replicas set as 0

--- src/ai/backend/manager/models/endpoint.py
+++ src/ai/backend/manager/models/endpoint.py
@@ -1159,9 +1159,10 @@ class ModifyEndpoint(graphene.Mutation):
                     except KeyError:
                         raise InvalidAPIParameters(f"Unsupported runtime {_newval}")
 
-                if (
-                    _newval := (props.replicas or props.desired_session_count)
-                ) is not None and _newval is not Undefined:
+                if _newval := props.desired_session_count is not None and _newval is not Undefined:
+                    endpoint_row.desired_session_count = _newval
+
+                if _newval := props.replicas is not None and _newval is not Undefined:
                     endpoint_row.replicas = _newval
 
                 if (_newval := props.resource_group) and _newval is not Undefined:
