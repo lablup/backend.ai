c62d12b23272b226b5d128d66946af01c7c6e034
Joongi Kim <joongi@lablup.com>
2024-12-24 01:06:22 +0900

    refactor: Container preparation step in Agent (#3267)

--- src/ai/backend/agent/dummy/agent.py
+++ src/ai/backend/agent/dummy/agent.py
@@ -98,7 +98,6 @@ class DummyKernelCreationContext(AbstractKernelCreationContext[DummyKernel]):
         current_resource_slots.set(known_slot_types)
         slots = slots.normalize_slots(ignore_unknown=True)
         resource_spec = KernelResourceSpec(
-            container_id="",
             allocations={},
             slots={**slots},  # copy
             mounts=[],
@@ -161,7 +160,7 @@ class DummyKernelCreationContext(AbstractKernelCreationContext[DummyKernel]):
     ):
         return Mount(MountTypes.BIND, Path(), Path())
 
-    async def spawn(
+    async def prepare_container(
         self,
         resource_spec: KernelResourceSpec,
         environ: Mapping[str, str],
