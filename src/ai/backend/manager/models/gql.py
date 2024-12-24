c9e57a9b7d801de4876b1c91f432f6e4c6138828
jopemachine <jopemachine@naver.com>
2024-12-24 03:46:46 +0000

    chore: Add news fragment

--- src/ai/backend/manager/models/gql.py
+++ src/ai/backend/manager/models/gql.py
@@ -283,7 +283,7 @@ class Mutations(graphene.ObjectType):
     modify_user = ModifyUser.Field()
     delete_user = DeleteUser.Field()
     purge_user = PurgeUser.Field()
-    scan_gpu_alloc_maps = ScanGPUAllocMap.Field()
+    scan_gpu_alloc_maps = ScanGPUAllocMap.Field(description="Added in 24.12.0.")
 
     # admin only
     create_keypair = CreateKeyPair.Field()
