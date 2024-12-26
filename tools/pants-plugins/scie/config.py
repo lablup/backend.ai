a5bf7e91b02998c2a1bc15884f040328dea5989b
Joongi Kim <joongi@lablup.com>
2024-12-26 19:15:17 +0900

    chore: Upgrade Python (3.12.6 -> 3.12.8) (#3302)
    Co-authored-by: octodog <mu001@lablup.com>

--- tools/pants-plugins/scie/config.py
+++ tools/pants-plugins/scie/config.py
@@ -58,7 +58,7 @@ class Interpreter:
     version: str
     id: str = "cpython"
     provider: str = "PythonBuildStandalone"
-    release: str = "20240909"
+    release: str = "20241219"
     lazy: bool = False
 
 
