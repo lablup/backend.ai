e5118bea20047b03aaee49833720aa15abd27d27
Sanghun Lee <sanghun@lablup.com>
2025-01-02 15:01:40 +0900

    replace 'gids' column with ('main_gid' and 'additional_gids')

--- src/ai/backend/manager/models/user.py
+++ src/ai/backend/manager/models/user.py
@@ -160,7 +160,10 @@ users = sa.Table(
         nullable=True,  # keypairs.user is non-nullable
     ),
     sa.Column("container_uid", sa.Integer, nullable=True, server_default=sa.null()),
-    sa.Column("container_gids", sa.ARRAY(sa.Integer), nullable=True, server_default=sa.null()),
+    sa.Column("container_main_gid", sa.Integer, nullable=True, server_default=sa.null()),
+    sa.Column(
+        "container_additional_gids", sa.ARRAY(sa.Integer), nullable=True, server_default=sa.null()
+    ),
 )
 
 
