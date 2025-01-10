5661a7f87f68ed416bba877650c3e08cb7b0d1c5
jopemachine <jopemachine@naver.com>
2025-01-10 05:23:39 +0000

    refactor: Revamp ContainerRegistryNode API

---
+++ src/ai/backend/client/func/container_registry.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from ai.backend.client.request import Request
+
+from .base import BaseFunction, api_function
+
+__all__ = ("ContainerRegistry",)
+
+
+class ContainerRegistry(BaseFunction):
+    """
+    Provides functions to manage container registries.
+    """
+
+    @api_function
+    @classmethod
+    # TODO: Implement params type
+    async def patch_container_registry(cls, registry_id: str, params) -> None:
+        """
+        Updates the container registry information, and return the container registry.
+
+        :param registry_id: ID of the container registry.
+        :param params: Parameters to update the container registry.
+        """
+
+        request = Request(
+            "PATCH",
+            f"/container-registries/{registry_id}",
+        )
+        request.set_json(params)
+
+        async with request.fetch() as resp:
+            await resp.read()
---
+++ src/ai/backend/manager/api/container_registry.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import logging
+import uuid
+from typing import TYPE_CHECKING, Iterable, Optional, Tuple
+
+import aiohttp_cors
+import sqlalchemy as sa
+from aiohttp import web
+from pydantic import BaseModel
+from sqlalchemy.exc import IntegrityError
+
+from ai.backend.logging import BraceStyleAdapter
+from ai.backend.manager.models.association_container_registries_groups import (
+    AssociationContainerRegistriesGroupsRow,
+)
+from ai.backend.manager.models.container_registry import ContainerRegistryRow
+from ai.backend.manager.models.utils import ExtendedAsyncSAEngine
+
+from .exceptions import GenericBadRequest, InternalServerError
+
+if TYPE_CHECKING:
+    from .context import RootContext
+
+from .auth import superadmin_required
+from .manager import READ_ALLOWED, server_status_required
+from .types import CORSOptions, WebMiddleware
+from .utils import pydantic_params_api_handler
+
+log = BraceStyleAdapter(logging.getLogger(__spec__.name))
+
+
+class AllowedGroups(BaseModel):
+    add: list[str] = []
+    remove: list[str] = []
+
+
+class PatchContainerRegistryRequestModel(BaseModel):
+    url: Optional[str] = None
+    type: Optional[str] = None
+    registry_name: Optional[str] = None
+    is_global: Optional[bool] = None
+    project: Optional[str] = None
+    username: Optional[str] = None
+    password: Optional[str] = None
+    ssl_verify: Optional[bool] = None
+    extra: Optional[str] = None
+    allowed_groups: Optional[AllowedGroups] = None
+
+
+# TODO: Add this. ContainerRegistryRow is not compatible with BaseModel
+# class PatchContainerRegistryResponseModel(BaseModel):
+#     container_registry: ContainerRegistryRow
+
+
+async def handle_allowed_groups_update(
+    db: ExtendedAsyncSAEngine, registry_id: uuid.UUID, allowed_group_updates: AllowedGroups
+):
+    async with db.begin_session() as db_sess:
+        if allowed_group_updates.add:
+            insert_values = [
+                {"registry_id": registry_id, "group_id": group_id}
+                for group_id in allowed_group_updates.add
+            ]
+
+            insert_query = sa.insert(AssociationContainerRegistriesGroupsRow).values(insert_values)
+            await db_sess.execute(insert_query)
+
+        if allowed_group_updates.remove:
+            delete_query = (
+                sa.delete(AssociationContainerRegistriesGroupsRow)
+                .where(AssociationContainerRegistriesGroupsRow.registry_id == registry_id)
+                .where(
+                    AssociationContainerRegistriesGroupsRow.group_id.in_(
+                        allowed_group_updates.remove
+                    )
+                )
+            )
+            await db_sess.execute(delete_query)
+
+
+@server_status_required(READ_ALLOWED)
+@superadmin_required
+@pydantic_params_api_handler(PatchContainerRegistryRequestModel)
+async def patch_container_registry(
+    request: web.Request, params: PatchContainerRegistryRequestModel
+) -> web.Response:
+    registry_id = uuid.UUID(request.match_info["registry_id"])
+    log.info("PATCH_CONTAINER_REGISTRY (cr:{})", registry_id)
+    root_ctx: RootContext = request.app["_root.context"]
+    input_config = params.model_dump(exclude={"allowed_groups"}, exclude_none=True)
+
+    async with root_ctx.db.begin_session() as db_session:
+        update_stmt = (
+            sa.update(ContainerRegistryRow)
+            .where(ContainerRegistryRow.id == registry_id)
+            .values(input_config)
+        )
+        await db_session.execute(update_stmt)
+
+        # select_stmt = sa.select(ContainerRegistryRow).where(ContainerRegistryRow.id == registry_id)
+        # updated_container_registry = await db_session.execute(select_stmt)
+
+    try:
+        if params.allowed_groups:
+            await handle_allowed_groups_update(root_ctx.db, registry_id, params.allowed_groups)
+    except IntegrityError as e:
+        raise GenericBadRequest(f"Failed to update allowed groups! Details: {str(e)}")
+    except Exception as e:
+        raise InternalServerError(f"Failed to update allowed groups! Details: {str(e)}")
+
+    # return PatchContainerRegistryResponseModel(container_registry=updated_container_registry)
+    return web.Response(status=204)
+
+
+def create_app(
+    default_cors_options: CORSOptions,
+) -> Tuple[web.Application, Iterable[WebMiddleware]]:
+    app = web.Application()
+    app["api_versions"] = (1, 2, 3, 4, 5)
+    app["prefix"] = "container-registries"
+    cors = aiohttp_cors.setup(app, defaults=default_cors_options)
+    cors.add(app.router.add_route("PATCH", "/{registry_id}", patch_container_registry))
+    return app, []
