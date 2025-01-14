9a4b126cbaa812e93b9b4ed4c2ce63391e46cee9
jopemachine <jopemachine@naver.com>
2025-01-14 06:25:14 +0000

    fix: Delete obsolete mutations

--- src/ai/backend/manager/models/gql_models/container_registry.py
+++ src/ai/backend/manager/models/gql_models/container_registry.py
@@ -4,75 +4,16 @@ import logging
 from typing import Self
 
 import graphene
-import sqlalchemy as sa
 
 from ai.backend.logging import BraceStyleAdapter
 
-from ..association_container_registries_groups import (
-    AssociationContainerRegistriesGroupsRow,
-)
-from ..base import BigInt, simple_db_mutate
+from ..base import BigInt
 from ..rbac import ScopeType
 from ..user import UserRole
 from .container_registry_utils import HarborQuotaManager
 from .fields import ScopeField
 
 log = BraceStyleAdapter(logging.getLogger(__spec__.name))  # type: ignore
-
-
-class AssociateContainerRegistryWithGroup(graphene.Mutation):
-    """Added in 25.2.0."""
-
-    allowed_roles = (UserRole.SUPERADMIN,)
-
-    class Arguments:
-        registry_id = graphene.String(required=True)
-        group_id = graphene.String(required=True)
-
-    ok = graphene.Boolean()
-    msg = graphene.String()
-
-    @classmethod
-    async def mutate(
-        cls,
-        root,
-        info: graphene.ResolveInfo,
-        registry_id: str,
-        group_id: str,
-    ) -> Self:
-        insert_query = sa.insert(AssociationContainerRegistriesGroupsRow).values({
-            "registry_id": registry_id,
-            "group_id": group_id,
-        })
-        return await simple_db_mutate(cls, info.context, insert_query)
-
-
-class DisassociateContainerRegistryWithGroup(graphene.Mutation):
-    """Added in 25.2.0."""
-
-    allowed_roles = (UserRole.SUPERADMIN,)
-
-    class Arguments:
-        registry_id = graphene.String(required=True)
-        group_id = graphene.String(required=True)
-
-    ok = graphene.Boolean()
-    msg = graphene.String()
-
-    @classmethod
-    async def mutate(
-        cls,
-        root,
-        info: graphene.ResolveInfo,
-        registry_id: str,
-        group_id: str,
-    ) -> Self:
-        delete_query = (
-            sa.delete(AssociationContainerRegistriesGroupsRow)
-            .where(AssociationContainerRegistriesGroupsRow.registry_id == registry_id)
-            .where(AssociationContainerRegistriesGroupsRow.group_id == group_id)
-        )
-        return await simple_db_mutate(cls, info.context, delete_query)
 
 
 class CreateContainerRegistryQuota(graphene.Mutation):
