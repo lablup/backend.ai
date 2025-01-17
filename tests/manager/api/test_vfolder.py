e867c076cf9296c2d90859b63e3016495a1732a2
octodog <mu001@lablup.com>
2025-01-17 15:28:44 +0900

    fix(BA-517): Raise exception if multiple VFolders exist in decorator (#3465) (#3485)
    Co-authored-by: HyeockJinKim <kherootz@gmail.com>

--- tests/manager/api/test_vfolder.py
+++ tests/manager/api/test_vfolder.py
@@ -1,11 +1,17 @@
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, Mock
 from uuid import UUID
 
 import pytest
+from aiohttp import web
 
 from ai.backend.manager.api import vfolder
-from ai.backend.manager.api.vfolder import with_vfolder_rows_resolved
-from ai.backend.manager.models.vfolder import VFolderPermissionSetAlias
+from ai.backend.manager.api.exceptions import TooManyVFoldersFound, VFolderNotFound
+from ai.backend.manager.api.vfolder import with_vfolder_rows_resolved, with_vfolder_status_checked
+from ai.backend.manager.models.vfolder import (
+    VFolderPermissionSetAlias,
+    VFolderRow,
+    VFolderStatusSet,
+)
 
 
 @pytest.mark.asyncio
@@ -27,3 +33,55 @@ async def test_uuid_or_name_resolution(monkeypatch: pytest.MonkeyPatch) -> None:
     await dummy_handler(mock_request)
     call = mock_resolver.await_args_list[1]
     assert isinstance(call.args[2], str)
+
+
+@pytest.mark.parametrize(
+    "vfolder_status",
+    [
+        VFolderStatusSet.ALL,
+        VFolderStatusSet.READABLE,
+        VFolderStatusSet.MOUNTABLE,
+        VFolderStatusSet.UPDATABLE,
+        VFolderStatusSet.DELETABLE,
+        VFolderStatusSet.PURGABLE,
+        VFolderStatusSet.RECOVERABLE,
+        VFolderStatusSet.INACCESSIBLE,
+    ],
+)
+async def test_too_many_vfolders(vfolder_status):
+    @with_vfolder_status_checked(vfolder_status)
+    async def too_many_vfolders_handler(request, row: VFolderRow):
+        return AsyncMock(return_value=web.Response(text="no response"))
+
+    mock_entry = {
+        "id": "fake-vfolder-id",
+        "host": "fake-vfolder-host",
+        "user_email": "fake-user@backend.ai",
+        "user": "fake-user",
+        "group_name": "fake-group",
+        "group": "fake-group-id",
+    }
+    with pytest.raises(TooManyVFoldersFound):
+        await too_many_vfolders_handler(Mock(), [mock_entry, mock_entry])
+
+
+@pytest.mark.parametrize(
+    "vfolder_status",
+    [
+        VFolderStatusSet.ALL,
+        VFolderStatusSet.READABLE,
+        VFolderStatusSet.MOUNTABLE,
+        VFolderStatusSet.UPDATABLE,
+        VFolderStatusSet.DELETABLE,
+        VFolderStatusSet.PURGABLE,
+        VFolderStatusSet.RECOVERABLE,
+        VFolderStatusSet.INACCESSIBLE,
+    ],
+)
+async def test_no_vfolders(vfolder_status):
+    @with_vfolder_status_checked(vfolder_status)
+    async def no_vfolders_handler(request, row: VFolderRow):
+        return AsyncMock(return_value=web.Response(text="no response"))
+
+    with pytest.raises(VFolderNotFound):
+        await no_vfolders_handler(Mock(), [])
