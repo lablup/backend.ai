74ec0b5974cd1f8e829c094719759798ae527f7b
Mincheol Kang <mincheol@lablup.com>
2025-02-04 14:39:19 +0900

    refactor(BA-627): Add type hints for instance variables (#3564)

--- src/ai/backend/storage/volumes/pool.py
+++ src/ai/backend/storage/volumes/pool.py
@@ -13,48 +13,52 @@ from .abc import AbstractVolume
 
 
 class VolumePool:
+    _volumes: dict[str, AbstractVolume]
+    _local_config: Mapping[str, Any]
+    _etcd: AsyncEtcd
+    _event_dispatcher: EventDispatcher
+    _event_producer: EventProducer
+
     def __init__(
         self,
         local_config: Mapping[str, Any],
         etcd: AsyncEtcd,
         event_dispatcher: EventDispatcher,
         event_producer: EventProducer,
-        backends: Mapping[str, Type[AbstractVolume]],
     ):
-        self.local_config = local_config
-        self.etcd = etcd
-        self.event_dispatcher = event_dispatcher
-        self.event_producer = event_producer
-        self.backends = backends
-        self.volumes: dict[str, AbstractVolume] = {}
+        self._volumes = {}
+        self._local_config = local_config
+        self._etcd = etcd
+        self._event_dispatcher = event_dispatcher
+        self._event_producer = event_producer
 
     def list_volumes(self) -> Mapping[str, VolumeInfo]:
         return {
             volume_id: VolumeInfo(**info)
-            for volume_id, info in self.local_config.get("volume", {}).items()
+            for volume_id, info in self._local_config["volume"].items()
         }
 
     @actxmgr
     async def get_volume(self, volume_id: str) -> AsyncIterator[AbstractVolume]:
-        if volume_id in self.volumes:
-            yield self.volumes[volume_id]
+        if volume_id in self._volumes:
+            yield self._volumes[volume_id]
         else:
             try:
-                volume_config = self.local_config["volume"][volume_id]
+                volume_config = self._local_config["volume"][volume_id]
             except KeyError:
                 raise InvalidVolumeError(volume_id)
 
-            volume_cls: Type[AbstractVolume] = self.backends[volume_config["backend"]]
+            volume_cls: Type[AbstractVolume] = self._backends[volume_config["backend"]]
             volume_obj = volume_cls(
-                local_config=self.local_config,
+                local_config=self._local_config,
                 mount_path=Path(volume_config["path"]),
-                options=volume_config.get("options", {}),
-                etcd=self.etcd,
-                event_dispatcher=self.event_dispatcher,
-                event_producer=self.event_producer,
+                etcd=self._etcd,
+                event_dispatcher=self._event_dispatcher,
+                event_producer=self._event_producer,
+                options=volume_config["options"] or {},
             )
 
             await volume_obj.init()
-            self.volumes[volume_id] = volume_obj
+            self._volumes[volume_id] = volume_obj
 
             yield volume_obj
