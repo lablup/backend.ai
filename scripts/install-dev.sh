0b75c935187a6c8f81e0f231c00a89f871dd2057
Jeongseok Kang <jskang@lablup.com>
2025-02-12 03:01:21 +0900

    fix(BA-714): Correct `mkdir` flag order for grafana-data

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -849,7 +849,7 @@ setup_environment() {
   mkdir -p "${HALFSTACK_VOLUME_PATH}/postgres-data"
   mkdir -p "${HALFSTACK_VOLUME_PATH}/etcd-data"
   mkdir -p "${HALFSTACK_VOLUME_PATH}/redis-data"
-  mkdir -p "${HALFSTACK_VOLUME_PATH}/grafana-data" -m 757
+  mkdir -m 757 -p "${HALFSTACK_VOLUME_PATH}/grafana-data"
 
   $docker_sudo docker compose -f "docker-compose.halfstack.current.yml" pull
 
