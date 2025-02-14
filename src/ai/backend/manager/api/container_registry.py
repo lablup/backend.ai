096670ca36831fff420a8c2bd4ea8d69489a2dcc
jopemachine <jopemachine@naver.com>
2025-02-14 04:20:43 +0000

    fix: Broken CI

--- src/ai/backend/manager/api/container_registry.py
+++ src/ai/backend/manager/api/container_registry.py
@@ -27,6 +27,7 @@ from ai.backend.manager.models.container_registry import (
 from ai.backend.manager.models.gql_models.container_registry_v2 import handle_allowed_groups_update
 
 from .exceptions import (
+    ContainerRegistryGroupsAssociationNotFound,
     ContainerRegistryNotFound,
     GenericBadRequest,
     HarborWebhookContainerRegistryRowNotFound,
@@ -72,7 +73,7 @@ async def patch_container_registry(
 
         if params.allowed_groups:
             await handle_allowed_groups_update(root_ctx.db, registry_id, params.allowed_groups)
-    except ContainerRegistryNotFound as e:
+    except (ContainerRegistryNotFound, ContainerRegistryGroupsAssociationNotFound) as e:
         raise e
     except IntegrityError as e:
         raise GenericBadRequest(f"Failed to update allowed groups! Details: {str(e)}")
