3dbf5b1537bd8b784655ee107bba3fdd9be37660
jopemachine <jopemachine@naver.com>
2025-02-19 06:25:09 +0000

    misc: `25.3.1`

--- src/ai/backend/manager/models/gql_models/agent.py
+++ src/ai/backend/manager/models/gql_models/agent.py
@@ -119,7 +119,7 @@ class AgentNode(graphene.ObjectType):
     auto_terminate_abusing_kernel = graphene.Boolean()
     local_config = graphene.JSONString()
     container_count = graphene.Int()
-    gpu_alloc_map = graphene.JSONString(description="Added in 25.3.0.")
+    gpu_alloc_map = graphene.JSONString(description="Added in 25.3.1.")
 
     kernel_nodes = PaginatedConnectionField(
         KernelConnection,
@@ -335,7 +335,7 @@ class Agent(graphene.ObjectType):
     auto_terminate_abusing_kernel = graphene.Boolean()
     local_config = graphene.JSONString()
     container_count = graphene.Int()
-    gpu_alloc_map = graphene.JSONString(description="Added in 25.3.0.")
+    gpu_alloc_map = graphene.JSONString(description="Added in 25.3.1.")
 
     # Legacy fields
     mem_slots = graphene.Int()
