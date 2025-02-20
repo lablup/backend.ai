332af09b10d08d0d5c6ecd7da831a15c987b18ea
Sion Kang <siontama@gmail.com>
2025-02-20 17:33:01 +0900

    Update check-alembic-revision.py

--- scripts/check-alembic-revision.py
+++ scripts/check-alembic-revision.py
@@ -15,15 +15,11 @@ def check_alembic_file_for_pass(file_path):
     downgrade_is_empty = True
 
     for node in ast.walk(tree):
-        if isinstance(node, ast.FunctionDef):
-            if node.name == "upgrade":
-                for stmt in node.body:
-                    if not isinstance(stmt, ast.Pass):
-                        upgrade_is_empty = False
-            elif node.name == "downgrade":
-                for stmt in node.body:
-                    if not isinstance(stmt, ast.Pass):
-                        downgrade_is_empty = False
+      if isinstance(node, ast.FunctionDef):
+          if node.name == "upgrade":
+              upgrade_is_empty = not any(not isinstance(stmt, ast.Pass) for stmt in node.body)
+          elif node.name == "downgrade":
+              downgrade_is_empty = not any(not isinstance(stmt, ast.Pass) for stmt in node.body)
 
     print(f"Upgrade function {'is empty' if upgrade_is_empty else 'is not empty'}.")
     print(f"Downgrade function {'is empty' if downgrade_is_empty else 'is not empty'}.")
