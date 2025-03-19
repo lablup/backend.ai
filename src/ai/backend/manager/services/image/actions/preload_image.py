22a7ff77a3ade9e99da81603e1c0e83316b88f32
jopemachine <jopemachine@naver.com>
2025-03-19 04:56:48 +0000

    fix: Add `preload_image`, `unload_image`

---
+++ src/ai/backend/manager/services/image/actions/preload_image.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import Optional, override
+
+from ai.backend.manager.actions.action import BaseActionResult
+from ai.backend.manager.services.image.base import ImageAction
+
+
+@dataclass
+class PreloadImageAction(ImageAction):
+    references: list[str]
+    agents: list[str]
+
+    @override
+    def entity_id(self) -> str:
+        # TODO: ?
+        return f"{self.references} on {self.agents}"
+
+    @override
+    def operation_type(self):
+        return "preload_image"
+
+
+@dataclass
+class PreloadImageActionResult(BaseActionResult):
+    @override
+    def entity_id(self) -> Optional[str]:
+        return None
+
+    @override
+    def status(self) -> str:
+        return "success"
+
+    @override
+    def description(self) -> Optional[str]:
+        return ""
+
+    # def __eq__(self, other: Any) -> bool:
+    #     if not isinstance(other, AliasImageActionResult):
+    #         return False
+    #     # TODO: 여기선 id로 비교못할 듯.
+    #     return self.image_alias.alias == other.image_alias.alias
