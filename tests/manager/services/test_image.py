bc0db2875e0a4db60e4d83c304975a45678731a1
jopemachine <jopemachine@naver.com>
2025-03-24 02:33:25 +0000

    fix: Update test cases

--- tests/manager/services/test_image.py
+++ tests/manager/services/test_image.py
@@ -1,16 +1,16 @@
+import dataclasses
 import uuid
+from dataclasses import replace
 
 import pytest
+from dateutil.parser import isoparse
 
 from ai.backend.manager.api.exceptions import ImageNotFound
+from ai.backend.manager.data.image.types import ImageData
 from ai.backend.manager.models.image import ImageAliasRow, ImageRow, ImageStatus, ImageType
 from ai.backend.manager.models.user import UserRole
 from ai.backend.manager.server import (
     agent_registry_ctx,
-)
-from ai.backend.manager.services.image.actions.dealias_image import (
-    DealiasImageAction,
-    DealiasImageActionResult,
 )
 from ai.backend.manager.services.image.actions.forget_image import (
     ForgetImageAction,
@@ -31,11 +31,14 @@ from .conftest import TestScenario
 IMAGE_ROW_FIXTURE = ImageRow(
     name="cr.backend.ai/stable/python:3.9-ubuntu20.04",
     image="stable/python",
+    created_at=isoparse("2021-01-01T00:00:00Z"),
     project="stable",
     registry="cr.backend.ai",
-    registry_id="11111111-1111-1111-1111-111111111111",
+    registry_id=uuid.UUID("11111111-1111-1111-1111-111111111111"),
     architecture="x86_64",
-    config_digest="sha256:abcdefgh0123456789abcdefgh0123456789abcdefgh0123456789abcd",
+    config_digest="sha256:abcdefgh0123456789abcdefgh0123456789abcdefgh0123456789abcd".ljust(
+        72, " "
+    ),  # config_digest column is sa.CHAR(length=72)
     size_bytes=12345678,
     is_local=False,
     type=ImageType.COMPUTE,
@@ -49,6 +52,13 @@ IMAGE_ALIAS_ROW_FIXTURE = ImageAliasRow(
     id=uuid.uuid4(),
     alias="python",
     image_id=IMAGE_ROW_FIXTURE.id,
+)
+
+IMAGE_FIXTURE_DATA = ImageData.from_image_row(IMAGE_ROW_FIXTURE)
+
+
+IMAGE_FIXTURE_DICT = dataclasses.asdict(
+    dataclasses.replace(IMAGE_FIXTURE_DATA, type=ImageType.COMPUTE._name_)  # type: ignore
 )
 
 
@@ -70,7 +80,7 @@ def processors(extra_fixtures, database_fixture, database_engine):
                 reference=IMAGE_ROW_FIXTURE.name,
                 architecture=IMAGE_ROW_FIXTURE.architecture,
             ),
-            ForgetImageActionResult(image=IMAGE_ROW_FIXTURE),
+            ForgetImageActionResult(image=replace(IMAGE_FIXTURE_DATA, status=ImageStatus.DELETED)),
         ),
         TestScenario.failure(
             "When the user is not SUPERADMIN, and the user is not the image's owner, raise Generic Forbidden Error",
@@ -101,22 +111,7 @@ def processors(extra_fixtures, database_fixture, database_engine):
     [
         {
             "images": [
-                {
-                    "id": str(IMAGE_ROW_FIXTURE.id),
-                    "name": IMAGE_ROW_FIXTURE.name,
-                    "image": IMAGE_ROW_FIXTURE.image,
-                    "project": IMAGE_ROW_FIXTURE.project,
-                    "registry": IMAGE_ROW_FIXTURE.registry,
-                    "registry_id": IMAGE_ROW_FIXTURE.registry_id,
-                    "architecture": IMAGE_ROW_FIXTURE.architecture,
-                    "config_digest": IMAGE_ROW_FIXTURE.config_digest,
-                    "size_bytes": IMAGE_ROW_FIXTURE.size_bytes,
-                    "is_local": IMAGE_ROW_FIXTURE.is_local,
-                    "type": IMAGE_ROW_FIXTURE.type._name_,
-                    "labels": IMAGE_ROW_FIXTURE.labels,
-                    "resources": IMAGE_ROW_FIXTURE.resources,
-                    "status": IMAGE_ROW_FIXTURE.status.value,
-                }
+                IMAGE_FIXTURE_DICT,
             ]
         }
     ],
@@ -126,7 +121,6 @@ async def test_forget_image(
     processors: ImageProcessors,
     test_scenario: TestScenario[ForgetImageAction, ForgetImageActionResult],
 ):
-    # TODO: forget_image가 성공했을 때 DB에 내용 반영된 것은 여기에 별도로 로직 넣어 확인해야 하는지?
     await test_scenario.test(processors.forget_image.wait_for_complete)
 
 
@@ -141,7 +135,9 @@ async def test_forget_image(
                 client_role=UserRole.SUPERADMIN,
                 image_id=IMAGE_ROW_FIXTURE.id,
             ),
-            ForgetImageByIdActionResult(image=IMAGE_ROW_FIXTURE),
+            ForgetImageByIdActionResult(
+                image=replace(IMAGE_FIXTURE_DATA, status=ImageStatus.DELETED)
+            ),
         ),
         TestScenario.failure(
             "When the user is not SUPERADMIN, and the user is not the image's owner, raise Generic Forbidden Error",
@@ -168,22 +164,7 @@ async def test_forget_image(
     [
         {
             "images": [
-                {
-                    "id": str(IMAGE_ROW_FIXTURE.id),
-                    "name": IMAGE_ROW_FIXTURE.name,
-                    "image": IMAGE_ROW_FIXTURE.image,
-                    "project": IMAGE_ROW_FIXTURE.project,
-                    "registry": IMAGE_ROW_FIXTURE.registry,
-                    "registry_id": IMAGE_ROW_FIXTURE.registry_id,
-                    "architecture": IMAGE_ROW_FIXTURE.architecture,
-                    "config_digest": IMAGE_ROW_FIXTURE.config_digest,
-                    "size_bytes": IMAGE_ROW_FIXTURE.size_bytes,
-                    "is_local": IMAGE_ROW_FIXTURE.is_local,
-                    "type": IMAGE_ROW_FIXTURE.type._name_,
-                    "labels": IMAGE_ROW_FIXTURE.labels,
-                    "resources": IMAGE_ROW_FIXTURE.resources,
-                    "status": IMAGE_ROW_FIXTURE.status.value,
-                }
+                IMAGE_FIXTURE_DICT,
             ]
         }
     ],
@@ -196,125 +177,71 @@ async def test_forget_image_by_id(
     await test_scenario.test(processors.forget_image_by_id.wait_for_complete)
 
 
-# TODO: 아래 테스트는 작동하지만, image 삭제할 때 image alias fk 위반 에러 발생함.
+# # TODO: 아래 테스트는 작동하지만, image 삭제할 때 image alias fk 위반 에러 발생함.
+# # @pytest.mark.asyncio
+# # @pytest.mark.parametrize(
+# #     "test_scenario",
+# #     [
+# #         TestScenario.success(
+# #             "Success Case",
+# #             AliasImageAction(
+# #                 image_canonical=IMAGE_ROW_FIXTURE.name,
+# #                 architecture=IMAGE_ROW_FIXTURE.architecture,
+# #                 alias="new-alias",
+# #             ),
+# #             AliasImageActionResult(image_alias=ImageAliasRow(
+# #                 id=uuid.uuid4(),
+# #                 alias="new-alias",
+# #                 image_id=IMAGE_ROW_FIXTURE.id,
+# #             )),
+# #         ),
+# #     ],
+# # )
+# # @pytest.mark.parametrize(
+# #     "extra_fixtures",
+# #     [
+# #         {
+# #             "images": [
+# #                 {
+# #                     "id": str(IMAGE_ROW_FIXTURE.id),
+# #                     "name": IMAGE_ROW_FIXTURE.name,
+# #                     "image": IMAGE_ROW_FIXTURE.image,
+# #                     "project": IMAGE_ROW_FIXTURE.project,
+# #                     "registry": IMAGE_ROW_FIXTURE.registry,
+# #                     "registry_id": IMAGE_ROW_FIXTURE.registry_id,
+# #                     "architecture": IMAGE_ROW_FIXTURE.architecture,
+# #                     "config_digest": IMAGE_ROW_FIXTURE.config_digest,
+# #                     "size_bytes": IMAGE_ROW_FIXTURE.size_bytes,
+# #                     "is_local": IMAGE_ROW_FIXTURE.is_local,
+# #                     "type": IMAGE_ROW_FIXTURE.type._name_,
+# #                     "labels": IMAGE_ROW_FIXTURE.labels,
+# #                     "resources": IMAGE_ROW_FIXTURE.resources,
+# #                     "status": IMAGE_ROW_FIXTURE.status.value,
+# #                 }
+# #             ],
+# #         }
+# #     ],
+# #     ids=[""],
+# # )
+# # async def test_alias_image(
+# #     processors: ImageProcessors,
+# #     test_scenario: TestScenario[AliasImageAction, AliasImageActionResult],
+# # ):
+# #     await test_scenario.test(processors.alias_image.wait_for_complete)
+
+
 # @pytest.mark.asyncio
 # @pytest.mark.parametrize(
 #     "test_scenario",
 #     [
 #         TestScenario.success(
 #             "Success Case",
-#             AliasImageAction(
-#                 image_canonical=IMAGE_ROW_FIXTURE.name,
-#                 architecture=IMAGE_ROW_FIXTURE.architecture,
-#                 alias="new-alias",
+#             DealiasImageAction(
+#                 alias=IMAGE_ALIAS_ROW_FIXTURE.alias,
 #             ),
-#             AliasImageActionResult(image_alias=ImageAliasRow(
-#                 id=uuid.uuid4(),
-#                 alias="new-alias",
-#                 image_id=IMAGE_ROW_FIXTURE.id,
-#             )),
-#         ),
-#     ],
-# )
-# @pytest.mark.parametrize(
-#     "extra_fixtures",
-#     [
-#         {
-#             "images": [
-#                 {
-#                     "id": str(IMAGE_ROW_FIXTURE.id),
-#                     "name": IMAGE_ROW_FIXTURE.name,
-#                     "image": IMAGE_ROW_FIXTURE.image,
-#                     "project": IMAGE_ROW_FIXTURE.project,
-#                     "registry": IMAGE_ROW_FIXTURE.registry,
-#                     "registry_id": IMAGE_ROW_FIXTURE.registry_id,
-#                     "architecture": IMAGE_ROW_FIXTURE.architecture,
-#                     "config_digest": IMAGE_ROW_FIXTURE.config_digest,
-#                     "size_bytes": IMAGE_ROW_FIXTURE.size_bytes,
-#                     "is_local": IMAGE_ROW_FIXTURE.is_local,
-#                     "type": IMAGE_ROW_FIXTURE.type._name_,
-#                     "labels": IMAGE_ROW_FIXTURE.labels,
-#                     "resources": IMAGE_ROW_FIXTURE.resources,
-#                     "status": IMAGE_ROW_FIXTURE.status.value,
-#                 }
-#             ],
-#         }
-#     ],
-#     ids=[""],
-# )
-# async def test_alias_image(
-#     processors: ImageProcessors,
-#     test_scenario: TestScenario[AliasImageAction, AliasImageActionResult],
-# ):
-#     await test_scenario.test(processors.alias_image.wait_for_complete)
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "test_scenario",
-    [
-        TestScenario.success(
-            "Success Case",
-            DealiasImageAction(
-                alias=IMAGE_ALIAS_ROW_FIXTURE.alias,
-            ),
-            DealiasImageActionResult(
-                image_id=IMAGE_ALIAS_ROW_FIXTURE.image_id, image_alias=IMAGE_ALIAS_ROW_FIXTURE
-            ),
-        ),
-    ],
-)
-@pytest.mark.parametrize(
-    "extra_fixtures",
-    [
-        {
-            "images": [
-                {
-                    "id": str(IMAGE_ROW_FIXTURE.id),
-                    "name": IMAGE_ROW_FIXTURE.name,
-                    "image": IMAGE_ROW_FIXTURE.image,
-                    "project": IMAGE_ROW_FIXTURE.project,
-                    "registry": IMAGE_ROW_FIXTURE.registry,
-                    "registry_id": IMAGE_ROW_FIXTURE.registry_id,
-                    "architecture": IMAGE_ROW_FIXTURE.architecture,
-                    "config_digest": IMAGE_ROW_FIXTURE.config_digest,
-                    "size_bytes": IMAGE_ROW_FIXTURE.size_bytes,
-                    "is_local": IMAGE_ROW_FIXTURE.is_local,
-                    "type": IMAGE_ROW_FIXTURE.type._name_,
-                    "labels": IMAGE_ROW_FIXTURE.labels,
-                    "resources": IMAGE_ROW_FIXTURE.resources,
-                    "status": IMAGE_ROW_FIXTURE.status.value,
-                }
-            ],
-            "image_aliases": [
-                {
-                    "id": str(IMAGE_ALIAS_ROW_FIXTURE.id),
-                    "alias": IMAGE_ALIAS_ROW_FIXTURE.alias,
-                    "image": str(IMAGE_ROW_FIXTURE.id),
-                }
-            ],
-        }
-    ],
-    ids=[""],
-)
-async def test_dealias_image(
-    processors: ImageProcessors,
-    test_scenario: TestScenario[DealiasImageAction, DealiasImageActionResult],
-):
-    await test_scenario.test(processors.dealias_image.wait_for_complete)
-
-
-# TODO: 일단 ClearImages에서 뭔가를 리턴해야...?
-# @pytest.mark.asyncio
-# @pytest.mark.parametrize(
-#     "test_scenario",
-#     [
-#         TestScenario.success(
-#             "Success Case",
-#             ClearImagesAction(
-#                 registry=IMAGE_ROW_FIXTURE.registry,
+#             DealiasImageActionResult(
+#                 image_id=IMAGE_ALIAS_ROW_FIXTURE.image_id, image_alias=IMAGE_ALIAS_ROW_FIXTURE
 #             ),
-#             ClearImagesActionResult(),
 #         ),
 #     ],
 # )
@@ -351,27 +278,81 @@ async def test_dealias_image(
 #     ],
 #     ids=[""],
 # )
-# async def test_clear_images(
+# async def test_dealias_image(
 #     processors: ImageProcessors,
-#     test_scenario: TestScenario[ClearImagesAction, ClearImagesActionResult],
+#     test_scenario: TestScenario[DealiasImageAction, DealiasImageActionResult],
 # ):
-#     await test_scenario.test(processors.clear_images.wait_for_complete)
+#     await test_scenario.test(processors.dealias_image.wait_for_complete)
 
 
-# @pytest.mark.parametrize(
-#     "test_scenario",
-#     [
-#         TestScenario.success("Success Case", PurgeImagesAction(), PurgeImagesActionResult()),
-#         TestScenario.failure(
-#             "When No Image exists, raise Image Not Found Error",
-#             PurgeImagesAction(),
-#             BackendError,
-#         ),
-#     ],
-# )
-# def test_purge_images(
-#     test_scenario: TestScenario[PurgeImagesAction, PurgeImagesActionResult],
-#     processors: ImageProcessors,
-# ):
-#     # test_scenario.test(processors.purge_images.fire_and_forget)
-#     pass
+# # TODO: 일단 ClearImages에서 뭔가를 리턴해야...?
+# # @pytest.mark.asyncio
+# # @pytest.mark.parametrize(
+# #     "test_scenario",
+# #     [
+# #         TestScenario.success(
+# #             "Success Case",
+# #             ClearImagesAction(
+# #                 registry=IMAGE_ROW_FIXTURE.registry,
+# #             ),
+# #             ClearImagesActionResult(),
+# #         ),
+# #     ],
+# # )
+# # @pytest.mark.parametrize(
+# #     "extra_fixtures",
+# #     [
+# #         {
+# #             "images": [
+# #                 {
+# #                     "id": str(IMAGE_ROW_FIXTURE.id),
+# #                     "name": IMAGE_ROW_FIXTURE.name,
+# #                     "image": IMAGE_ROW_FIXTURE.image,
+# #                     "project": IMAGE_ROW_FIXTURE.project,
+# #                     "registry": IMAGE_ROW_FIXTURE.registry,
+# #                     "registry_id": IMAGE_ROW_FIXTURE.registry_id,
+# #                     "architecture": IMAGE_ROW_FIXTURE.architecture,
+# #                     "config_digest": IMAGE_ROW_FIXTURE.config_digest,
+# #                     "size_bytes": IMAGE_ROW_FIXTURE.size_bytes,
+# #                     "is_local": IMAGE_ROW_FIXTURE.is_local,
+# #                     "type": IMAGE_ROW_FIXTURE.type._name_,
+# #                     "labels": IMAGE_ROW_FIXTURE.labels,
+# #                     "resources": IMAGE_ROW_FIXTURE.resources,
+# #                     "status": IMAGE_ROW_FIXTURE.status.value,
+# #                 }
+# #             ],
+# #             "image_aliases": [
+# #                 {
+# #                     "id": str(IMAGE_ALIAS_ROW_FIXTURE.id),
+# #                     "alias": IMAGE_ALIAS_ROW_FIXTURE.alias,
+# #                     "image": str(IMAGE_ROW_FIXTURE.id),
+# #                 }
+# #             ],
+# #         }
+# #     ],
+# #     ids=[""],
+# # )
+# # async def test_clear_images(
+# #     processors: ImageProcessors,
+# #     test_scenario: TestScenario[ClearImagesAction, ClearImagesActionResult],
+# # ):
+# #     await test_scenario.test(processors.clear_images.wait_for_complete)
+
+
+# # @pytest.mark.parametrize(
+# #     "test_scenario",
+# #     [
+# #         TestScenario.success("Success Case", PurgeImagesAction(), PurgeImagesActionResult()),
+# #         TestScenario.failure(
+# #             "When No Image exists, raise Image Not Found Error",
+# #             PurgeImagesAction(),
+# #             BackendError,
+# #         ),
+# #     ],
+# # )
+# # def test_purge_images(
+# #     test_scenario: TestScenario[PurgeImagesAction, PurgeImagesActionResult],
+# #     processors: ImageProcessors,
+# # ):
+# #     # test_scenario.test(processors.purge_images.fire_and_forget)
+# #     pass
