cf31bc2bcecf636f7977876bc9c9ae342b702d8a
HyeockJinKim <hyeokjin@lablup.com>
2025-03-28 12:01:00 +0900

    Add changelog

--- src/ai/backend/manager/actions/processor.py
+++ src/ai/backend/manager/actions/processor.py
@@ -25,17 +25,19 @@ class ActionProcessor(Generic[TAction, TActionResult]):
 
     async def _run(self, action: TAction) -> TActionResult:
         started_at = datetime.now()
-        status: str
+        status: str = "unknown"
+        description: str = "unknown"
+        result: Optional[TActionResult] = None
         for monitor in self._monitors:
             await monitor.prepare(action)
         try:
             result = await self._func(action)
             status = "success"
             description = "Success"
-        except Exception as e:
+        except BaseException as e:
             status = "error"
             description = str(e)
-            raise e
+            raise
         finally:
             end_at = datetime.now()
             duration = (end_at - started_at).total_seconds()
@@ -49,7 +51,9 @@ class ActionProcessor(Generic[TAction, TActionResult]):
             process_result = ProcessResult(meta=meta, result=result)
             for monitor in reversed(self._monitors):
                 await monitor.done(action, process_result)
-            return result
+        if result is None:
+            raise RuntimeError("not set result")
+        return result
 
     async def wait_for_complete(self, action: TAction) -> TActionResult:
         return await self._run(action)
