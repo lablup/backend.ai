101db68f0f9a0213cc870e4e837e5bca33e7ba87
jopemachine <jopemachine@naver.com>
2025-03-29 03:25:22 +0000

    fix: Add `create_cluster`

--- src/ai/backend/manager/server.py
+++ src/ai/backend/manager/server.py
@@ -440,6 +440,7 @@ async def processors_ctx(root_ctx: RootContext) -> AsyncIterator[None]:
         stats_monitor=root_ctx.stats_monitor,
         event_producer=root_ctx.event_producer,
         background_task_manager=root_ctx.background_task_manager,
+        error_monitor=root_ctx.error_monitor,
     )
 
     root_ctx.processors = Processors(
