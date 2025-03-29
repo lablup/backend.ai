0ffa4a42365135bc6245036ddce8843971a36482
jopemachine <jopemachine@naver.com>
2025-03-29 01:31:42 +0000

    fix: Add `complete`

---
+++ src/ai/backend/manager/services/session/actions/complete.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Any, Mapping, Optional, override
+
+from ai.backend.common.types import AccessKey
+from ai.backend.manager.actions.action import BaseActionResult
+from ai.backend.manager.models.session import SessionRow
+from ai.backend.manager.services.session.base import SessionAction
+
+
+# TODO: Rename this?
+@dataclass
+class CompleteAction(SessionAction):
+    session_name: str
+    requester_access_key: AccessKey
+    owner_access_key: AccessKey
+
+    code: str
+    # TODO: Add type
+    options: Optional[Mapping[str, Any]] = None
+
+    @override
+    def entity_id(self) -> Optional[str]:
+        return None
+
+    @override
+    def operation_type(self):
+        return "complete"
+
+
+@dataclass
+class CompleteActionResult(BaseActionResult):
+    # TODO: Add SessionData type
+    session_row: SessionRow
+
+    # TODO: Add proper type
+    result: dict[str, Any]
+
+    @override
+    def entity_id(self) -> Optional[str]:
+        return str(self.session_row.session_id)
--- src/ai/backend/manager/services/session/processors.py
+++ src/ai/backend/manager/services/session/processors.py
@@ -3,11 +3,17 @@ from ai.backend.manager.services.session.actions.commit_session import (
     CommitSessionAction,
     CommitSessionActionResult,
 )
+from ai.backend.manager.services.session.actions.complete import (
+    CompleteAction,
+    CompleteActionResult,
+)
 from ai.backend.manager.services.session.service import SessionService
 
 
 class SessionProcessors:
     commit_session: ActionProcessor[CommitSessionAction, CommitSessionActionResult]
+    complete: ActionProcessor[CompleteAction, CompleteActionResult]
 
     def __init__(self, service: SessionService) -> None:
         self.commit_session = ActionProcessor(service.commit_session)
+        self.complete = ActionProcessor(service.complete)
