868f6f3282528415a8fea97792e9c4bf23b2dfe8
BoGyum Kim <bkkim@lablup.com>
2025-04-01 13:24:28 +0900

    feat: Introduce TriState for gql mutation

--- src/ai/backend/manager/types.py
+++ src/ai/backend/manager/types.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import enum
 import uuid
-from typing import TYPE_CHECKING, Annotated, Protocol
+from typing import TYPE_CHECKING, Annotated, Any, Generic, Optional, Protocol, TypeVar
 
 import attr
 from pydantic import AliasChoices, BaseModel, Field
@@ -56,3 +56,53 @@ class MountOptionModel(BaseModel):
         MountPermission | None,
         Field(validation_alias=AliasChoices("permission", "perm"), default=None),
     ]
+
+
+TVal = TypeVar("TVal")
+
+
+class TriState(Generic[TVal]):
+    _attr_name: str
+    _unset: bool
+    _value: Optional[TVal]
+
+    def __init__(self, attr_name: str, unset: bool, value: Optional[TVal]):
+        self._attr_name = attr_name
+        self._unset = unset
+        self._value = value
+
+    @classmethod
+    def set(cls, attr_name: str, value: TVal) -> TriState[TVal]:
+        return cls(attr_name, False, value)
+
+    @classmethod
+    def unset(cls, attr_name: str) -> TriState[TVal]:
+        return cls(attr_name, True, None)
+
+    @classmethod
+    def nop(cls, attr_name: str) -> TriState[TVal]:
+        return cls(attr_name, False, None)
+
+    def set_attr(self, row: Any):
+        if self._unset:
+            # TODO: Is this necessary?
+            assert self._value is None
+            setattr(row, self._attr_name, None)
+        else:
+            if self._value is not None:
+                setattr(row, self._attr_name, self._value)
+            else:
+                pass
+
+
+class OptionalState(TriState[TVal]):
+    def __init__(self, attr_name: str, value: Optional[TVal]):
+        super().__init__(attr_name, False, value)
+
+    @classmethod
+    def set(cls, attr_name: str, value: TVal) -> OptionalState[TVal]:
+        return cls(attr_name, value)
+
+    @classmethod
+    def nop(cls, attr_name: str) -> OptionalState[TVal]:
+        return cls(attr_name, None)
