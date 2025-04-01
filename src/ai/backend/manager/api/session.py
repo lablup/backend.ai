a5aaff1f1a94bdb0708f4f5ba0f6f403d14025af
jopemachine <jopemachine@naver.com>
2025-04-01 04:27:46 +0000

    fix: Add `restart_session`

--- src/ai/backend/manager/api/session.py
+++ src/ai/backend/manager/api/session.py
@@ -82,6 +82,7 @@ from ai.backend.manager.services.session.actions.interrupt import InterruptActio
 from ai.backend.manager.services.session.actions.list_files import ListFilesAction
 from ai.backend.manager.services.session.actions.match_sessions import MatchSessionsAction
 from ai.backend.manager.services.session.actions.rename_session import RenameSessionAction
+from ai.backend.manager.services.session.actions.restart_session import RestartSessionAction
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncConnection as SAConnection
@@ -1291,16 +1292,14 @@ async def restart(request: web.Request, params: Any) -> web.Response:
     session_name = request.match_info["session_name"]
     requester_access_key, owner_access_key = await get_access_key_scopes(request, params)
     log.info("RESTART (ak:{0}/{1}, s:{2})", requester_access_key, owner_access_key, session_name)
-    async with root_ctx.db.begin_session() as db_sess:
-        session = await SessionRow.get_session(
-            db_sess,
-            session_name,
-            owner_access_key,
-            kernel_loading_strategy=KernelLoadingStrategy.ALL_KERNELS,
-        )
+
     try:
-        await root_ctx.registry.increment_session_usage(session)
-        await root_ctx.registry.restart_session(session)
+        await root_ctx.processors.session.restart_session.wait_for_complete(
+            RestartSessionAction(
+                session_name=session_name,
+                owner_access_key=owner_access_key,
+            )
+        )
     except BackendError:
         log.exception("RESTART: exception")
         raise
