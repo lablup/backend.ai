499eee7a7b3c0843c082385cd834a046c7a4c2de
jopemachine <jopemachine@naver.com>
2025-04-04 03:52:58 +0000

    feat: Add AuditLog reporter

--- src/ai/backend/manager/services/processors.py
+++ src/ai/backend/manager/services/processors.py
@@ -1,3 +1,4 @@
+from ai.backend.manager.actions.monitors.monitor import ActionMonitor
 from ai.backend.manager.services.container_registry.processors import ContainerRegistryProcessors
 from ai.backend.manager.services.container_registry.service import ContainerRegistryService
 from ai.backend.manager.services.image.processors import ImageProcessors
@@ -9,7 +10,12 @@ class Processors:
     container_registry: ContainerRegistryProcessors
 
     def __init__(
-        self, image_service: ImageService, container_registry_service: ContainerRegistryService
+        self,
+        image_service: ImageService,
+        container_registry_service: ContainerRegistryService,
+        action_monitors: list[ActionMonitor],
     ) -> None:
-        self.image = ImageProcessors(image_service)
-        self.container_registry = ContainerRegistryProcessors(container_registry_service)
+        self.image = ImageProcessors(image_service, action_monitors)
+        self.container_registry = ContainerRegistryProcessors(
+            container_registry_service, action_monitors
+        )
