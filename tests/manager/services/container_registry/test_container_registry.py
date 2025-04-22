a63a554ebfbac3a7912e988fc8bc4ff26319d525
jopemachine <gbl@lablup.com>
2025-04-22 07:01:47 +0000

    fix: Add purge images test

--- tests/manager/services/container_registry/test_container_registry.py
+++ tests/manager/services/container_registry/test_container_registry.py
@@ -11,19 +11,68 @@ from ai.backend.manager.data.image.types import (
     ImageStatus,
     ImageType,
 )
+from ai.backend.manager.services.container_registry.actions.clear_images import (
+    ClearImagesAction,
+    ClearImagesActionResult,
+)
 from ai.backend.manager.services.container_registry.actions.rescan_images import (
     RescanImagesAction,
     RescanImagesActionResult,
 )
 from ai.backend.manager.services.container_registry.processors import ContainerRegistryProcessors
+from ai.backend.manager.services.container_registry.service import ContainerRegistryService
 from ai.backend.testutils.mock import mock_aioresponses_sequential_payloads, setup_dockerhub_mocking
 
-from ...fixtures import (
+from ..fixtures import (
     CONTAINER_REGISTRY_FIXTURE_DATA,
     CONTAINER_REGISTRY_FIXTURE_DICT,
     CONTAINER_REGISTRY_ROW_FIXTURE,
+    IMAGE_FIXTURE_DICT,
 )
-from ...test_utils import TestScenario
+from ..test_utils import TestScenario
+
+
+@pytest.fixture
+def processors(extra_fixtures, database_fixture, database_engine):
+    container_registry_service = ContainerRegistryService(
+        db=database_engine,
+    )
+    return ContainerRegistryProcessors(container_registry_service, [])
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "test_scenario",
+    [
+        TestScenario.success(
+            "Success Case",
+            ClearImagesAction(
+                registry=CONTAINER_REGISTRY_ROW_FIXTURE.registry_name,
+                project=CONTAINER_REGISTRY_ROW_FIXTURE.project,
+            ),
+            ClearImagesActionResult(registry=CONTAINER_REGISTRY_FIXTURE_DATA),
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "extra_fixtures",
+    [
+        {
+            "images": [
+                IMAGE_FIXTURE_DICT,
+            ],
+            "container_registries": [
+                CONTAINER_REGISTRY_FIXTURE_DICT,
+            ],
+        }
+    ],
+)
+async def test_clear_images(
+    processors: ContainerRegistryProcessors,
+    test_scenario: TestScenario[ClearImagesAction, ClearImagesActionResult],
+):
+    await test_scenario.test(processors.clear_images.wait_for_complete)
+
 
 EXPECTED_IMAGE_RESCAN_RESULT = [
     ImageData(
@@ -67,7 +116,7 @@ EXPECTED_IMAGE_RESCAN_RESULT = [
     [
         (
             TestScenario.success(
-                "Success Case",
+                "Rescan all projects",
                 RescanImagesAction(
                     registry=CONTAINER_REGISTRY_ROW_FIXTURE.registry_name,
                     project=None,
@@ -122,6 +171,7 @@ EXPECTED_IMAGE_RESCAN_RESULT = [
 )
 async def test_rescan_images(
     dockerhub_responses_mock,
+    extra_fixtures,
     processors: ContainerRegistryProcessors,
     test_scenario: TestScenario[RescanImagesAction, RescanImagesActionResult],
 ):
--- tests/manager/services/image/conftest.py
+++ tests/manager/services/image/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 
-from ai.backend.manager.server import agent_registry_ctx
 from ai.backend.manager.services.image.processors import ImageProcessors
 from ai.backend.manager.services.image.service import ImageService
 
 
 @pytest.fixture
-def processors(extra_fixtures, database_fixture, database_engine):
-    image_service = ImageService(db=database_engine, agent_registry=agent_registry_ctx)
+def processors(extra_fixtures, database_fixture, database_engine, registry_ctx):
+    agent_registry, _, _, _, _, _, _ = registry_ctx
+    image_service = ImageService(db=database_engine, agent_registry=agent_registry)
     return ImageProcessors(image_service, [])
