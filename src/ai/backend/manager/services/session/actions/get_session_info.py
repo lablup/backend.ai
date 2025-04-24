1e29994eb0f77ba13d993358295763d31b1789e8
jopemachine <gbl@lablup.com>
2025-04-24 06:00:08 +0000

    feat: Add `test_get_session_info`

--- src/ai/backend/manager/services/session/actions/get_session_info.py
+++ src/ai/backend/manager/services/session/actions/get_session_info.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
-from typing import Any, Optional, override
+from typing import Optional, override
 
 from ai.backend.common.types import AccessKey
 from ai.backend.manager.actions.action import BaseActionResult
-from ai.backend.manager.models.session import SessionRow
+from ai.backend.manager.data.session.types import SessionData
 from ai.backend.manager.services.session.base import SessionAction
+from ai.backend.manager.services.session.types import LegacySessionInfo
 
 
 @dataclass
@@ -24,10 +25,9 @@ class GetSessionInfoAction(SessionAction):
 
 @dataclass
 class GetSessionInfoActionResult(BaseActionResult):
-    # TODO: Add proper type
-    result: Any
-    session_row: SessionRow
+    session_info: LegacySessionInfo
+    session_data: SessionData
 
     @override
     def entity_id(self) -> Optional[str]:
-        return str(self.session_row.id)
+        return str(self.session_data.id)
