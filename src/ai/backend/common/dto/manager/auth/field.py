089ff3abadb6a4fe264f103fde7311b0cf4095a9
Sanghun Lee <sanghun@lablup.com>
2025-05-12 13:47:10 +0000

    apply dto

--- src/ai/backend/common/dto/manager/auth/field.py
+++ src/ai/backend/common/dto/manager/auth/field.py
@@ -1,4 +1,5 @@
 import enum
+from typing import Any, Optional
 
 from pydantic import BaseModel
 
@@ -14,16 +15,29 @@ class AuthResponseData(BaseModel):
     role: str
     status: str
 
+    def to_dict(self) -> dict[str, str]:
+        return self.model_dump(mode="json")
+
 
 class RedirectAuthResponseData(BaseModel):
-    redirect_url: str
+    redirect_url: Optional[str]
     token: str
 
     def to_dict(self) -> dict[str, str]:
-        return self.model_dump()
+        return self.model_dump(mode="json")
 
 
 class AuthResponse(BaseModel):
-    status: int
     data: AuthResponseData | RedirectAuthResponseData
     type: AuthTokenTypes = AuthTokenTypes.KEYPAIR
+
+    @classmethod
+    def from_auth_response(cls, response: dict[str, Any]) -> "AuthResponse":
+        if "redirect_url" in response:
+            return AuthResponse(
+                data=RedirectAuthResponseData(**response),
+            )
+        else:
+            return AuthResponse(
+                data=AuthResponseData(**response),
+            )
