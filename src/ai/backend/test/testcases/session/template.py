b1cd64f0082ca2f741396e70d36b348986371877
jopemachine <gbl@lablup.com>
2025-06-10 10:54:48 +0000

    feat: Refactoring using BaseTestContext

--- src/ai/backend/test/testcases/session/template.py
+++ src/ai/backend/test/testcases/session/template.py
@@ -2,15 +2,16 @@ import uuid
 from contextlib import asynccontextmanager as actxmgr
 from typing import AsyncIterator, override
 
+from ai.backend.client.session import AsyncSession
 from ai.backend.test.testcases.session.context import ComputeSessionContext
 from ai.backend.test.testcases.template import WrapperTestTemplate
 
 
-class SessionTestTemplateWrapper(WrapperTestTemplate):
+class SessionSetupTemplateWrapper(WrapperTestTemplate):
     @property
     @override
     def name(self) -> str:
-        return "session_test"
+        return "session_setup"
 
     @override
     @actxmgr
@@ -19,5 +20,8 @@ class SessionTestTemplateWrapper(WrapperTestTemplate):
         Context manager to set the compute session for the duration of the context.
         """
         session_name = f"test_session_{uuid.uuid4()}"
-        async with ComputeSessionContext.with_session(session_name):
+        async with ComputeSessionContext.with_current(session_name):
             yield
+
+            async with AsyncSession() as client_session:
+                await client_session.ComputeSession(session_name).destroy()
