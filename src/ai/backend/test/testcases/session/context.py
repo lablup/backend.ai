bc61afc62c508d090c4a132c1003406bfbc923c1
jopemachine <gbl@lablup.com>
2025-06-10 10:59:45 +0000

    fix: Remove useless variable

--- src/ai/backend/test/testcases/session/context.py
+++ src/ai/backend/test/testcases/session/context.py
@@ -1,8 +1,7 @@
-from contextvars import ContextVar, Token
+from contextvars import ContextVar
 
 from ai.backend.test.testcases.context import BaseTestContext
 
 
 class ComputeSessionContext(BaseTestContext[str]):
     _ctxvar: ContextVar[str] = ContextVar("compute_session_name")
-    _tokens: dict[str, Token[str]] = {}
