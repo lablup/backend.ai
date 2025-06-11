4f1ee9ef7e7e003e841134a5d5e3281ec3937154
jopemachine <gbl@lablup.com>
2025-06-11 07:54:05 +0000

    fix: Broken CI

--- src/ai/backend/test/testcases/session/context.py
+++ src/ai/backend/test/testcases/session/context.py
@@ -1,7 +1,5 @@
-from contextvars import ContextVar
-
 from ai.backend.test.testcases.context import BaseTestContext
 
 
 class ComputeSessionContext(BaseTestContext[str]):
-    _ctxvar: ContextVar[str] = ContextVar("compute_session_name")
+    pass
