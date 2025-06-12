1a8886119c4ebdd7c1388c659305df49ba7499ce
Sanghun Lee <sanghun@lablup.com>
2025-06-12 13:17:03 +0000

    fix build file

--- src/ai/backend/agent/task_runner/heartbeat.py
+++ src/ai/backend/agent/task_runner/heartbeat.py
@@ -1,18 +1,22 @@
-from typing import Optional, override, Callable, TYPE_CHECKING
-from collections.abc import Awaitable, Sequence, Mapping
 import logging
+from collections.abc import Awaitable, Sequence
+from typing import Callable, Optional, override
 
-from ai.backend.logging.utils import BraceStyleAdapter
-from ai.backend.common.task_runner.types import AbstractTask, TaskRunner
 from ai.backend.common.events.dispatcher import EventProducer
-from ai.backend.common.events.event_types.agent.anycast import AgentStatusHeartbeat, ContainerStatusData
-from ai.backend.common.types import KernelId, AgentId, SessionId,ContainerStatus
+from ai.backend.common.events.event_types.agent.anycast import (
+    AgentStatusHeartbeat,
+    ContainerStatusData,
+)
+from ai.backend.common.task_runner.types import AbstractTask
+from ai.backend.common.types import AgentId, ContainerStatus, KernelId
+from ai.backend.logging.utils import BraceStyleAdapter
 
 from ..types import (
     Container,
 )
 
 log = BraceStyleAdapter(logging.getLogger(__spec__.name))
+
 
 class HeartbeatTask(AbstractTask):
     def __init__(
@@ -26,11 +30,11 @@ class HeartbeatTask(AbstractTask):
         self._agent_id = agent_id
         self._container_enumerator = container_enumerator
         self._event_producer = event_producer
-    
+
     @override
     @classmethod
     def name(cls) -> str:
-        return "container_observer"
+        return "agent_hearbeat"
 
     @override
     @classmethod
@@ -40,11 +44,14 @@ class HeartbeatTask(AbstractTask):
     @override
     async def run(self) -> None:
         containers = await self._container_enumerator(frozenset([ContainerStatus.RUNNING]))
-        container_data = [ContainerStatusData(
-            container.id,
-            kernel_id,
-            container.status,
-        ) for kernel_id, container in containers]
+        container_data = [
+            ContainerStatusData(
+                container.id,
+                kernel_id,
+                container.status,
+            )
+            for kernel_id, container in containers
+        ]
         await self._event_producer.anycast_event(
             AgentStatusHeartbeat(
                 self._agent_id,
