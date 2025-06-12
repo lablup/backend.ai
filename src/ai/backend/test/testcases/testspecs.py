233542c3422a9703541dbc31b4c0466058dd75e4
jopemachine <gbl@lablup.com>
2025-06-12 00:59:31 +0000

    fix: Reflect feedback

--- src/ai/backend/test/testcases/testspecs.py
+++ src/ai/backend/test/testcases/testspecs.py
@@ -1,26 +1,7 @@
-from ai.backend.test.templates.auth.keypair import KeypairTemplate
-from ai.backend.test.templates.template import BasicTestTemplate, NopTestCode
 from ai.backend.test.testcases.auth.testspecs import AUTH_TEST_SPECS
-from ai.backend.test.testcases.session.session_creation import TestSessionCreation
-from ai.backend.test.testcases.spec_manager import TestSpec, TestTag
+from ai.backend.test.testcases.session.testspecs import SESSION_TEST_SPECS
 
 ROOT_TEST_SPECS = {
     **AUTH_TEST_SPECS,
-    "nop": TestSpec(
-        name="nop",
-        description="No operation test case.",
-        tags=set(),
-        template=BasicTestTemplate(NopTestCode()),
-    ),
-    "session": TestSpec(
-        name="session",
-        description="Test session management.",
-        tags={TestTag.SESSION, TestTag.MANAGER},
-        template=BasicTestTemplate(
-            testcode=TestSessionCreation(),
-            wrapper_templates=[
-                KeypairTemplate,
-            ],
-        ),
-    ),
+    **SESSION_TEST_SPECS,
 }
