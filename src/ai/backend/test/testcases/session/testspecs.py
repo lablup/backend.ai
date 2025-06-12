342374cad401ca4e5630c2905f0abd34d3d604d1
jopemachine <gbl@lablup.com>
2025-06-12 08:24:53 +0000

    feat: Rename test cases

--- src/ai/backend/test/testcases/session/testspecs.py
+++ src/ai/backend/test/testcases/session/testspecs.py
@@ -2,7 +2,7 @@ import textwrap
 
 from ai.backend.test.templates.session.batch_session import BatchSessionTemplate
 from ai.backend.test.templates.session.interactive_session import InteractiveSessionTemplate
-from ai.backend.test.testcases.session.batchsession_creation_failure_wrong_command import (
+from ai.backend.test.testcases.session.batch_session_creation_failure_wrong_command import (
     BatchSessionCreationFailureWrongCommand,
 )
 from ai.backend.test.testcases.session.session_creation_failure_low_resources import (
@@ -16,8 +16,8 @@ from ...templates.template import BasicTestTemplate, NopTestCode
 from ..testcases import TestSpec, TestTag
 
 SESSION_TEST_SPECS = {
-    "single_node_single_container_batch_session": TestSpec(
-        name="single_node_single_container_batch_session",
+    "execution_success_single_node_single_container": TestSpec(
+        name="execution_success_single_node_single_container",
         description=textwrap.dedent("""\
             Test for creating a single-node, single-container batch session.
             This test verifies that a session can be created with a single node and a single container, and that it transitions through the expected lifecycle events.
@@ -29,8 +29,8 @@ SESSION_TEST_SPECS = {
         tags={TestTag.MANAGER, TestTag.AGENT, TestTag.SESSION},
         template=BatchSessionTemplate(BasicTestTemplate(NopTestCode())),
     ),
-    # "single_node_multi_container_batch_session": TestSpec(
-    #     name="single_node_multi_container_batch_session",
+    # "execution_success_single_node_multi_container": TestSpec(
+    #     name="execution_success_single_node_multi_container",
     #     description=textwrap.dedent("""\
     #         Test for creating a single-node, multi-container batch session.
     #         This test verifies that a session can be created with a single node and multiple containers, and that it transitions through the expected lifecycle events.
@@ -42,8 +42,8 @@ SESSION_TEST_SPECS = {
     #     tags={TestTag.MANAGER, TestTag.AGENT, TestTag.SESSION},
     #     template=BatchSessionTemplate(BasicTestTemplate(NopTestCode())),
     # ),
-    # "multi_node_multi_container_batch_session": TestSpec(
-    #     name="multi_node_multi_container_batch_session",
+    # "execution_success_multi_node_multi_container": TestSpec(
+    #     name="execution_success_multi_node_multi_container",
     #     description=textwrap.dedent("""\
     #         Test for creating a multi-node, multi-container batch session.
     #         This test verifies that a session can be created with multiple nodes and multiple containers, and that it transitions through the expected lifecycle events.
@@ -55,8 +55,8 @@ SESSION_TEST_SPECS = {
     #     tags={TestTag.MANAGER, TestTag.AGENT, TestTag.SESSION},
     #     template=BatchSessionTemplate(BasicTestTemplate(NopTestCode())),
     # ),
-    "single_node_single_container_session": TestSpec(
-        name="single_node_single_container_session",
+    "creation_success_single_node_single_container": TestSpec(
+        name="creation_success_single_node_single_container",
         description=textwrap.dedent("""\
             Test for creating a single-node, single-container session.
             This test verifies that a session can be created with a single node and a single container, and that it transitions through the expected lifecycle events.
@@ -69,8 +69,8 @@ SESSION_TEST_SPECS = {
         tags={TestTag.MANAGER, TestTag.AGENT, TestTag.SESSION},
         template=InteractiveSessionTemplate(BasicTestTemplate(NopTestCode())),
     ),
-    "create_session_failure_due_to_low_resources": TestSpec(
-        name="create_session_failure_due_to_low_resources",
+    "creation_failure_due_to_low_resources": TestSpec(
+        name="creation_failure_due_to_low_resources",
         description=textwrap.dedent("""\
             Test for creating a session with too low resources.
             This test verifies that a session creation fails when the specified resources are insufficient to run the image.
@@ -81,8 +81,8 @@ SESSION_TEST_SPECS = {
         tags={TestTag.MANAGER, TestTag.AGENT, TestTag.SESSION},
         template=BasicTestTemplate(SessionCreationFailureLowResources()),
     ),
-    "create_session_failure_due_to_too_many_container_count": TestSpec(
-        name="create_session_failure_due_to_too_many_container_count",
+    "creation_failure_due_to_too_many_container_count": TestSpec(
+        name="creation_failure_due_to_too_many_container_count",
         description=textwrap.dedent("""\
             Test for creating a session with too many containers.
             This test verifies that a session creation fails when the specified container count exceeds the limit.
@@ -93,10 +93,14 @@ SESSION_TEST_SPECS = {
         tags={TestTag.MANAGER, TestTag.AGENT, TestTag.SESSION},
         template=BasicTestTemplate(SessionCreationFailureTooManyContainer()),
     ),
-    "create_batchsession_failure_due_to_wrong_command": TestSpec(
+    "creation_failure_due_to_wrong_command": TestSpec(
         name="create_batchsession_failure_due_to_wrong_command",
         description=textwrap.dedent("""\
             Test for creating a batch session with an invalid startup command.
+            This test verifies that a batch session creation fails when the startup command is invalid.
+            The test will:
+            1. Attempt to create a batch session with the specified image and an invalid startup command.
+            2. Assert that the session creation fails with an appropriate error message.
         """),
         tags={TestTag.MANAGER, TestTag.AGENT, TestTag.SESSION},
         template=BasicTestTemplate(BatchSessionCreationFailureWrongCommand()),
