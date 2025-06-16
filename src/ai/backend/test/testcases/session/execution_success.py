be82402b4611c34d2e3f61824cc0fb7820d5306e
jopemachine <gbl@lablup.com>
2025-06-16 08:37:46 +0000

    feat: Add failure case

--- src/ai/backend/test/testcases/session/execution_success.py
+++ src/ai/backend/test/testcases/session/execution_success.py
@@ -22,3 +22,22 @@ class InteractiveSessionExecuteCodeSuccess(TestCode):
         assert result["console"] == [["stdout", code_dep.expected_result]], (
             f"Expected console output to match, Actual console: {result['console']}"
         )
+
+
+class InteractiveSessionExecuteCodeFailureWrongCommand(TestCode):
+    async def test(self) -> None:
+        client_session = ClientSessionContext.current()
+        session_id = CreatedSessionIDContext.current()
+
+        result = await client_session.ComputeSession(session_id).execute(
+            code="some wrong command !@#",
+        )
+
+        assert result["status"] == "finished", (
+            f"Expected status to be finished, Actual status: {result['status']}"
+        )
+        # TODO: Is 0 correct exitcode when the command fails?
+        assert result["exitCode"] == 0, (
+            f"Expected exitCode to be 0, Actual exitCode: {result['exitCode']}"
+        )
+        assert result["console"][0] == ["stderr"]
