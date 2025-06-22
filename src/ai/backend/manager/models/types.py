b98a2263f9077029073fa383c193e82b30ab75b4
Sanghun Lee <sanghun@lablup.com>
2025-06-22 03:37:18 +0000

    update session query codes

--- src/ai/backend/manager/models/types.py
+++ src/ai/backend/manager/models/types.py
@@ -1,29 +1,19 @@
-from typing import Callable, Optional, Protocol
+from typing import Callable, Optional
 
 import sqlalchemy as sa
 
 type QueryConditionCallable = Callable[
     [Optional[sa.sql.expression.BinaryExpression]], sa.sql.expression.BinaryExpression
 ]
-type QueryCondition = Callable[..., QueryConditionCallable]
+type QueryCondition = Callable[[sa.sql.Select], QueryConditionCallable]
 
 type QueryOptionCallable = Callable[[sa.sql.Select], sa.sql.Select]
 type QueryOption = Callable[..., Callable[[sa.sql.Select], sa.sql.Select]]
 
 
-class _LoadableField(Protocol):
-    def __call__(self, *args, **kwargs) -> Callable:
-        pass
+def load_related_field(field: sa.orm.Load) -> QueryOptionCallable:
+    return lambda stmt: stmt.options(field)
 
 
-class _JoinableField(Protocol):
-    def __call__(self, *args, **kwargs) -> sa.orm.attributes.InstrumentedAttribute:
-        pass
-
-
-def load_related_field(field: _LoadableField) -> QueryOptionCallable:
-    return lambda stmt: stmt.options(field())
-
-
-def join_by_related_field(field: _JoinableField) -> QueryOptionCallable:
-    return lambda stmt: stmt.join(field())
+def join_by_related_field(field: sa.orm.attributes.InstrumentedAttribute) -> QueryOptionCallable:
+    return lambda stmt: stmt.join(field)
