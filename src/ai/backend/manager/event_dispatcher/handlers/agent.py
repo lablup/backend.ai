22ab7b041ec191a46e42db2becff895a8458134d
Sanghun Lee <sanghun@lablup.com>
2025-06-22 02:21:18 +0000

    refactor: Simple model query pattern

--- src/ai/backend/manager/event_dispatcher/handlers/agent.py
+++ src/ai/backend/manager/event_dispatcher/handlers/agent.py
@@ -25,7 +25,6 @@ from ai.backend.manager.registry import AgentRegistry
 
 from ...models.agent import AgentStatus, agents
 from ...models.kernel import (
-    ConditionMerger,
     KernelRow,
     by_kernel_ids,
 )
@@ -166,12 +165,8 @@ class AgentEventHandler:
             *(k.kernel_id for k in event.active_kernels),
             *(c.kernel_id for c in event.active_containers),
         ])
-        kernel_condition = by_kernel_ids(
-            all_kernel_ids,
-            ConditionMerger.AND,
-        )
         kernel_rows = await KernelRow.get_kernels(
-            [kernel_condition],
+            [by_kernel_ids(all_kernel_ids)],
             db=self._db,
         )
         kernel_should_alive: set[KernelId] = {
--- src/ai/backend/manager/models/types.py
+++ src/ai/backend/manager/models/types.py
@@ -1,55 +1,14 @@
-from dataclasses import dataclass
-from typing import Callable, Optional, Protocol, Self
+from typing import Callable, Optional, Protocol
 
 import sqlalchemy as sa
 
-from .exceptions import EmptySQLCondition
-
-type QuerySingleCondition = sa.sql.expression.BinaryExpression
+type QueryConditionCallable = Callable[
+    [Optional[sa.sql.expression.BinaryExpression]], sa.sql.expression.BinaryExpression
+]
+type QueryCondition = Callable[..., QueryConditionCallable]
 
 type QueryOptionCallable = Callable[[sa.sql.Select], sa.sql.Select]
 type QueryOption = Callable[..., Callable[[sa.sql.Select], sa.sql.Select]]
-
-
-@dataclass
-class QueryCondition:
-    """
-    A class representing a SQL condition for querying.
-    Raises an EmptySQLCondition exception if the condition is empty.
-    """
-
-    _sql_condition: Optional[QuerySingleCondition]
-
-    @classmethod
-    def multiple(cls, operator: Callable, conditions: list[Self]) -> Self:
-        """
-        Args:
-            operator: The SQL operator to use for combining conditions, one of `sqlalchemy.or_`, `sqlalchemy.and_`.
-            conditions: A list of QueryCondition instances to combine.
-        Returns:
-            A new QueryCondition instance with the combined conditions.
-        """
-        sql_conditions = []
-        for cond in conditions:
-            try:
-                sql_conditions.append(cond.final_sql_condition)
-            except EmptySQLCondition:
-                continue
-        if not sql_conditions:
-            return cls(None)
-        if len(sql_conditions) == 1:
-            return cls(sql_conditions[0])
-        return cls(operator(*sql_conditions))
-
-    @classmethod
-    def single(cls, condition: QuerySingleCondition) -> Self:
-        return cls(condition)
-
-    @property
-    def final_sql_condition(self) -> QuerySingleCondition:
-        if self._sql_condition is None:
-            raise EmptySQLCondition
-        return self._sql_condition
 
 
 class _LoadableField(Protocol):
