25260d7ef24e0d087dc3cd4f66485c5e1a7ad1ed
BoGyum Kim <bkkim@lablup.com>
2025-06-26 12:06:08 +0900

    feat: Add cpu intensive work in test

--- src/ai/backend/test/testcases/model_service/scale_by_auto_scaling_rule.py
+++ src/ai/backend/test/testcases/model_service/scale_by_auto_scaling_rule.py
@@ -1,6 +1,8 @@
 import asyncio
 from typing import override
+from uuid import UUID
 
+from ai.backend.client.session import AsyncSession
 from ai.backend.test.contexts.client_session import ClientSessionContext
 from ai.backend.test.contexts.model_service import (
     AutoScalingRuleContext,
@@ -19,7 +21,7 @@ class ScaleByAutoScalingRules(TestCode):
     @override
     async def test(self) -> None:
         endpoint_meta = CreatedModelServiceEndpointMetaContext.current()
-        model_service_id = endpoint_meta.service_id
+        service_id = endpoint_meta.service_id
         client_session = ClientSessionContext.current()
         model_service_dep = ModelServiceContext.current()
         auto_scaling_rule_dep = AutoScalingRuleContext.current()
@@ -30,17 +32,25 @@ class ScaleByAutoScalingRules(TestCode):
             client_session, model_service_dep.model_vfolder_name
         )
 
+        await self._make_cpu_intensive_task_in_service(
+            client_session,
+            service_id,
+            code="while True: pass",
+        )
+
+        await asyncio.sleep(1)  # Give some time for making cpu-intensive environment
+
         await asyncio.wait_for(
             wait_until_all_inference_sessions_ready(
                 client_session,
-                model_service_id,
+                service_id,
                 max_replicas,
                 vfolder_id,
             ),
             timeout=_SCALE_TIMEOUT,
         )
 
-        result = await client_session.Service(model_service_id).info()
+        result = await client_session.Service(service_id).info()
         assert result["replicas"] == max_replicas, (
             f"Expected replicas count: {max_replicas}, actual: {result['replicas']}"
         )
@@ -48,3 +58,13 @@ class ScaleByAutoScalingRules(TestCode):
             f"Expected desired session count: {max_replicas}, "
             f"actual: {result['desired_session_count']}"
         )
+
+    async def _make_cpu_intensive_task_in_service(
+        self, client_session: AsyncSession, service_id: UUID, code: str
+    ) -> None:
+        result = await client_session.Service(service_id).info()
+        active_routes = result["active_routes"]
+        session_ids = [route["session_id"] for route in active_routes]
+        print(f"code excuted on session_ids: {session_ids}")
+        for session_id in session_ids:
+            await client_session.ComputeSession.from_session_id(session_id).execute(code=code)
