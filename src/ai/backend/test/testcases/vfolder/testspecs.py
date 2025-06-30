a4466b80215e27396d8eea5e19aafa2ffdd3443e
jopemachine <gbl@lablup.com>
2025-06-30 06:57:06 +0000

    fix: Resolve merge conflict

--- src/ai/backend/test/testcases/vfolder/testspecs.py
+++ src/ai/backend/test/testcases/vfolder/testspecs.py
@@ -206,4 +206,37 @@ VFOLDER_TEST_SPECS = {
             ]
         },
     ),
+    "clone_vfolder": TestSpec(
+        name="clone_vfolder",
+        description=textwrap.dedent("""\
+            Test for renaming a file in a VFolder.
+            The test will:
+            1. Create a VFolder.
+            2. Upload files to the VFolder.
+            3. Clone the VFolder.
+            4. Verify that the files in the cloned VFolder match the original VFolder.
+        """),
+        tags={TestTag.MANAGER, TestTag.VFOLDER},
+        template=BasicTestTemplate(VFolderCloneSuccess()).with_wrappers(
+            KeypairAuthTemplate, GeneralVFolderTemplate, PlainTextFilesUploader
+        ),
+        parametrizes={
+            ContextName.VFOLDER_UPLOAD_FILES: [
+                [
+                    UploadFileDep(
+                        path="test_1.txt",
+                        content=_TEST_FILE_CONTENT,
+                    ),
+                    UploadFileDep(
+                        path="test_2.txt",
+                        content=_TEST_FILE_CONTENT,
+                    ),
+                    UploadFileDep(
+                        path="nested/test_3.txt",
+                        content=_TEST_FILE_CONTENT,
+                    ),
+                ]
+            ]
+        },
+    ),
 }
