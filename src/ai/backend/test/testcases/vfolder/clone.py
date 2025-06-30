68d616fef1d02f9b91714f8b9968eddada075368
Gyubong Lee <jopemachine@naver.com>
2025-06-30 16:16:39 +0900

    feat(BA-1696): Add VFolder clone test (#4885)

---
+++ src/ai/backend/test/testcases/vfolder/clone.py
@@ -0,0 +1,20 @@
+from ai.backend.test.contexts.client_session import ClientSessionContext
+from ai.backend.test.contexts.vfolder import CreatedVFolderMetaContext
+from ai.backend.test.templates.template import TestCode
+from ai.backend.test.templates.vfolder.utils import retrieve_all_files
+
+
+class VFolderCloneSuccess(TestCode):
+    async def test(self) -> None:
+        client_session = ClientSessionContext.current()
+        vfolder_meta = CreatedVFolderMetaContext.current()
+
+        new_vfolder_name = vfolder_meta.name + "_cloned"
+        await client_session.VFolder(vfolder_meta.name).clone(target_name=new_vfolder_name)
+
+        old_vfolder_files = await retrieve_all_files(client_session, vfolder_meta.name)
+        new_vfolder_files = await retrieve_all_files(client_session, new_vfolder_name)
+
+        assert old_vfolder_files == new_vfolder_files, (
+            "Files in the cloned VFolder do not match the original VFolder."
+        )
--- src/ai/backend/test/testcases/vfolder/testspecs.py
+++ src/ai/backend/test/testcases/vfolder/testspecs.py
@@ -6,6 +6,7 @@ from ai.backend.test.templates.template import BasicTestTemplate
 from ai.backend.test.templates.vfolder.file_uploader import PlainTextFilesUploader
 from ai.backend.test.templates.vfolder.general_vfolder import GeneralVFolderTemplate
 from ai.backend.test.testcases.spec_manager import TestSpec, TestTag
+from ai.backend.test.testcases.vfolder.clone import VFolderCloneSuccess
 from ai.backend.test.testcases.vfolder.delete_files import (
     VFolderFilesDeletionSuccess,
     VFolderFilesRecursiveDeletionSuccess,
@@ -199,6 +200,39 @@ VFOLDER_TEST_SPECS = {
                     ),
                     UploadFileDep(
                         path="nested/inner.txt",
+                        content=_TEST_FILE_CONTENT,
+                    ),
+                ]
+            ]
+        },
+    ),
+    "clone_vfolder": TestSpec(
+        name="clone_vfolder",
+        description=textwrap.dedent("""\
+            Test for renaming a file in a VFolder.
+            The test will:
+            1. Create a VFolder.
+            2. Upload files to the VFolder.
+            3. Clone the VFolder.
+            4. Verify that the files in the cloned VFolder match the original VFolder.
+        """),
+        tags={TestTag.MANAGER, TestTag.VFOLDER},
+        template=BasicTestTemplate(VFolderCloneSuccess()).with_wrappers(
+            KeypairAuthTemplate, GeneralVFolderTemplate, PlainTextFilesUploader
+        ),
+        parametrizes={
+            ContextName.VFOLDER_UPLOAD_FILES: [
+                [
+                    UploadFileDep(
+                        path="test_1.txt",
+                        content=_TEST_FILE_CONTENT,
+                    ),
+                    UploadFileDep(
+                        path="test_2.txt",
+                        content=_TEST_FILE_CONTENT,
+                    ),
+                    UploadFileDep(
+                        path="nested/test_3.txt",
                         content=_TEST_FILE_CONTENT,
                     ),
                 ]
