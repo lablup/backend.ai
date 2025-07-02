8c9199d741bf9d329ea5f5f82d76c83e7cbf10f0
Sanghun Lee <sanghun@lablup.com>
2025-07-02 17:32:22 +0900

    restore falsy added code

--- src/ai/backend/manager/api/manager.py
+++ src/ai/backend/manager/api/manager.py
@@ -203,7 +203,7 @@ async def update_manager_status(request: web.Request, params: Any) -> web.Respon
         # It supposed to kill all running sessions and kernels
         # but there is no RPC to do that.
         pass
-    await root_ctx.config_provider.legacy_etcd_config_loader.update_manager_status(status)
+    await root_ctx.shared_config.update_manager_status(status)
 
     return web.Response(status=HTTPStatus.NO_CONTENT)
 
