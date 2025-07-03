5ec09db76bdaf29639ecd5f6d8104ff9c8abb98f
Sanghun Lee <sanghun@lablup.com>
2025-07-03 18:26:38 +0900

    fix(BA-1404): Calculate correct permissions for admins #4459 (#4963)

--- src/ai/backend/manager/models/vfolder.py
+++ src/ai/backend/manager/models/vfolder.py
@@ -2174,11 +2174,7 @@ WhereClauseType: TypeAlias = (
 OWNER_PERMISSIONS: frozenset[VFolderRBACPermission] = frozenset([
     perm for perm in VFolderRBACPermission
 ])
-ADMIN_PERMISSIONS: frozenset[VFolderRBACPermission] = frozenset([
-    VFolderRBACPermission.READ_ATTRIBUTE,
-    VFolderRBACPermission.UPDATE_ATTRIBUTE,
-    VFolderRBACPermission.DELETE_VFOLDER,
-])
+ADMIN_PERMISSIONS: frozenset[VFolderRBACPermission] = frozenset()
 MONITOR_PERMISSIONS: frozenset[VFolderRBACPermission] = frozenset([
     VFolderRBACPermission.READ_ATTRIBUTE,
     VFolderRBACPermission.UPDATE_ATTRIBUTE,
