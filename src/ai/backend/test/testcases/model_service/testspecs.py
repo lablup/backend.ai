04b66f358b08a9a11821d5efd6f7b7b34fbb831f
BoGyum Kim <bkkim@lablup.com>
2025-07-04 18:22:12 +0900

    fix: Revert test commentation

--- src/ai/backend/test/testcases/model_service/testspecs.py
+++ src/ai/backend/test/testcases/model_service/testspecs.py
@@ -28,119 +28,119 @@ from ai.backend.test.testcases.spec_manager import TestSpec, TestTag
 from ai.backend.test.tester.dependency import AutoScalingRuleDep, ClusterDep
 
 SINGLE_NODE_SINGLE_CONTAINER_MODEL_SERVICE_TEST_SPECS = {
-    # "single_node_single_container_creation_endpoint_success": TestSpec(
-    #     name="single_node_single_container_creation_endpoint_success",
-    #     description=textwrap.dedent("""\
-    #         Test for successful creation of an endpoint.
-    #         This test verifies that an endpoint can be created successfully.
-    #         The test will:
-    #         1. Create an endpoint with a specified name and resources.
-    #         2. Verify that the endpoint is not available without generating JWT token.
-    #         3. Clean up the endpoint after verification.
-    #     """),
-    #     tags={
-    #         TestTag.MANAGER,
-    #         TestTag.AGENT,
-    #         TestTag.MODEL_SERVICE,
-    #         TestTag.SESSION,
-    #         TestTag.SINGLE_NODE_SINGLE_CONTAINER,
-    #     },
-    #     # Endpoint health check failure is expected.
-    #     template=BasicTestTemplate(
-    #         EndpointHealthCheck(expected_status_codes={400, 401})
-    #     ).with_wrappers(KeypairAuthTemplate, EndpointTemplate),
-    #     parametrizes={
-    #         ContextName.CLUSTER_CONFIG: [
-    #             ClusterDep(
-    #                 cluster_mode=ClusterMode.SINGLE_NODE,
-    #                 cluster_size=1,
-    #             ),
-    #         ]
-    #     },
-    # ),
-    # "single_node_single_container_creation_public_endpoint_success": TestSpec(
-    #     name="single_node_single_container_creation_public_endpoint_success",
-    #     description=textwrap.dedent("""\
-    #         Test for successful creation of an endpoint.
-    #         This test verifies that an endpoint can be created successfully.
-    #         The test will:
-    #         1. Create an endpoint with a specified name and resources.
-    #         2. Verify that the endpoint is not available without generating JWT token.
-    #         3. Clean up the endpoint after verification.
-    #     """),
-    #     tags={TestTag.MANAGER, TestTag.AGENT, TestTag.MODEL_SERVICE, TestTag.SESSION},
-    #     # Endpoint health check failure is expected.
-    #     template=BasicTestTemplate(
-    #         EndpointHealthCheck(expected_status_codes={400, 401})
-    #     ).with_wrappers(KeypairAuthTemplate, EndpointTemplate),
-    #     parametrizes={
-    #         ContextName.CLUSTER_CONFIG: [
-    #             ClusterDep(
-    #                 cluster_mode=ClusterMode.SINGLE_NODE,
-    #                 cluster_size=1,
-    #             ),
-    #         ]
-    #     },
-    # ),
-    # "creation_public_endpoint_success": TestSpec(
-    #     name="creation_public_endpoint_success",
-    #     description=textwrap.dedent("""\
-    #         Test for successful creation of an endpoint.
-    #         This test verifies that an endpoint can be created successfully.
-    #         The test will:
-    #         1. Create an endpoint with a specified name and resources.
-    #         2. Verify that the endpoint is created and available.
-    #         3. Clean up the endpoint after verification.
-    #     """),
-    #     tags={
-    #         TestTag.MANAGER,
-    #         TestTag.AGENT,
-    #         TestTag.MODEL_SERVICE,
-    #         TestTag.SESSION,
-    #         TestTag.SINGLE_NODE_SINGLE_CONTAINER,
-    #     },
-    #     template=BasicTestTemplate(EndpointHealthCheck(expected_status_codes={200})).with_wrappers(
-    #         KeypairAuthTemplate, PublicEndpointTemplate
-    #     ),
-    #     parametrizes={
-    #         ContextName.CLUSTER_CONFIG: [
-    #             ClusterDep(
-    #                 cluster_mode=ClusterMode.SINGLE_NODE,
-    #                 cluster_size=1,
-    #             ),
-    #         ]
-    #     },
-    # ),
-    # "single_node_single_container_creation_private_endpoint_success": TestSpec(
-    #     name="single_node_single_container_creation_private_endpoint_success",
-    #     description=textwrap.dedent("""\
-    #         Test for successful creation of an endpoint.
-    #         This test verifies that an endpoint can be created successfully.
-    #         The test will:
-    #         1. Create an endpoint with a specified name and resources.
-    #         2. Verify that the endpoint is created and available.
-    #         3. Clean up the endpoint after verification.
-    #     """),
-    #     tags={
-    #         TestTag.MANAGER,
-    #         TestTag.AGENT,
-    #         TestTag.MODEL_SERVICE,
-    #         TestTag.SESSION,
-    #         TestTag.SINGLE_NODE_SINGLE_CONTAINER,
-    #     },
-    #     # Endpoint health check success is expected since we're inject JWT token into the request.
-    #     template=BasicTestTemplate(
-    #         EndpointHealthCheckWithToken(expected_status_codes={200})
-    #     ).with_wrappers(KeypairAuthTemplate, EndpointTemplate, ModelServiceTokenTemplate),
-    #     parametrizes={
-    #         ContextName.CLUSTER_CONFIG: [
-    #             ClusterDep(
-    #                 cluster_mode=ClusterMode.SINGLE_NODE,
-    #                 cluster_size=1,
-    #             ),
-    #         ]
-    #     },
-    # ),
+    "single_node_single_container_creation_endpoint_success": TestSpec(
+        name="single_node_single_container_creation_endpoint_success",
+        description=textwrap.dedent("""\
+            Test for successful creation of an endpoint.
+            This test verifies that an endpoint can be created successfully.
+            The test will:
+            1. Create an endpoint with a specified name and resources.
+            2. Verify that the endpoint is not available without generating JWT token.
+            3. Clean up the endpoint after verification.
+        """),
+        tags={
+            TestTag.MANAGER,
+            TestTag.AGENT,
+            TestTag.MODEL_SERVICE,
+            TestTag.SESSION,
+            TestTag.SINGLE_NODE_SINGLE_CONTAINER,
+        },
+        # Endpoint health check failure is expected.
+        template=BasicTestTemplate(
+            EndpointHealthCheck(expected_status_codes={400, 401})
+        ).with_wrappers(KeypairAuthTemplate, EndpointTemplate),
+        parametrizes={
+            ContextName.CLUSTER_CONFIG: [
+                ClusterDep(
+                    cluster_mode=ClusterMode.SINGLE_NODE,
+                    cluster_size=1,
+                ),
+            ]
+        },
+    ),
+    "single_node_single_container_creation_public_endpoint_success": TestSpec(
+        name="single_node_single_container_creation_public_endpoint_success",
+        description=textwrap.dedent("""\
+            Test for successful creation of an endpoint.
+            This test verifies that an endpoint can be created successfully.
+            The test will:
+            1. Create an endpoint with a specified name and resources.
+            2. Verify that the endpoint is not available without generating JWT token.
+            3. Clean up the endpoint after verification.
+        """),
+        tags={TestTag.MANAGER, TestTag.AGENT, TestTag.MODEL_SERVICE, TestTag.SESSION},
+        # Endpoint health check failure is expected.
+        template=BasicTestTemplate(
+            EndpointHealthCheck(expected_status_codes={400, 401})
+        ).with_wrappers(KeypairAuthTemplate, EndpointTemplate),
+        parametrizes={
+            ContextName.CLUSTER_CONFIG: [
+                ClusterDep(
+                    cluster_mode=ClusterMode.SINGLE_NODE,
+                    cluster_size=1,
+                ),
+            ]
+        },
+    ),
+    "creation_public_endpoint_success": TestSpec(
+        name="creation_public_endpoint_success",
+        description=textwrap.dedent("""\
+            Test for successful creation of an endpoint.
+            This test verifies that an endpoint can be created successfully.
+            The test will:
+            1. Create an endpoint with a specified name and resources.
+            2. Verify that the endpoint is created and available.
+            3. Clean up the endpoint after verification.
+        """),
+        tags={
+            TestTag.MANAGER,
+            TestTag.AGENT,
+            TestTag.MODEL_SERVICE,
+            TestTag.SESSION,
+            TestTag.SINGLE_NODE_SINGLE_CONTAINER,
+        },
+        template=BasicTestTemplate(EndpointHealthCheck(expected_status_codes={200})).with_wrappers(
+            KeypairAuthTemplate, PublicEndpointTemplate
+        ),
+        parametrizes={
+            ContextName.CLUSTER_CONFIG: [
+                ClusterDep(
+                    cluster_mode=ClusterMode.SINGLE_NODE,
+                    cluster_size=1,
+                ),
+            ]
+        },
+    ),
+    "single_node_single_container_creation_private_endpoint_success": TestSpec(
+        name="single_node_single_container_creation_private_endpoint_success",
+        description=textwrap.dedent("""\
+            Test for successful creation of an endpoint.
+            This test verifies that an endpoint can be created successfully.
+            The test will:
+            1. Create an endpoint with a specified name and resources.
+            2. Verify that the endpoint is created and available.
+            3. Clean up the endpoint after verification.
+        """),
+        tags={
+            TestTag.MANAGER,
+            TestTag.AGENT,
+            TestTag.MODEL_SERVICE,
+            TestTag.SESSION,
+            TestTag.SINGLE_NODE_SINGLE_CONTAINER,
+        },
+        # Endpoint health check success is expected since we're inject JWT token into the request.
+        template=BasicTestTemplate(
+            EndpointHealthCheckWithToken(expected_status_codes={200})
+        ).with_wrappers(KeypairAuthTemplate, EndpointTemplate, ModelServiceTokenTemplate),
+        parametrizes={
+            ContextName.CLUSTER_CONFIG: [
+                ClusterDep(
+                    cluster_mode=ClusterMode.SINGLE_NODE,
+                    cluster_size=1,
+                ),
+            ]
+        },
+    ),
     "single_node_single_container_scale_out_by_cpu_util_auto_scaling_rule_success": TestSpec(
         name="single_node_single_container_scale_out_by_cpu_util_auto_scaling_rule_success",
         description=textwrap.dedent("""\
@@ -228,95 +228,95 @@ SINGLE_NODE_SINGLE_CONTAINER_MODEL_SERVICE_TEST_SPECS = {
 }
 
 SINGLE_NODE_MULTI_CONTAINER_MODEL_SERVICE_TEST_SPECS = {
-    # "single_node_multi_container_creation_endpoint_success": TestSpec(
-    #     name="single_node_multi_container_creation_endpoint_success",
-    #     description=textwrap.dedent("""\
-    #         Test for successful creation of an endpoint.
-    #         This test verifies that an endpoint can be created successfully.
-    #         The test will:
-    #         1. Create an endpoint with a specified name and resources.
-    #         2. Verify that the endpoint is not available without generating JWT token.
-    #         3. Clean up the endpoint after verification.
-    #     """),
-    #     tags={
-    #         TestTag.MANAGER,
-    #         TestTag.AGENT,
-    #         TestTag.MODEL_SERVICE,
-    #         TestTag.SESSION,
-    #         TestTag.REQUIRED_SINGLE_NODE_MULTI_CONTAINER_CONFIGURATION,
-    #     },
-    #     # Endpoint health check failure is expected.
-    #     template=BasicTestTemplate(
-    #         EndpointHealthCheck(expected_status_codes={400, 401})
-    #     ).with_wrappers(KeypairAuthTemplate, EndpointTemplate),
-    #     parametrizes={
-    #         ContextName.CLUSTER_CONFIG: [
-    #             ClusterDep(
-    #                 cluster_mode=ClusterMode.SINGLE_NODE,
-    #                 cluster_size=3,
-    #             ),
-    #         ]
-    #     },
-    # ),
-    # "single_node_multi_container_creation_public_endpoint_success": TestSpec(
-    #     name="single_node_multi_container_creation_public_endpoint_success",
-    #     description=textwrap.dedent("""\
-    #         Test for successful creation of an endpoint.
-    #         This test verifies that an endpoint can be created successfully.
-    #         The test will:
-    #         1. Create an endpoint with a specified name and resources.
-    #         2. Verify that the endpoint is created and available.
-    #         3. Clean up the endpoint after verification.
-    #     """),
-    #     tags={
-    #         TestTag.MANAGER,
-    #         TestTag.AGENT,
-    #         TestTag.MODEL_SERVICE,
-    #         TestTag.SESSION,
-    #         TestTag.REQUIRED_SINGLE_NODE_MULTI_CONTAINER_CONFIGURATION,
-    #     },
-    #     template=BasicTestTemplate(EndpointHealthCheck(expected_status_codes={200})).with_wrappers(
-    #         KeypairAuthTemplate, PublicEndpointTemplate
-    #     ),
-    #     parametrizes={
-    #         ContextName.CLUSTER_CONFIG: [
-    #             ClusterDep(
-    #                 cluster_mode=ClusterMode.SINGLE_NODE,
-    #                 cluster_size=3,
-    #             ),
-    #         ]
-    #     },
-    # ),
-    # "single_node_multi_container_creation_private_endpoint_success": TestSpec(
-    #     name="single_node_multi_container_creation_private_endpoint_success",
-    #     description=textwrap.dedent("""\
-    #         Test for successful creation of an endpoint.
-    #         This test verifies that an endpoint can be created successfully.
-    #         The test will:
-    #         1. Create an endpoint with a specified name and resources.
-    #         2. Verify that the endpoint is created and available.
-    #         3. Clean up the endpoint after verification.
-    #     """),
-    #     tags={
-    #         TestTag.MANAGER,
-    #         TestTag.AGENT,
-    #         TestTag.MODEL_SERVICE,
-    #         TestTag.SESSION,
-    #         TestTag.REQUIRED_SINGLE_NODE_MULTI_CONTAINER_CONFIGURATION,
-    #     },
-    #     # Endpoint health check success is expected since we're inject JWT token into the request.
-    #     template=BasicTestTemplate(
-    #         EndpointHealthCheckWithToken(expected_status_codes={200})
-    #     ).with_wrappers(KeypairAuthTemplate, EndpointTemplate, ModelServiceTokenTemplate),
-    #     parametrizes={
-    #         ContextName.CLUSTER_CONFIG: [
-    #             ClusterDep(
-    #                 cluster_mode=ClusterMode.SINGLE_NODE,
-    #                 cluster_size=3,
-    #             ),
-    #         ]
-    #     },
-    # ),
+    "single_node_multi_container_creation_endpoint_success": TestSpec(
+        name="single_node_multi_container_creation_endpoint_success",
+        description=textwrap.dedent("""\
+            Test for successful creation of an endpoint.
+            This test verifies that an endpoint can be created successfully.
+            The test will:
+            1. Create an endpoint with a specified name and resources.
+            2. Verify that the endpoint is not available without generating JWT token.
+            3. Clean up the endpoint after verification.
+        """),
+        tags={
+            TestTag.MANAGER,
+            TestTag.AGENT,
+            TestTag.MODEL_SERVICE,
+            TestTag.SESSION,
+            TestTag.REQUIRED_SINGLE_NODE_MULTI_CONTAINER_CONFIGURATION,
+        },
+        # Endpoint health check failure is expected.
+        template=BasicTestTemplate(
+            EndpointHealthCheck(expected_status_codes={400, 401})
+        ).with_wrappers(KeypairAuthTemplate, EndpointTemplate),
+        parametrizes={
+            ContextName.CLUSTER_CONFIG: [
+                ClusterDep(
+                    cluster_mode=ClusterMode.SINGLE_NODE,
+                    cluster_size=3,
+                ),
+            ]
+        },
+    ),
+    "single_node_multi_container_creation_public_endpoint_success": TestSpec(
+        name="single_node_multi_container_creation_public_endpoint_success",
+        description=textwrap.dedent("""\
+            Test for successful creation of an endpoint.
+            This test verifies that an endpoint can be created successfully.
+            The test will:
+            1. Create an endpoint with a specified name and resources.
+            2. Verify that the endpoint is created and available.
+            3. Clean up the endpoint after verification.
+        """),
+        tags={
+            TestTag.MANAGER,
+            TestTag.AGENT,
+            TestTag.MODEL_SERVICE,
+            TestTag.SESSION,
+            TestTag.REQUIRED_SINGLE_NODE_MULTI_CONTAINER_CONFIGURATION,
+        },
+        template=BasicTestTemplate(EndpointHealthCheck(expected_status_codes={200})).with_wrappers(
+            KeypairAuthTemplate, PublicEndpointTemplate
+        ),
+        parametrizes={
+            ContextName.CLUSTER_CONFIG: [
+                ClusterDep(
+                    cluster_mode=ClusterMode.SINGLE_NODE,
+                    cluster_size=3,
+                ),
+            ]
+        },
+    ),
+    "single_node_multi_container_creation_private_endpoint_success": TestSpec(
+        name="single_node_multi_container_creation_private_endpoint_success",
+        description=textwrap.dedent("""\
+            Test for successful creation of an endpoint.
+            This test verifies that an endpoint can be created successfully.
+            The test will:
+            1. Create an endpoint with a specified name and resources.
+            2. Verify that the endpoint is created and available.
+            3. Clean up the endpoint after verification.
+        """),
+        tags={
+            TestTag.MANAGER,
+            TestTag.AGENT,
+            TestTag.MODEL_SERVICE,
+            TestTag.SESSION,
+            TestTag.REQUIRED_SINGLE_NODE_MULTI_CONTAINER_CONFIGURATION,
+        },
+        # Endpoint health check success is expected since we're inject JWT token into the request.
+        template=BasicTestTemplate(
+            EndpointHealthCheckWithToken(expected_status_codes={200})
+        ).with_wrappers(KeypairAuthTemplate, EndpointTemplate, ModelServiceTokenTemplate),
+        parametrizes={
+            ContextName.CLUSTER_CONFIG: [
+                ClusterDep(
+                    cluster_mode=ClusterMode.SINGLE_NODE,
+                    cluster_size=3,
+                ),
+            ]
+        },
+    ),
     "single_node_multi_container_scale_out_by_cpu_util_auto_scaling_rule_success": TestSpec(
         name="single_node_multi_container_scale_out_by_cpu_util_auto_scaling_rule_success",
         description=textwrap.dedent("""\
@@ -581,6 +581,6 @@ MULTI_NODE_MULTI_CONTAINER_MODEL_SERVICE_TEST_SPECS = {
 
 MODEL_SERVICE_TEST_SPECS = {
     **SINGLE_NODE_SINGLE_CONTAINER_MODEL_SERVICE_TEST_SPECS,
-    # **SINGLE_NODE_MULTI_CONTAINER_MODEL_SERVICE_TEST_SPECS,
-    # **MULTI_NODE_MULTI_CONTAINER_MODEL_SERVICE_TEST_SPECS,
+    **SINGLE_NODE_MULTI_CONTAINER_MODEL_SERVICE_TEST_SPECS,
+    **MULTI_NODE_MULTI_CONTAINER_MODEL_SERVICE_TEST_SPECS,
 }
