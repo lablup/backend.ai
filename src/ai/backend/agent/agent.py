a81c191b38679fe032798076ad022a430b6ccaba
Sanghun Lee <sanghun@lablup.com>
2025-07-07 20:09:22 +0900

    fix: Avoid using potential unbound service_port var #4998 (#5007)
    Co-authored-by: HyeockJinKim <hyeokjin@lablup.com>

--- src/ai/backend/agent/agent.py
+++ src/ai/backend/agent/agent.py
@@ -2680,7 +2680,7 @@ class AbstractAgent(
                         kernel_id,
                         session_id,
                         pretty_container_id,
-                        service_port,
+                        service_ports,
                     )
                 except asyncio.TimeoutError:
                     await self.inject_container_lifecycle_event(
