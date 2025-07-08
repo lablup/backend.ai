66f64456af1d0322269f2aee9ae5510db803a6df
Sanghun Lee <sanghun@lablup.com>
2025-07-08 04:18:46 +0000

    add image pull check provisioner

--- src/ai/backend/agent/stage/kernel_lifecycle/docker/image.py
+++ src/ai/backend/agent/stage/kernel_lifecycle/docker/image.py
@@ -10,15 +10,15 @@ from ai.backend.common.asyncio import closing_async
 from ai.backend.common.docker import ImageRef
 from ai.backend.common.exception import ImageNotAvailable
 from ai.backend.common.stage.types import (
-    ArgsSpecGenerator,
     Provisioner,
     ProvisionStage,
+    SpecGenerator,
 )
 from ai.backend.common.types import AutoPullBehavior, ImageRegistry
 
 
 @dataclass
-class ImagePullSpec:
+class ImagePullCheckSpec:
     image_ref: ImageRef
     image_digest: str
     registry_conf: ImageRegistry
@@ -26,34 +26,29 @@ class ImagePullSpec:
     auto_pull_behavior: AutoPullBehavior
 
 
-class ImagePullSpecGenerator(ArgsSpecGenerator[ImagePullSpec]):
-    pass
-
-
 @dataclass
-class ImagePullResult:
+class ImagePullSpec:
+    do_pull: bool
     image_ref: ImageRef
-    did_pull: bool
+    registry_conf: ImageRegistry
+    pull_timeout: Optional[float]
 
 
-class ImagePullProvisioner(Provisioner[ImagePullSpec, ImagePullResult]):
+class ImagePullSpecProvisioner(Provisioner[ImagePullCheckSpec, ImagePullSpec]):
     @property
-    @override
     def name(self) -> str:
-        return "docker-image-pull"
+        return "docker-image-pull-check"
 
-    @override
-    async def setup(self, spec: ImagePullSpec) -> ImagePullResult:
+    async def setup(self, spec: ImagePullCheckSpec) -> ImagePullSpec:
         do_pull = (not spec.image_ref.is_local) and await self._check_image_exist(spec)
-        if do_pull:
-            image_ref = await self._pull_image(spec)
-            did_pull = True
-        else:
-            image_ref = spec.image_ref
-            did_pull = False
-        return ImagePullResult(image_ref=image_ref, did_pull=did_pull)
+        return ImagePullSpec(
+            do_pull=do_pull,
+            image_ref=spec.image_ref,
+            registry_conf=spec.registry_conf,
+            pull_timeout=spec.pull_timeout,
+        )
 
-    async def _check_image_exist(self, spec: ImagePullSpec) -> bool:
+    async def _check_image_exist(self, spec: ImagePullCheckSpec) -> bool:
         try:
             async with closing_async(Docker()) as docker:
                 image_info = await docker.images.inspect(spec.image_ref.canonical)
@@ -70,6 +65,41 @@ class ImagePullProvisioner(Provisioner[ImagePullSpec, ImagePullResult]):
             else:
                 raise
         return False
+
+    async def teardown(self, resource: ImagePullSpec) -> None:
+        pass
+
+
+class ImagePullSpecGenerator(SpecGenerator[ImagePullSpec]):
+    def __init__(self, args: ImagePullCheckSpec) -> None:
+        self._args = args
+        self._provisioner = ImagePullSpecProvisioner()
+
+    async def wait_for_spec(self) -> ImagePullSpec:
+        return await self._provisioner.setup(self._args)
+
+
+@dataclass
+class ImagePullResult:
+    image_ref: ImageRef
+    did_pull: bool
+
+
+class ImagePullProvisioner(Provisioner[ImagePullSpec, ImagePullResult]):
+    @property
+    @override
+    def name(self) -> str:
+        return "docker-image-pull"
+
+    @override
+    async def setup(self, spec: ImagePullSpec) -> ImagePullResult:
+        if spec.do_pull:
+            image_ref = await self._pull_image(spec)
+            did_pull = True
+        else:
+            image_ref = spec.image_ref
+            did_pull = False
+        return ImagePullResult(image_ref=image_ref, did_pull=did_pull)
 
     async def _pull_image(self, spec: ImagePullSpec) -> ImageRef:
         auth_config: Optional[dict[str, str]] = None
