cb5120b5f529fcaedd43f377056e268390dffdd2
Sanghun Lee <sanghun@lablup.com>
2025-07-13 08:35:05 +0900

    wip

---
+++ src/ai/backend/manager/repositories/permission_controller/repository.py
@@ -0,0 +1,8 @@
+from ai.backend.manager.models.utils import ExtendedAsyncSAEngine
+
+
+class PermissionControllerRepository:
+    _db: ExtendedAsyncSAEngine
+
+    def __init__(self, db: ExtendedAsyncSAEngine) -> None:
+        self._db = db
