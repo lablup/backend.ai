46999c167f460c8b15779ae8edca8732d6774188
Sanghun Lee <sanghun@lablup.com>
2025-07-15 22:26:07 +0900

    feat(BA-1838): Update RBAC DB schema (#5083)

---
+++ src/ai/backend/manager/models/rbac_models/association_scopes_entities.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import uuid
+from datetime import datetime
+
+import sqlalchemy as sa
+
+from ..base import (
+    Base,
+    IDColumn,
+)
+
+
+class AssociationScopesEntitiesRow(Base):
+    __tablename__ = "association_scopes_entities"
+    __table_args__ = (
+        # constraint
+        sa.UniqueConstraint("scope_id", "entity_id", name="uq_scope_id_entity_id"),
+    )
+
+    id: uuid.UUID = IDColumn()
+
+    scope_type: str = sa.Column(
+        "scope_type",
+        sa.String(32),
+        nullable=False,
+    )  # e.g., "global", "domain", "project", "user" etc.
+    scope_id: str = sa.Column(
+        "scope_id",
+        sa.String(64),
+        nullable=False,
+    )  # e.g., "global", "domain_id", "project_id", "user_id" etc.
+    entity_type: str = sa.Column(
+        "entity_type", sa.String(32), nullable=False
+    )  # e.g., "session", "vfolder", "image" etc.
+    entity_id: str = sa.Column(
+        "entity_id",
+        sa.String(64),
+        nullable=False,
+    )
+    registered_at: datetime = sa.Column(
+        "registered_at",
+        sa.DateTime(timezone=True),
+        nullable=False,
+        server_default=sa.func.now(),
+    )
--- src/ai/backend/manager/models/rbac_models/object_permission.py
+++ src/ai/backend/manager/models/rbac_models/object_permission.py
@@ -15,35 +15,47 @@ from ..base import (
     GUID,
     Base,
     IDColumn,
+    StrEnumType,
 )
+from .types import PermissionStatus
 
 if TYPE_CHECKING:
     from .role import RoleRow
 
 
-class RolePermissionRow(Base):
-    __tablename__ = "role_permissions"
+class ObjectPermissionRow(Base):
+    __tablename__ = "object_permissions"
+    __table_args__ = (
+        sa.Index("ix_role_id_entity_id", "status", "role_id", "entity_id"),
+        sa.UniqueConstraint(
+            "entity_id", "operation", name="uq_object_permissions_entity_id_operation"
+        ),
+    )
 
     id: uuid.UUID = IDColumn()
+    status: PermissionStatus = sa.Column(
+        "status",
+        StrEnumType(PermissionStatus),
+        nullable=False,
+        default=PermissionStatus.ACTIVE,
+        server_default=PermissionStatus.ACTIVE,
+    )
     role_id: uuid.UUID = sa.Column("role_id", GUID, nullable=False)
     entity_type: str = sa.Column(
         "entity_type", sa.String(32), nullable=False
     )  # e.g., "session", "vfolder", "image" etc.
+    entity_id: str = sa.Column(
+        "entity_id", sa.String(64), nullable=False
+    )  # e.g., "session_id", "vfolder_id" etc.
     operation: str = sa.Column(
         "operation", sa.String(32), nullable=False
     )  # e.g., "create", "read", "delete", "grant:create", "grant:read" etc.
-    scope_type: str = sa.Column(
-        "scope_type", sa.String(32), nullable=False
-    )  # e.g., "global", "domain", "project", "user" etc.
-    scope_id: str = sa.Column(
-        "scope_id", sa.String(64), nullable=False
-    )  # e.g., "global", "domain_id", "project_id", "user_id" etc.
     created_at: datetime = sa.Column(
         "created_at", sa.DateTime(timezone=True), nullable=False, server_default=sa.func.now()
     )
 
     role_row: RoleRow = relationship(
         "RoleRow",
-        back_populates="role_permission_rows",
-        primaryjoin="RoleRow.id == foreign(RolePermissionRow.role_id)",
+        back_populates="object_permission_rows",
+        primaryjoin="RoleRow.id == foreign(ObjectPermissionRow.role_id)",
     )
