d452963ed9f244f5952ecd580690b13dd0298e17
HyeockJinKim <hyeokjin@lablup.com>
2025-07-16 14:47:39 +0900

    remove unnecessary exception handling

--- src/ai/backend/common/middlewares/exception.py
+++ src/ai/backend/common/middlewares/exception.py
@@ -4,7 +4,6 @@ import logging
 from aiohttp import web
 from aiohttp.typedefs import Handler
 
-from ai.backend.common.exception import BackendAIError
 from ai.backend.logging.utils import BraceStyleAdapter
 
 log = BraceStyleAdapter(logging.getLogger(__spec__.name))
@@ -19,19 +18,6 @@ async def general_exception_middleware(
     log.info("Handling request: ({}) {}", method, endpoint)
     try:
         resp = await handler(request)
-    except BackendAIError as ex:
-        if ex.status_code // 100 == 4:
-            log.warning(
-                "client error raised inside handlers: ({} {}): {}", method, endpoint, repr(ex)
-            )
-        elif ex.status_code // 100 == 5:
-            log.exception(
-                "Internal server error raised inside handlers: ({} {}): {}",
-                method,
-                endpoint,
-                repr(ex),
-            )
-        raise
     except web.HTTPException as ex:
         if ex.status_code // 100 == 4:
             log.warning("client error raised inside handlers: ({} {}): {}", method, endpoint, ex)
