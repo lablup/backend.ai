3cb01282b2120998e835c2d28f69b60fb89d3895
HyeockJinKim <hyeokjin@lablup.com>
2025-07-22 13:34:58 +0900

    feat: Implement ActionValidator

--- src/ai/backend/manager/actions/processor.py
+++ src/ai/backend/manager/actions/processor.py
@@ -1,10 +1,13 @@
 import asyncio
+import logging
 import uuid
 from datetime import datetime
 from typing import Awaitable, Callable, Generic, Optional
 
 from ai.backend.common.exception import BackendAIError, ErrorCode
+from ai.backend.logging.utils import BraceStyleAdapter
 from ai.backend.manager.actions.types import OperationStatus
+from ai.backend.manager.actions.validators.validator import ActionValidator
 
 from .action import (
     BaseActionResultMeta,
@@ -15,18 +18,23 @@ from .action import (
 )
 from .monitors.monitor import ActionMonitor
 
+log = BraceStyleAdapter(logging.getLogger(__spec__.name))
+
 
 class ActionProcessor(Generic[TAction, TActionResult]):
     _monitors: list[ActionMonitor]
+    _validators: list[ActionValidator]
     _func: Callable[[TAction], Awaitable[TActionResult]]
 
     def __init__(
         self,
         func: Callable[[TAction], Awaitable[TActionResult]],
         monitors: Optional[list[ActionMonitor]] = None,
+        validators: Optional[list[ActionValidator]] = None,
     ) -> None:
         self._func = func
         self._monitors = monitors or []
+        self._validators = validators or []
 
     async def _run(self, action: TAction) -> TActionResult:
         started_at = datetime.now()
@@ -38,11 +46,17 @@ class ActionProcessor(Generic[TAction, TActionResult]):
         action_id = uuid.uuid4()
         action_trigger_meta = BaseActionTriggerMeta(action_id=action_id, started_at=started_at)
         for monitor in self._monitors:
-            await monitor.prepare(action, action_trigger_meta)
+            try:
+                await monitor.prepare(action, action_trigger_meta)
+            except Exception as e:
+                log.warning("Error in monitor prepare method: {}", e)
         try:
+            for validator in self._validators:
+                await validator.validate(action, action_trigger_meta)
             result = await self._func(action)
             status = OperationStatus.SUCCESS
             description = "Success"
+            return result
         except BackendAIError as e:
             status = OperationStatus.ERROR
             description = str(e)
@@ -71,8 +85,10 @@ class ActionProcessor(Generic[TAction, TActionResult]):
             )
             process_result = ProcessResult(meta=meta)
             for monitor in reversed(self._monitors):
-                await monitor.done(action, process_result)
-        return result
+                try:
+                    await monitor.done(action, process_result)
+                except Exception as e:
+                    log.warning("Error in monitor done method: {}", e)
 
     async def wait_for_complete(self, action: TAction) -> TActionResult:
         return await self._run(action)
