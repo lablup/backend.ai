799a176e2a42e6370472eaa804a75276471b7afd
jopemachine <gbl@lablup.com>
2025-07-28 08:03:57 +0000

    fix: Reflect feedback

--- src/ai/backend/manager/api/admin.py
+++ src/ai/backend/manager/api/admin.py
@@ -164,29 +164,8 @@ async def handle_gql_v2(request: web.Request, params: Any) -> web.Response:
             validation_result = ExecutionResult(None, errors=validate_errors)
             return web.json_response(validation_result.formatted, status=HTTPStatus.BAD_REQUEST)
 
-    manager_status = await root_ctx.config_provider.legacy_etcd_config_loader.get_manager_status()
-    known_slot_types = await root_ctx.config_provider.legacy_etcd_config_loader.get_resource_slots()
-
     strawberry_ctx = StrawberryGQLContext(
-        dataloader_manager=DataLoaderManager(),
-        config_provider=root_ctx.config_provider,
-        etcd=root_ctx.etcd,
         user=request["user"],
-        access_key=request["keypair"]["access_key"],
-        db=root_ctx.db,
-        valkey_stat=root_ctx.valkey_stat,
-        valkey_image=root_ctx.valkey_image,
-        valkey_live=root_ctx.valkey_live,
-        network_plugin_ctx=root_ctx.network_plugin_ctx,
-        manager_status=manager_status,
-        known_slot_types=known_slot_types,
-        background_task_manager=root_ctx.background_task_manager,
-        services_ctx=root_ctx.services_ctx,
-        storage_manager=root_ctx.storage_manager,
-        registry=root_ctx.registry,
-        idle_checker_host=root_ctx.idle_checker_host,
-        metric_observer=root_ctx.metrics.gql,
-        processors=root_ctx.processors,
     )
 
     result = await app_ctx.gql_v2_schema.execute(
--- src/ai/backend/manager/api/gql/types.py
+++ src/ai/backend/manager/api/gql/types.py
@@ -2,53 +2,11 @@
 from __future__ import annotations
 
 from collections.abc import Mapping
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 import attrs
-
-from ai.backend.common.clients.valkey_client.valkey_image.client import ValkeyImageClient
-from ai.backend.common.clients.valkey_client.valkey_live.client import ValkeyLiveClient
-from ai.backend.common.clients.valkey_client.valkey_stat.client import ValkeyStatClient
-from ai.backend.common.etcd import AsyncEtcd
-from ai.backend.common.metrics.metric import GraphQLMetricObserver
-from ai.backend.manager.api import ManagerStatus
-from ai.backend.manager.config.provider import ManagerConfigProvider
-from ai.backend.manager.idle import IdleCheckerHost
-from ai.backend.manager.models.base import DataLoaderManager
-from ai.backend.manager.models.storage import StorageSessionManager
-from ai.backend.manager.models.utils import ExtendedAsyncSAEngine
-from ai.backend.manager.plugin.network import NetworkPluginContext
-from ai.backend.manager.registry import AgentRegistry
-from ai.backend.manager.service.base import ServicesContext
-from ai.backend.manager.services.processors import Processors
-
-if TYPE_CHECKING:
-    from ai.backend.common.bgtask.bgtask import BackgroundTaskManager
-    from ai.backend.common.etcd import AsyncEtcd
-    from ai.backend.common.types import (
-        SlotName,
-        SlotTypes,
-    )
 
 
 @attrs.define(auto_attribs=True, slots=True)
 class StrawberryGQLContext:
-    dataloader_manager: DataLoaderManager
-    config_provider: ManagerConfigProvider
-    etcd: AsyncEtcd
     user: Mapping[str, Any]  # TODO: express using typed dict
-    access_key: str
-    db: ExtendedAsyncSAEngine
-    network_plugin_ctx: NetworkPluginContext
-    services_ctx: ServicesContext
-    valkey_stat: ValkeyStatClient
-    valkey_live: ValkeyLiveClient
-    valkey_image: ValkeyImageClient
-    manager_status: ManagerStatus
-    known_slot_types: Mapping[SlotName, SlotTypes]
-    background_task_manager: BackgroundTaskManager
-    storage_manager: StorageSessionManager
-    registry: AgentRegistry
-    idle_checker_host: IdleCheckerHost
-    metric_observer: GraphQLMetricObserver
-    processors: Processors
