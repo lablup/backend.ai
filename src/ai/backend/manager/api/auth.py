296d600f1c5710eef4da8136d9c0f7ec9756abce
Bokyum Kim | 김보겸 <bkkim@lablup.com>
2025-07-31 15:04:35 +0900

    fix(BA-1999): Handle None user when request context setup in auth middleware (#5327)

--- src/ai/backend/manager/api/auth.py
+++ src/ai/backend/manager/api/auth.py
@@ -538,25 +538,26 @@ async def auth_middleware(request: web.Request, handler) -> web.StreamResponse:
         request.update(auth_result)
 
     with ExitStack() as stack:
-        user_id = request.get("user", {}).get("uuid")
-        if user_id is not None:
-            stack.enter_context(
-                with_user(
-                    UserData(
-                        user_id=user_id,
-                        is_authorized=request.get("is_authorized", False),
-                        is_admin=request.get("is_admin", False),
-                        is_superadmin=request.get("is_superadmin", False),
-                        role=request["user"]["role"],
-                        domain_name=request["user"]["domain_name"],
+        if user := request.get("user"):
+            user_id = user.get("uuid")
+            if user_id is not None:
+                stack.enter_context(
+                    with_user(
+                        UserData(
+                            user_id=user_id,
+                            is_authorized=request.get("is_authorized", False),
+                            is_admin=request.get("is_admin", False),
+                            is_superadmin=request.get("is_superadmin", False),
+                            role=request["user"]["role"],
+                            domain_name=request["user"]["domain_name"],
+                        )
                     )
                 )
-            )
-            stack.enter_context(
-                with_log_context_fields({
-                    "user_id": str(user_id),
-                })
-            )
+                stack.enter_context(
+                    with_log_context_fields({
+                        "user_id": str(user_id),
+                    })
+                )
         # No matter if authenticated or not, pass-through to the handler.
         # (if it's required, `auth_required` decorator will handle the situation.)
         return await handler(request)
