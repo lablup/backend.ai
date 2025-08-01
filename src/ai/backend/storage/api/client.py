c5ef31e0f3f7f891ed9e2f8f808049f0cd24b085
jopemachine <gbl@lablup.com>
2025-08-01 02:59:04 +0000

    fix: WIP

--- src/ai/backend/storage/api/client.py
+++ src/ai/backend/storage/api/client.py
@@ -41,6 +41,8 @@ from .. import __version__
 from ..exception import InvalidAPIParameters
 from ..types import SENTINEL
 from ..utils import CheckParamSource, check_params
+from .v1.registries import create_app as create_registries_app
+from .v1.storages import create_app as create_storages_app
 
 if TYPE_CHECKING:
     from ..context import RootContext
@@ -460,4 +462,6 @@ async def init_client_app(ctx: RootContext) -> web.Application:
     r.add_route("HEAD", tus_check_session)
     r.add_route("PATCH", tus_upload_part)
 
+    app.add_subapp("/v1/storages", create_storages_app(ctx))
+    app.add_subapp("/v1/registries", create_registries_app(ctx))
     return app
