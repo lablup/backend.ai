bd4ad57555b1d7d261128c50fbf727e9f673295d
jopemachine <gbl@lablup.com>
2025-08-05 05:20:28 +0000

    feat: Add ObjectStorage update, delete service (WIP)

---
+++ src/ai/backend/manager/services/object_storage/actions/update.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Optional, override
+
+from ai.backend.manager.actions.action import BaseActionResult
+from ai.backend.manager.data.object_storage.modifier import ObjectStorageModifier
+from ai.backend.manager.data.object_storage.types import ObjectStorageData
+from ai.backend.manager.services.model_serving.actions.base import ModelServiceAction
+
+
+@dataclass
+class UpdateObjectStorageAction(ModelServiceAction):
+    modifier: ObjectStorageModifier
+
+    @override
+    def entity_id(self) -> Optional[str]:
+        return None
+
+    @override
+    @classmethod
+    def operation_type(cls) -> str:
+        return "update"
+
+
+@dataclass
+class UpdateObjectStorageActionResult(BaseActionResult):
+    result: ObjectStorageData
+
+    @override
+    def entity_id(self) -> Optional[str]:
+        return str(self.result.id)
--- src/ai/backend/manager/services/object_storage/service.py
+++ src/ai/backend/manager/services/object_storage/service.py
@@ -6,6 +6,14 @@ from ai.backend.manager.services.object_storage.actions.create import (
     CreateObjectStorageAction,
     CreateObjectStorageActionResult,
 )
+from ai.backend.manager.services.object_storage.actions.delete import (
+    DeleteObjectStorageAction,
+    DeleteObjectStorageActionResult,
+)
+from ai.backend.manager.services.object_storage.actions.update import (
+    UpdateObjectStorageAction,
+    UpdateObjectStorageActionResult,
+)
 
 log = BraceStyleAdapter(logging.getLogger(__spec__.name))  # type: ignore
 
@@ -23,3 +31,19 @@ class ObjectStorageService:
         log.info("Creating object storage with data: {}", action.creator)
         storage_data = await self._object_storage_repository.create(action.creator)
         return CreateObjectStorageActionResult(result=storage_data)
+
+    async def update(self, action: UpdateObjectStorageAction) -> UpdateObjectStorageActionResult:
+        """
+        Update an existing object storage.
+        """
+        log.info("Updating object storage with data: {}", action.modifier)
+        storage_data = await self._object_storage_repository.update(action.modifier)
+        return UpdateObjectStorageActionResult(result=storage_data)
+
+    async def delete(self, action: DeleteObjectStorageAction) -> DeleteObjectStorageActionResult:
+        """
+        Delete an existing object storage.
+        """
+        log.info("Deleting object storage with id: {}", action.storage_id)
+        storage_data = await self._object_storage_repository.delete(action.storage_id)
+        return DeleteObjectStorageActionResult(deleted_storage_id=storage_data)
