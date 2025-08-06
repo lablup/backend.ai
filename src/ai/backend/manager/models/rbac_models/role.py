95fe2ad98d26031591775d4af3a9a0bb81954177
Sanghun Lee <sanghun@lablup.com>
2025-08-06 13:48:15 +0900

    restore scope_permission table

--- src/ai/backend/manager/models/rbac_models/role.py
+++ src/ai/backend/manager/models/rbac_models/role.py
@@ -57,15 +57,6 @@ class RoleRow(Base):
         default=RoleStatus.ACTIVE,
         server_default=RoleStatus.ACTIVE,
     )
-    operation: str = sa.Column(
-        "operation", sa.String(32), nullable=False
-    )  # e.g., "create", "read", "delete", "grant:create", "grant:read" etc.
-    scope_type: str = sa.Column(
-        "scope_type", sa.String(32), nullable=False
-    )  # e.g., "global", "domain", "project", "user" etc.
-    scope_id: str = sa.Column(
-        "scope_id", sa.String(64), nullable=False
-    )  # e.g., "global", "domain_id", "project_id", "user_id" etc.
     created_at: datetime = sa.Column(
         "created_at", sa.DateTime(timezone=True), nullable=False, server_default=sa.func.now()
     )
