ab25673ae1bee9f312d7026c8a777dbc1c3e8c26
Sanghun Lee <sanghun@lablup.com>
2025-08-11 16:20:48 +0900

    remove user_role input in batch group type

--- src/ai/backend/manager/models/rbac_models/migration/utils.py
+++ src/ai/backend/manager/models/rbac_models/migration/utils.py
@@ -9,7 +9,6 @@ from .models import (
     get_object_permissions_table,
     get_roles_table,
     get_scope_permissions_table,
-    get_user_roles_table,
 )
 from .types import RoleCreationInputGroup
 
@@ -31,13 +30,9 @@ def batch_insert_from_role_creation_input_group(
     )
     role_records = cast(list[uuid.UUID], role_result.all())
 
-    user_role_values: list[dict[str, Any]] = []
     scope_permission_values: list[dict[str, Any]] = []
     object_permission_values: list[dict[str, Any]] = []
     for group, role_id in zip(input_groups, role_records):
-        if group.user_role is not None:
-            user_role_input = group.user_role.to_input(role_id)
-            user_role_values.append(user_role_input.to_dict())
         for scope_permission in group.scope_permissions:
             scope_permission_input = scope_permission.to_input(role_id)
             scope_permission_values.append(scope_permission_input.to_dict())
@@ -45,6 +40,5 @@ def batch_insert_from_role_creation_input_group(
             object_permission_input = object_permission.to_input(role_id)
             object_permission_values.append(object_permission_input.to_dict())
 
-    insert_if_data_exists(db_conn, get_user_roles_table(), user_role_values)
     insert_if_data_exists(db_conn, get_scope_permissions_table(), scope_permission_values)
     insert_if_data_exists(db_conn, get_object_permissions_table(), object_permission_values)
