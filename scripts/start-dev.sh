392c36b777374bf2be569651ba4618cc755cc5b9
Yubin Kim <yubin@lablup.com>
2025-08-19 17:19:10 +0900

    chore: wait for 200 sec

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -15,10 +15,7 @@ tmux new-session -d -s backendai-appproxy-dev
 tmux new-window -t backendai-appproxy-dev -n "coordinator"
 tmux send-keys "./backend.ai app-proxy-coordinator start-server --debug" C-m
 
-until redis-cli -h $REDIS ping && redis-cli -h $REDIS hget worker:<id> created_at; do
-    echo "waiting for worker init..."
-    sleep 2
-done
+sleep 200
 
 tmux new-window -t backendai-appproxy-dev -n "worker"
 tmux send-keys "./backend.ai app-proxy-worker start-server --debug" C-m
