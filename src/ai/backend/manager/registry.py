036af8cc089f2bd96ef1a7e603ed60dcdfb9ebaf
Sanghun Lee <oper6909@gmail.com>
2025-08-26 15:29:14 +0900

    simple type checks

--- src/ai/backend/manager/registry.py
+++ src/ai/backend/manager/registry.py
@@ -2152,32 +2152,24 @@ class AgentRegistry:
             updates = {}
             keys = await self._scan_keys(r, f"{COMPUTE_CONCURRENCY_USED_KEY_PREFIX}*")
             for stat_key in keys:
-                if isinstance(stat_key, bytes):
-                    _stat_key = stat_key.decode("utf-8")
-                else:
-                    _stat_key = cast(str, stat_key)
-                ak = _stat_key.replace(COMPUTE_CONCURRENCY_USED_KEY_PREFIX, "")
+                ak = stat_key.replace(COMPUTE_CONCURRENCY_USED_KEY_PREFIX, "")
                 concurrent_sessions = access_key_to_concurrency_used.get(AccessKey(ak))
                 usage = (
                     len(concurrent_sessions.compute_session_ids)
                     if concurrent_sessions is not None
                     else 0
                 )
-                updates[_stat_key] = usage
+                updates[stat_key] = usage
             keys = await self._scan_keys(r, f"{SYSTEM_CONCURRENCY_USED_KEY_PREFIX}*")
             for stat_key in keys:
-                if isinstance(stat_key, bytes):
-                    _stat_key = stat_key.decode("utf-8")
-                else:
-                    _stat_key = cast(str, stat_key)
-                ak = _stat_key.replace(SYSTEM_CONCURRENCY_USED_KEY_PREFIX, "")
+                ak = stat_key.replace(SYSTEM_CONCURRENCY_USED_KEY_PREFIX, "")
                 concurrent_sessions = access_key_to_concurrency_used.get(AccessKey(ak))
                 usage = (
                     len(concurrent_sessions.system_concurrency_used_key)
                     if concurrent_sessions is not None
                     else 0
                 )
-                updates[_stat_key] = usage
+                updates[stat_key] = usage
             if updates:
                 await r.mset(typing.cast(MSetType, updates))
 
@@ -2213,6 +2205,7 @@ class AgentRegistry:
         while True:
             try:
                 await self.recalc_resource_usage(do_fullscan=True)
+                log.debug("Recalculated resource usage periodically")
             except Exception:
                 log.exception("Failed to recalculate resource usage periodically")
             await asyncio.sleep(60)
