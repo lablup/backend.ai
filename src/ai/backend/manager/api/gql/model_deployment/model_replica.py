fb0d5bd0d985ed76006da11e0eef7b17ab2c77e8
BoGyum Kim <bkkim@lablup.com>
2025-09-09 18:34:25 +0900

    fix: Change replica filter id filed type to ID

--- src/ai/backend/manager/api/gql/model_deployment/model_replica.py
+++ src/ai/backend/manager/api/gql/model_deployment/model_replica.py
@@ -62,7 +62,7 @@ class ReplicaFilter:
     readiness_status: Optional[ReadinessStatusFilter] = None
     liveness_status: Optional[LivenessStatusFilter] = None
     activeness_status: Optional[ActivenessStatusFilter] = None
-    id: Optional[UUID] = None
+    id: Optional[ID] = None
 
     AND: Optional[list["ReplicaFilter"]] = None
     OR: Optional[list["ReplicaFilter"]] = None
