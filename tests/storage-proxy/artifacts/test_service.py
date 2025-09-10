ffeff1889b75fd8a5ec1240384e9e255707414e5
jopemachine <gbl@lablup.com>
2025-09-10 05:41:53 +0000

    fix: Broken CI

--- tests/storage-proxy/artifacts/test_service.py
+++ tests/storage-proxy/artifacts/test_service.py
@@ -757,9 +757,6 @@ class TestReservoirService:
 
         with (
             patch.object(reservoir_service, "_stream_bucket_to_bucket") as mock_stream,
-            patch.object(
-                reservoir_service._event_producer, "anycast_event", new=AsyncMock()
-            ) as mock_event,
         ):
             mock_stream.return_value = 1000
 
@@ -772,7 +769,6 @@ class TestReservoirService:
             )
 
             mock_stream.assert_called_once()
-            mock_event.assert_called_once()
 
     @pytest.mark.asyncio
     async def test_import_model_no_registry_config(self) -> None:
