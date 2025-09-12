5bba141fa0d42e98655884b95639bb3b0c58318c
Jeongseok Kang <jskang@lablup.com>
2025-09-12 14:53:33 +0900

    feat(BA-2325): Add SyncClientPool

--- src/ai/backend/common/clients/http_client/client_pool.py
+++ src/ai/backend/common/clients/http_client/client_pool.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import abc
 import asyncio
 import inspect
 import logging
@@ -12,6 +13,9 @@ from typing import Optional, Protocol
 import aiohttp
 
 from ai.backend.logging.utils import BraceStyleAdapter
+
+from ...sync import SyncWorkerThread
+from ...types import Sentinel
 
 log = BraceStyleAdapter(logging.getLogger(__spec__.name))
 
@@ -68,8 +72,16 @@ class ClientKey:
     """An optional identifier to associate with the API request context."""
 
 
-class ClientPool:
+class BaseClientPool(metaclass=abc.ABCMeta):
     _clients: MutableMapping[ClientKey, _Client]
+
+    async def close(self) -> None:
+        for client in self._clients.values():
+            await client.session.close()
+        self._clients.clear()
+
+
+class ClientPool(BaseClientPool):
     _cleanup_task: asyncio.Task[None]
 
     def __init__(
@@ -95,9 +107,7 @@ class ClientPool:
             except asyncio.CancelledError:
                 # FIXME: use safer cancel-and-wait approach
                 pass
-        for client in self._clients.values():
-            await client.session.close()
-        self._clients.clear()
+        await super().close()
 
     def __repr__(self) -> str:
         return (
@@ -132,3 +142,61 @@ class ClientPool:
         client_session = self._client_session_factory(key)
         self._clients[key] = _Client(session=client_session, last_used=now)
         return client_session
+
+
+AsyncClientPool = ClientPool
+
+
+class SyncClientPool(BaseClientPool):
+    def __init__(
+        self,
+        factory: ClientSessionFactory,
+        cleanup_interval_seconds: float = 600,
+    ) -> None:
+        frame = inspect.stack()[1]
+        self._creator_info = f"{frame.filename}:{frame.lineno}:{frame.function}()"
+        self._worker_thread = SyncWorkerThread()
+        self._worker_thread.start()
+        self._worker_thread.execute(self._cleanup_loop(cleanup_interval_seconds))
+
+        self._client_session_factory = factory
+        self._clients = {}
+
+    def __del__(self) -> None:
+        if self._clients:
+            warnings.warn(
+                f"{self!r} is garbage-collected but still has active client sessions.",
+                ResourceWarning,
+            )
+
+    def __repr__(self) -> str:
+        return f"<http_client.SyncClientPool object at {hex(id(self))} created from {self._creator_info}>"
+
+    async def _cleanup_loop(self, cleanup_interval_seconds: float) -> None:
+        while True:
+            await asyncio.sleep(cleanup_interval_seconds)
+            now = time.perf_counter()
+            for key, client in tuple(self._clients.items()):
+                if now - client.last_used > cleanup_interval_seconds:
+                    del self._clients[key]
+                    try:
+                        await client.session.close()
+                    except Exception as e:
+                        log.exception("Error closing client session: {}", e)
+
+    def load_client_session(self, key: ClientKey) -> aiohttp.ClientSession:
+        session = self._clients.get(key, None)
+        now = time.perf_counter()
+        if session is not None:
+            session.last_used = now
+            return session.session
+        client_session = self._client_session_factory(key)
+        self._clients[key] = _Client(session=client_session, last_used=now)
+        return client_session
+
+    async def close(self) -> None:
+        if self._worker_thread.is_alive():
+            self._worker_thread.interrupt_generator()
+            self._worker_thread.work_queue.put(Sentinel.TOKEN)
+            self._worker_thread.join()
+        await super().close()
