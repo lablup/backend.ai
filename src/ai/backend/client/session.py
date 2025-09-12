426750367469a33cd456109cd7b3367d44dad658
Jeongseok Kang <jskang@lablup.com>
2025-09-12 14:53:18 +0900

    refactor(BA-2325): Migrate SyncWorkerThread from client to common package

--- src/ai/backend/client/session.py
+++ src/ai/backend/client/session.py
@@ -2,30 +2,24 @@ from __future__ import annotations
 
 import abc
 import asyncio
-import inspect
-import queue
-import threading
 import warnings
-from contextvars import Context, ContextVar, copy_context
+from contextvars import ContextVar
 from typing import (
-    Any,
-    AsyncIterator,
     Awaitable,
-    Coroutine,
-    Iterator,
     Literal,
     Optional,
     Tuple,
-    TypeVar,
     Union,
 )
 
 import aiohttp
 from multidict import CIMultiDict
 
+from ai.backend.common.sync import SyncWorkerThread
+from ai.backend.common.types import Sentinel
+
 from .config import MIN_API_VERSION, APIConfig, get_config, parse_api_version
 from .exceptions import APIVersionWarning, BackendAPIError, BackendClientError
-from .types import Sentinel, sentinel
 
 __all__ = (
     "BaseSession",
@@ -124,116 +118,6 @@ async def _close_aiohttp_session(session: aiohttp.ClientSession) -> None:
     await session.close()
     if transports > 0:
         await all_is_lost.wait()
-
-
-_Item = TypeVar("_Item")
-
-
-class _SyncWorkerThread(threading.Thread):
-    work_queue: queue.Queue[
-        Union[
-            Tuple[Union[AsyncIterator, Coroutine], Context],
-            Sentinel,
-        ]
-    ]
-    done_queue: queue.Queue[Union[Any, Exception]]
-    stream_queue: queue.Queue[Union[Any, Exception, Sentinel]]
-    stream_block: threading.Event
-    agen_shutdown: bool
-
-    __slots__ = (
-        "work_queue",
-        "done_queue",
-        "stream_queue",
-        "stream_block",
-        "agen_shutdown",
-    )
-
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self.work_queue = queue.Queue()
-        self.done_queue = queue.Queue()
-        self.stream_queue = queue.Queue()
-        self.stream_block = threading.Event()
-        self.agen_shutdown = False
-
-    def run(self) -> None:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        try:
-            while True:
-                item = self.work_queue.get()
-                if item is sentinel:
-                    break
-                coro, ctx = item
-                if inspect.isasyncgen(coro):
-                    ctx.run(loop.run_until_complete, self.agen_wrapper(coro))
-                else:
-                    try:
-                        # FIXME: Once python/mypy#12756 is resolved, remove the type-ignore tag.
-                        result = ctx.run(loop.run_until_complete, coro)  # type: ignore
-                    except Exception as e:
-                        self.done_queue.put_nowait(e)
-                    else:
-                        self.done_queue.put_nowait(result)
-                self.work_queue.task_done()
-        except (SystemExit, KeyboardInterrupt):
-            pass
-        finally:
-            loop.run_until_complete(loop.shutdown_asyncgens())
-            loop.stop()
-            loop.close()
-
-    def execute(self, coro: Coroutine) -> Any:
-        ctx = copy_context()  # preserve context for the worker thread
-        try:
-            self.work_queue.put((coro, ctx))
-            result = self.done_queue.get()
-            self.done_queue.task_done()
-            if isinstance(result, Exception):
-                raise result
-            return result
-        finally:
-            del ctx
-
-    async def agen_wrapper(self, agen):
-        self.agen_shutdown = False
-        try:
-            async for item in agen:
-                self.stream_block.clear()
-                self.stream_queue.put(item)
-                # flow-control the generator.
-                self.stream_block.wait()
-                if self.agen_shutdown:
-                    break
-        except Exception as e:
-            self.stream_queue.put(e)
-        finally:
-            self.stream_queue.put(sentinel)
-            await agen.aclose()
-
-    def execute_generator(self, asyncgen: AsyncIterator[_Item]) -> Iterator[_Item]:
-        ctx = copy_context()  # preserve context for the worker thread
-        try:
-            self.work_queue.put((asyncgen, ctx))
-            while True:
-                item = self.stream_queue.get()
-                try:
-                    if item is sentinel:
-                        break
-                    if isinstance(item, Exception):
-                        raise item
-                    yield item
-                finally:
-                    self.stream_block.set()
-                    self.stream_queue.task_done()
-        finally:
-            del ctx
-
-    def interrupt_generator(self):
-        self.agen_shutdown = True
-        self.stream_block.set()
-        self.stream_queue.put(sentinel)
 
 
 class BaseSession(metaclass=abc.ABCMeta):
@@ -424,7 +308,7 @@ class Session(BaseSession):
         aiohttp_session: Optional[aiohttp.ClientSession] = None,
     ) -> None:
         super().__init__(config=config, proxy_mode=proxy_mode)
-        self._worker_thread = _SyncWorkerThread()
+        self._worker_thread = SyncWorkerThread()
         self._worker_thread.start()
 
         if aiohttp_session is not None:
@@ -458,7 +342,7 @@ class Session(BaseSession):
         self._worker_thread.interrupt_generator()
         if self._owns_session:
             self._worker_thread.execute(_close_aiohttp_session(self.aiohttp_session))
-        self._worker_thread.work_queue.put(sentinel)
+        self._worker_thread.work_queue.put(Sentinel.TOKEN)
         self._worker_thread.join()
         api_session.reset(self._context_token)
 
