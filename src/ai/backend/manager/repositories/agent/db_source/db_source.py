06eda65e94b6f9583d90f45025ccd52a9dda80fe
BoGyum Kim <bkkim@lablup.com>
2025-09-17 18:10:43 +0900

    refactor: Check scaling group existence before data insert

--- src/ai/backend/manager/repositories/agent/db_source/db_source.py
+++ src/ai/backend/manager/repositories/agent/db_source/db_source.py
@@ -13,6 +13,7 @@ from ai.backend.manager.data.agent.types import (
 from ai.backend.manager.errors.resource import ScalingGroupNotFound
 from ai.backend.manager.models import agents
 from ai.backend.manager.models.agent import AgentRow
+from ai.backend.manager.models.scaling_group import ScalingGroupRow
 from ai.backend.manager.models.utils import ExtendedAsyncSAEngine
 from ai.backend.manager.services.agent.types import AgentData
 
@@ -36,6 +37,14 @@ class AgentDBSource:
 
     async def upsert_agent_with_state(self, upsert_data: AgentHeartbeatUpsert) -> UpsertResult:
         async with self._db.begin() as conn:
+            scaling_group_query = sa.select(ScalingGroupRow).where(
+                ScalingGroupRow.name == upsert_data.scaling_group
+            )
+            scaling_group_result = (await conn.execute(scaling_group_query)).scalar()
+            if scaling_group_result is None:
+                log.error("Scaling group named [{}] does not exist.", upsert_data.scaling_group)
+                raise ScalingGroupNotFound(upsert_data.scaling_group)
+
             query = sa.select(AgentRow).where(AgentRow.id == upsert_data.id).with_for_update()
             result = await conn.execute(query)
             row = result.first()
@@ -45,10 +54,7 @@ class AgentDBSource:
             final_query = stmt.on_conflict_do_update(
                 index_elements=["id"], set_=upsert_data.update_fields
             )
-            try:
-                await conn.execute(final_query)
-            except sa.exc.IntegrityError:
-                log.error("Scaling group named [{}] does not exist.", upsert_data.scaling_group)
-                raise ScalingGroupNotFound(upsert_data.scaling_group)
+
+            await conn.execute(final_query)
 
             return upsert_result
