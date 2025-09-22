5da6c240383c7d50c9713602d777d00a80c832fe
BoGyum Kim <bkkim@lablup.com>
2025-09-22 14:30:06 +0900

    refactor: Group relevant fields to dataclass in AgentHeartbeatUpsert

--- src/ai/backend/manager/data/agent/types.py
+++ src/ai/backend/manager/data/agent/types.py
@@ -23,37 +23,52 @@ class AgentStateSyncData:
 
 
 @dataclass
-class AgentHeartbeatUpsert:
+class AgentMetadata:
     id: AgentId
     status: AgentStatus
     region: str
     scaling_group: str
-    available_slots: ResourceSlot
+    architecture: str
+    version: str
+    auto_terminate_abusing_kernel: bool
+
+
+@dataclass
+class AgentNetworkInfo:
     addr: str
     public_host: str
-    public_key: str
-    version: str
+    public_key: PublicKey
+
+
+@dataclass
+class AgentResourceInfo:
+    available_slots: ResourceSlot
     compute_plugins: Mapping[DeviceName, Mapping[str, str]]
-    architecture: str
-    auto_terminate_abusing_kernel: bool
+
+
+@dataclass
+class AgentHeartbeatUpsert:
+    metadata: AgentMetadata
+    network_info: AgentNetworkInfo
+    resource_info: AgentResourceInfo
     lost_at: Optional[datetime]
     first_contact: datetime
 
     @property
     def insert_fields(self) -> dict[str, Any]:
         return {
-            "id": self.id,
+            "id": self.metadata.id,
             "status": AgentStatus.ALIVE,
-            "region": self.region,
-            "scaling_group": self.scaling_group,
-            "available_slots": self.available_slots,
-            "addr": self.addr,
-            "public_host": self.public_host,
-            "public_key": self.public_key,
-            "version": self.version,
-            "compute_plugins": self.compute_plugins,
-            "architecture": self.architecture,
-            "auto_terminate_abusing_kernel": self.auto_terminate_abusing_kernel,
+            "region": self.metadata.region,
+            "scaling_group": self.metadata.scaling_group,
+            "available_slots": self.resource_info.available_slots,
+            "addr": self.network_info.addr,
+            "public_host": self.network_info.public_host,
+            "public_key": self.network_info.public_key,
+            "version": self.metadata.version,
+            "compute_plugins": self.resource_info.compute_plugins,
+            "architecture": self.metadata.architecture,
+            "auto_terminate_abusing_kernel": self.metadata.auto_terminate_abusing_kernel,
             "lost_at": None,
             "occupied_slots": {},
             "first_contact": self.first_contact,
@@ -62,18 +77,18 @@ class AgentHeartbeatUpsert:
     @property
     def update_fields(self) -> dict[str, Any]:
         return {
-            "id": self.id,
+            "id": self.metadata.id,
             "status": AgentStatus.ALIVE,
-            "region": self.region,
-            "scaling_group": self.scaling_group,
-            "available_slots": self.available_slots,
-            "addr": self.addr,
-            "public_host": self.public_host,
-            "public_key": self.public_key,
-            "version": self.version,
-            "compute_plugins": self.compute_plugins,
-            "architecture": self.architecture,
-            "auto_terminate_abusing_kernel": self.auto_terminate_abusing_kernel,
+            "region": self.metadata.region,
+            "scaling_group": self.metadata.scaling_group,
+            "available_slots": self.resource_info.available_slots,
+            "addr": self.network_info.addr,
+            "public_host": self.network_info.public_host,
+            "public_key": self.network_info.public_key,
+            "version": self.metadata.version,
+            "compute_plugins": self.resource_info.compute_plugins,
+            "architecture": self.metadata.architecture,
+            "auto_terminate_abusing_kernel": self.metadata.auto_terminate_abusing_kernel,
             "lost_at": None,
         }
 
@@ -82,20 +97,28 @@ class AgentHeartbeatUpsert:
         cls, agent_id: AgentId, agent_info: Mapping[Any, Any], heartbeat_received: datetime
     ) -> Self:
         return cls(
-            id=agent_id,
-            status=AgentStatus.ALIVE,
-            region=agent_info["region"],
-            scaling_group=agent_info.get("scaling_group", "default"),
-            available_slots=ResourceSlot({
-                SlotName(k): Decimal(v[1]) for k, v in agent_info["resource_slots"].items()
-            }),
-            addr=agent_info["addr"],
-            public_host=agent_info["public_host"],
-            public_key=agent_info["public_key"],
-            version=agent_info["version"],
-            compute_plugins=agent_info["compute_plugins"],
-            architecture=agent_info.get("architecture", "x86_64"),
-            auto_terminate_abusing_kernel=agent_info.get("auto_terminate_abusing_kernel", False),
+            metadata=AgentMetadata(
+                id=agent_id,
+                status=AgentStatus.ALIVE,
+                region=agent_info["region"],
+                scaling_group=agent_info.get("scaling_group", "default"),
+                architecture=agent_info.get("architecture", "x86_64"),
+                auto_terminate_abusing_kernel=agent_info.get(
+                    "auto_terminate_abusing_kernel", False
+                ),
+                version=agent_info["version"],
+            ),
+            network_info=AgentNetworkInfo(
+                addr=agent_info["addr"],
+                public_host=agent_info["public_host"],
+                public_key=agent_info["public_key"],
+            ),
+            resource_info=AgentResourceInfo(
+                available_slots=ResourceSlot({
+                    SlotName(k): Decimal(v[1]) for k, v in agent_info["resource_slots"].items()
+                }),
+                compute_plugins=agent_info["compute_plugins"],
+            ),
             lost_at=None,
             first_contact=heartbeat_received,
         )
@@ -120,16 +143,16 @@ class UpsertResult:
 
         was_revived = existing_row.status in (AgentStatus.LOST, AgentStatus.TERMINATED)
         need_resource_slot_update = (
-            existing_row.available_slots != upsert_data.available_slots
-            or existing_row.scaling_group != upsert_data.scaling_group
-            or existing_row.addr != upsert_data.addr
-            or existing_row.public_host != upsert_data.public_host
-            or existing_row.public_key != upsert_data.public_key
-            or existing_row.version != upsert_data.version
-            or existing_row.compute_plugins != upsert_data.compute_plugins
-            or existing_row.architecture != upsert_data.architecture
+            existing_row.available_slots != upsert_data.resource_info.available_slots
+            or existing_row.scaling_group != upsert_data.metadata.scaling_group
+            or existing_row.addr != upsert_data.network_info.addr
+            or existing_row.public_host != upsert_data.network_info.public_host
+            or existing_row.public_key != upsert_data.network_info.public_key
+            or existing_row.version != upsert_data.metadata.version
+            or existing_row.compute_plugins != upsert_data.resource_info.compute_plugins
+            or existing_row.architecture != upsert_data.metadata.architecture
             or existing_row.auto_terminate_abusing_kernel
-            != upsert_data.auto_terminate_abusing_kernel
+            != upsert_data.metadata.auto_terminate_abusing_kernel
         )
         return cls(
             was_insert=False,
