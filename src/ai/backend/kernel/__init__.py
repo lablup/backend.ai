ea2caa9b9f0ebe2a5829bf3a68b73132a363cc49
Joongi Kim <joongi@lablup.com>
2025-09-27 01:12:13 +0900

    feat: Implement cmdline overriding
    - We need extra care for handling virtualenvs and site-package paths.
    - Add an alias of the "python" runtime-type to remove any mention to
      "python" from the cmdline.

--- src/ai/backend/kernel/__init__.py
+++ src/ai/backend/kernel/__init__.py
@@ -1,5 +1,52 @@
-from .base import BaseRunner
-from .terminal import Terminal
+import os
+import sys
+from pathlib import Path
+
+
+def _sitepkg_version() -> str:
+    version = sys.version_info
+    if hasattr(sys, "abiflags") and "t" in sys.abiflags:
+        abi_thread = "t"
+    else:
+        abi_thread = ""
+    return f"python{version[0]}.{version[1]}{abi_thread}"
+
+
+def _reexec_with_argv0(argv0: str) -> None:
+    if os.environ.get("BACKEND_REEXECED") != "1":
+        env = os.environ.copy()
+        env["BACKEND_REEXECED"] = "1"
+        mod_name = None
+        try:
+            spec = globals().get("__spec__")
+            if spec and getattr(spec, "name", None):
+                mod_name = spec.name
+        except Exception:
+            pass
+        argv1 = sys.argv[1]
+        # Prevent being terminated by "pkill -f python" by aliasing the runtime-type argument.
+        if argv1 == "python":
+            argv1 = "default"
+        # "-s" option is to disable user site configurations.
+        if mod_name:
+            argv = [argv0, "-s", "-m", mod_name, argv1] + sys.argv[2:]
+        else:
+            argv = [argv0, "-s", sys.argv[0], argv1] + sys.argv[2:]
+        os.execvpe(sys.executable, argv, env)
+    else:
+        venv = os.environ.get("VIRTUAL_ENV", None)
+        if venv is not None:
+            sys.prefix = venv
+            sys.path.insert(0, str(Path(venv) / "lib" / _sitepkg_version() / "site-packages"))
+
+
+# Prevent being terminated by "pkill -f python" by overriding the cmdline executable name.
+_reexec_with_argv0("bai-krunner")
+
+
+# The above reexec function must run before ANY import!
+from .base import BaseRunner  # noqa: E402
+from .terminal import Terminal  # noqa: E402
 
 __all__ = (
     "BaseRunner",
@@ -10,6 +57,7 @@ __all__ = (
 lang_map = {
     "app": "ai.backend.kernel.app.Runner",
     "python": "ai.backend.kernel.python.Runner",
+    "default": "ai.backend.kernel.python.Runner",
     "c": "ai.backend.kernel.c.Runner",
     "cpp": "ai.backend.kernel.cpp.Runner",
     "golang": "ai.backend.kernel.golang.Runner",
