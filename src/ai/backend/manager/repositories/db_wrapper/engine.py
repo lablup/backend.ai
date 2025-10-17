75e0088a3f3e7f67703dbda314e10eacf2a39088
Sanghun Lee <sanghun@lablup.com>
2025-10-17 22:16:28 +0900

    simple engine wrapper

--- src/ai/backend/manager/repositories/db_wrapper/engine.py
+++ src/ai/backend/manager/repositories/db_wrapper/engine.py
@@ -1,32 +1,20 @@
 from collections.abc import AsyncGenerator
-from typing import Generic
+
+from sqlalchemy.ext.asyncio import AsyncSession as SASession
 
 from ai.backend.manager.models.utils import ExtendedAsyncSAEngine
 
-from .session.creator import Creator
-from .session.querier import Querier
-from .session.updator import Updator
-from .types import TBaseEntityData, TRow, TUpdator
 
-
-class EngineWrapper(Generic[TRow, TBaseEntityData, TUpdator]):
+class EngineWrapper:
     _db: ExtendedAsyncSAEngine
 
     def __init__(self, db: ExtendedAsyncSAEngine) -> None:
         self._db = db
 
-    async def begin_session(
-        self,
-    ) -> AsyncGenerator[
-        tuple[Querier[TRow], Creator[TRow, TBaseEntityData], Updator[TUpdator]], None
-    ]:
+    async def begin_session(self) -> AsyncGenerator[SASession, None]:
         async with self._db.begin_session() as session:
-            querier = Querier[TRow](session)
-            creator = Creator[TRow, TBaseEntityData](session)
-            updator = Updator[TUpdator](session)
-            yield querier, creator, updator
+            yield session
 
-    async def begin_readonly_session(self) -> AsyncGenerator[Querier[TRow], None]:
+    async def begin_readonly_session(self) -> AsyncGenerator[SASession, None]:
         async with self._db.begin_readonly_session() as session:
-            querier = Querier[TRow](session)
-            yield querier
+            yield session
