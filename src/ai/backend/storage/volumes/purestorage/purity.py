75eea83c30aec2a74b3c1ec8125c42080f18094c
Sanghun Lee <sanghun@lablup.com>
2025-11-26 00:06:22 +0900

    fix: PureStorage client incorrectly using context variables

--- src/ai/backend/storage/volumes/purestorage/purity.py
+++ src/ai/backend/storage/volumes/purestorage/purity.py
@@ -1,20 +1,20 @@
 from __future__ import annotations
 
-from contextvars import ContextVar, Token
-from typing import Any, AsyncGenerator, Mapping
+from typing import Any, AsyncGenerator, Mapping, Optional
 
 import aiohttp
 from yarl import URL
+
+from .exceptions import UnauthorizedPurityClient
 
 
 class PurityClient:
     endpoint: URL
     api_token: str
     api_version: str
-    auth_token: ContextVar[str]
+    _auth_token: Optional[str]
 
     _session: aiohttp.ClientSession
-    _auth_token_cvtoken: Token
 
     def __init__(
         self,
@@ -26,7 +26,7 @@ class PurityClient:
         self.endpoint = URL(endpoint)
         self.api_token = api_token
         self.api_version = api_version
-        self.auth_token = ContextVar("auth_token")
+        self._auth_token = None
         self._session = aiohttp.ClientSession()
 
     async def aclose(self) -> None:
@@ -40,25 +40,25 @@ class PurityClient:
             raise_for_status=True,
         ) as resp:
             auth_token = resp.headers["x-auth-token"]
-            self._auth_token_cvtoken = self.auth_token.set(auth_token)
+            self._auth_token = auth_token
             _ = await resp.json()
         return self
 
     async def __aexit__(self, *exc_info) -> None:
-        self.auth_token.reset(self._auth_token_cvtoken)
+        self._auth_token = None
 
     # For the concrete API reference, check out:
     # https://purity-fb.readthedocs.io/en/latest/
 
     async def get_metadata(self) -> Mapping[str, Any]:
-        if self.auth_token is None:
-            raise RuntimeError("The auth token for Purity API is not initialized.")
+        if self._auth_token is None:
+            raise UnauthorizedPurityClient("The auth token for Purity API is not initialized.")
         items = []
         pagination_token = ""
         while True:
             async with self._session.get(
                 (self.endpoint / "api" / self.api_version / "arrays"),
-                headers={"x-auth-token": self.auth_token.get()},
+                headers={"x-auth-token": self._auth_token},
                 params={
                     "items_returned": 10,
                     "token": pagination_token,
@@ -89,13 +89,13 @@ class PurityClient:
         self,
         fs_name: str,
     ) -> AsyncGenerator[Mapping[str, Any], None]:
-        if self.auth_token is None:
-            raise RuntimeError("The auth token for Purity API is not initialized.")
+        if self._auth_token is None:
+            raise UnauthorizedPurityClient("The auth token for Purity API is not initialized.")
         pagination_token = ""
         while True:
             async with self._session.get(
                 (self.endpoint / "api" / self.api_version / "file-systems" / "performance"),
-                headers={"x-auth-token": self.auth_token.get()},
+                headers={"x-auth-token": self._auth_token},
                 params={
                     "names": fs_name,
                     "protocol": "NFS",
@@ -113,14 +113,14 @@ class PurityClient:
                     break
 
     async def get_usage(self, fs_name: str) -> Mapping[str, Any]:
-        if self.auth_token is None:
-            raise RuntimeError("The auth token for Purity API is not initialized.")
+        if self._auth_token is None:
+            raise UnauthorizedPurityClient("The auth token for Purity API is not initialized.")
         items = []
         pagination_token = ""
         while True:
             async with self._session.get(
                 (self.endpoint / "api" / self.api_version / "file-systems"),
-                headers={"x-auth-token": self.auth_token.get()},
+                headers={"x-auth-token": self._auth_token},
                 params={
                     "names": fs_name,
                     "items_returned": 10,
