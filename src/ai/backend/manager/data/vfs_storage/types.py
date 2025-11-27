e16cd9795449adac201127002d70bee74753dce4
jopemachine <gbl@lablup.com>
2025-11-27 23:14:52 +0000

    feat: Add convert logic

--- src/ai/backend/manager/data/vfs_storage/types.py
+++ src/ai/backend/manager/data/vfs_storage/types.py
@@ -1,6 +1,9 @@
 import uuid
 from dataclasses import dataclass
 from pathlib import Path
+from typing import Self
+
+from ai.backend.common.data.storage.types import VFSStorageStatefulData
 
 
 @dataclass
@@ -9,3 +12,20 @@ class VFSStorageData:
     name: str
     host: str
     base_path: Path
+
+    @classmethod
+    def from_stateful_data(cls, data: VFSStorageStatefulData) -> Self:
+        return cls(
+            id=data.id,
+            name=data.name,
+            host=data.host,
+            base_path=data.base_path,
+        )
+
+    def to_stateful_data(self) -> VFSStorageStatefulData:
+        return VFSStorageStatefulData(
+            id=self.id,
+            name=self.name,
+            host=self.host,
+            base_path=self.base_path,
+        )
