2ae8fcb3e227c2c4ebf799212b4e8bb75f4da419
jopemachine <gbl@lablup.com>
2025-11-27 04:21:00 +0000

    fix: Remove unused types

--- src/ai/backend/common/data/artifact_registry/types.py
+++ src/ai/backend/common/data/artifact_registry/types.py
@@ -6,24 +6,6 @@ from typing import Optional
 
 
 @dataclass
-class HuggingFaceRegistryData:
-    id: uuid.UUID
-    name: str
-    url: str
-    token: Optional[str]
-
-
-@dataclass
-class ReservoirRegistryData:
-    id: uuid.UUID
-    name: str
-    endpoint: str
-    access_key: str
-    secret_key: str
-    api_version: str
-
-
-@dataclass
 class HuggingFaceRegistrySharedData:
     """Shared data type for HuggingFace registry used in Valkey client."""
 
