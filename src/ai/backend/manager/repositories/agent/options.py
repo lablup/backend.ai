773d28d3c1ac09c49fbb8bee76b43fdd3c2d5a49
BoGyum Kim <bkkim@lablup.com>
2025-12-02 18:06:29 +0900

    misc: Remove unused order condition

--- src/ai/backend/manager/repositories/agent/options.py
+++ src/ai/backend/manager/repositories/agent/options.py
@@ -59,13 +59,6 @@ class AgentOrders:
             return AgentRow.id.desc()
 
     @staticmethod
-    def status(ascending: bool = True) -> QueryOrder:
-        if ascending:
-            return AgentRow.status.asc()
-        else:
-            return AgentRow.status.desc()
-
-    @staticmethod
     def scaling_group(ascending: bool = True) -> QueryOrder:
         if ascending:
             return AgentRow.scaling_group.asc()
