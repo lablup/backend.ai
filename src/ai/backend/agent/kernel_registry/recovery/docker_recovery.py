1a059c0bc0d20151c746d3c0806a14eb2de1f544
Sanghun Lee <sanghun@lablup.com>
2025-12-09 20:43:33 +0900

    remove unused code

--- src/ai/backend/agent/kernel_registry/recovery/docker_recovery.py
+++ src/ai/backend/agent/kernel_registry/recovery/docker_recovery.py
@@ -3,12 +3,11 @@ from __future__ import annotations
 import logging
 from dataclasses import dataclass
 from pathlib import Path
-from typing import TYPE_CHECKING, Self
+from typing import TYPE_CHECKING
 
 from ai.backend.common.types import AgentId
 from ai.backend.logging import BraceStyleAdapter
 
-from ..pickle.creator import PickleBasedKernelRegistryCreatorArgs, PickleBasedLoaderWriterCreator
 from .base_recovery import BaseKernelRegistryRecovery
 
 if TYPE_CHECKING:
@@ -30,21 +29,4 @@ class DockerKernelRegistryRecoveryArgs:
 
 
 class DockerKernelRegistryRecovery(BaseKernelRegistryRecovery):
-    @classmethod
-    def create(cls, args: DockerKernelRegistryRecoveryArgs) -> Self:
-        pickle_loader_writer_creator = PickleBasedLoaderWriterCreator(
-            PickleBasedKernelRegistryCreatorArgs(
-                scratch_root=args.scratch_root,
-                ipc_base_path=args.ipc_base_path,
-                var_base_path=args.var_base_path,
-                agent_id=args.agent_id,
-                local_instance_id=args.local_instance_id,
-            )
-        )
-
-        return cls(
-            loader=pickle_loader_writer_creator.create_loader(),
-            writers=[
-                pickle_loader_writer_creator.create_writer(),
-            ],
-        )
+    pass
