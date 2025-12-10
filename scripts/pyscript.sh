9541470a1cb78c6484fb420acdec36a0289dba76
Joongi Kim <joongi@lablup.com>
2025-12-10 16:41:05 +0900

    chore: Update Python versions in pyscript.sh, python.sh and build-macro.py for SCIE builds

--- scripts/pyscript.sh
+++ scripts/pyscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-PYVER="3.13.7"
+PYVER="3.14.2"
 if ! command -v uv >/dev/null 2>&1; then
   curl -LsSf https://astral.sh/uv/install.sh | sh
   if [ $SHELL = "/bin/fish" ]; then
