6ad1932df5eb98d7f07eb5c816a9307d04686980
TaekYoungKwon <tykwon@lablup.com>
2026-01-08 15:59:19 +0900

    refactor: Add Auth adaptor for transforming result data to DTO

--- src/ai/backend/common/dto/manager/auth/__init__.py
+++ src/ai/backend/common/dto/manager/auth/__init__.py
@@ -1,68 +1,8 @@
 """
 Common DTOs for auth system used by both Client SDK and Manager.
+
+Import directly from submodules:
+- types: AuthTokenType, AuthResponseType, TwoFactorType, AuthResponse, etc.
+- request: AuthorizeRequest, SignupRequest, SignoutRequest, etc.
+- response: AuthorizeResponse, SignupResponse, SignoutResponse, etc.
 """
-
-from __future__ import annotations
-
-from .request import (
-    AuthorizeRequest,
-    GetRoleRequest,
-    SignoutRequest,
-    SignupRequest,
-    UpdateFullNameRequest,
-    UpdatePasswordNoAuthRequest,
-    UpdatePasswordRequest,
-    UploadSSHKeypairRequest,
-)
-from .response import (
-    AuthorizeResponse,
-    GetRoleResponse,
-    GetSSHKeypairResponse,
-    SignoutResponse,
-    SignupResponse,
-    SSHKeypairResponse,
-    UpdateFullNameResponse,
-    UpdatePasswordNoAuthResponse,
-    UpdatePasswordResponse,
-)
-from .types import (
-    AuthResponse,
-    AuthResponseType,
-    AuthSuccessResponse,
-    AuthTokenType,
-    RequireTwoFactorAuthResponse,
-    RequireTwoFactorRegistrationResponse,
-    TwoFactorType,
-    parse_auth_response,
-)
-
-__all__ = (
-    # Types
-    "AuthTokenType",
-    "AuthResponseType",
-    "TwoFactorType",
-    "AuthResponse",
-    "AuthSuccessResponse",
-    "RequireTwoFactorRegistrationResponse",
-    "RequireTwoFactorAuthResponse",
-    "parse_auth_response",
-    # Request DTOs
-    "AuthorizeRequest",
-    "GetRoleRequest",
-    "SignupRequest",
-    "SignoutRequest",
-    "UpdateFullNameRequest",
-    "UpdatePasswordRequest",
-    "UpdatePasswordNoAuthRequest",
-    "UploadSSHKeypairRequest",
-    # Response DTOs
-    "AuthorizeResponse",
-    "GetRoleResponse",
-    "SignupResponse",
-    "SignoutResponse",
-    "UpdateFullNameResponse",
-    "UpdatePasswordResponse",
-    "UpdatePasswordNoAuthResponse",
-    "GetSSHKeypairResponse",
-    "SSHKeypairResponse",
-)
