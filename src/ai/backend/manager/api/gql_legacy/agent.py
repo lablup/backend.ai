36e9b63ec2c3ebdb0a38c6bd98e8f0d38514cd48
Bokyum Kim | 김보겸 <bkkim@lablup.com>
2026-01-09 18:40:07 +0900

    fix(BA-3807): Restore id parameter name from `agent_id` to `id` in `ModifyAgent` mutation (#7891)

--- src/ai/backend/manager/api/gql_legacy/agent.py
+++ src/ai/backend/manager/api/gql_legacy/agent.py
@@ -873,13 +873,13 @@ class ModifyAgent(graphene.Mutation):
     @classmethod
     @privileged_mutation(
         UserRole.SUPERADMIN,
-        lambda agent_id, **kwargs: (None, agent_id),
+        lambda id, **kwargs: (None, id),  # noqa: A006
     )
     async def mutate(
         cls,
         root,
         info: graphene.ResolveInfo,
-        agent_id: str,
+        id: str,
         props: ModifyAgentInput,
     ) -> ModifyAgent:
         graph_ctx: GraphQueryContext = info.context
@@ -888,9 +888,9 @@ class ModifyAgent(graphene.Mutation):
         set_if_set(props, data, "scaling_group")
         # TODO: Need to skip the following RPC call if the agent is not alive, or timeout.
         if (scaling_group := data.get("scaling_group")) is not None:
-            await graph_ctx.registry.update_scaling_group(agent_id, scaling_group)
+            await graph_ctx.registry.update_scaling_group(id, scaling_group)
 
-        update_query = sa.update(agents).values(data).where(agents.c.id == agent_id)
+        update_query = sa.update(agents).values(data).where(agents.c.id == id)
         return await simple_db_mutate(cls, graph_ctx, update_query)
 
 
