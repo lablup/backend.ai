915b8aece9c0761cf414a886acf88ab7a813c484
jopemachine <gbl@lablup.com>
2026-01-16 07:11:48 +0000

    WIP

---
+++ src/ai/backend/manager/api/gql/common/__init__.py
@@ -0,0 +1,41 @@
+"""Common GraphQL types shared across multiple domains."""
+
+from __future__ import annotations
+
+from .types import (
+    DotfileInfoGQL,
+    MetricStatGQL,
+    MetricValueGQL,
+    MountPermissionGQL,
+    ResourceOptsEntryGQL,
+    ResourceOptsGQL,
+    SchedulerInfoGQL,
+    SchedulerPredicateGQL,
+    ServicePortEntryGQL,
+    ServicePortProtocolGQL,
+    ServicePortsGQL,
+    SessionResultGQL,
+    SessionTypesGQL,
+    SSHKeypairGQL,
+    VFolderMountGQL,
+    VFolderUsageModeGQL,
+)
+
+__all__ = [
+    "DotfileInfoGQL",
+    "MetricStatGQL",
+    "MetricValueGQL",
+    "MountPermissionGQL",
+    "ResourceOptsEntryGQL",
+    "ResourceOptsGQL",
+    "SchedulerInfoGQL",
+    "SchedulerPredicateGQL",
+    "ServicePortEntryGQL",
+    "ServicePortProtocolGQL",
+    "ServicePortsGQL",
+    "SessionResultGQL",
+    "SessionTypesGQL",
+    "SSHKeypairGQL",
+    "VFolderMountGQL",
+    "VFolderUsageModeGQL",
+]
