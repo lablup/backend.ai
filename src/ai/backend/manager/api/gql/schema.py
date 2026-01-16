3326088c4757d72559405ccb41409cb181524631
jopemachine <gbl@lablup.com>
2026-01-16 07:41:10 +0000

    WIP

--- src/ai/backend/manager/api/gql/schema.py
+++ src/ai/backend/manager/api/gql/schema.py
@@ -84,7 +84,7 @@ from .huggingface_registry import (
     huggingface_registry,
     update_huggingface_registry,
 )
-from .kernel.resolver import kernels_v2
+from .kernel.resolver import kernel_v2, kernels_v2
 from .notification import (
     create_notification_channel,
     create_notification_rule,
@@ -187,6 +187,7 @@ class Query:
     default_artifact_registry = default_artifact_registry
     inference_runtime_configs = inference_runtime_configs
     inference_runtime_config = inference_runtime_config
+    kernel_v2 = kernel_v2
     kernels_v2 = kernels_v2
     route = route
     routes = routes
