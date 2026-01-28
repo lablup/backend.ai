8fa6197948e00fb49a3f4cfd7a4e4e2aadc29e4e
octodog <mu001@lablup.com>
2026-01-28 14:43:07 +0900

    fix(BA-3522): Prevent race condition in kernel metrics cleanup (#8250) (#8398)
    Co-authored-by: Sanghun Lee <sanghun@lablup.com>

--- src/ai/backend/agent/stats.py
+++ src/ai/backend/agent/stats.py
@@ -414,11 +414,14 @@ class StatContext:
         known_metrics = self.kernel_metrics.get(kernel_id)
         log.debug("Known metrics for kernel {}: {}", kernel_id, known_metrics)
         if known_metrics is None:
+            log.warning("No known metrics for kernel {}", kernel_id)
             return
         metric_keys = list(known_metrics.keys())
         agent_id = self.agent.id
         session_id, owner_user_id, project_id = self._get_ownership_info_from_kernel(kernel_id)
+        # TODO: Remove passing kernel_metrics dict to UtilizationMetricObserver
         await self._utilization_metric_observer.lazy_remove_container_metric(
+            self.kernel_metrics,
             agent_id=agent_id,
             kernel_id=kernel_id,
             session_id=session_id,
@@ -608,10 +611,6 @@ class StatContext:
                 else:
                     kernel_id_map[ContainerId(cid)] = kid
                     kernel_obj_map[kid] = info
-            unused_kernel_ids = set(self.kernel_metrics.keys()) - set(kernel_id_map.values())
-            for unused_kernel_id in unused_kernel_ids:
-                log.debug("removing kernel_metric for {}", unused_kernel_id)
-                self.kernel_metrics.pop(unused_kernel_id, None)
 
             # Here we use asyncio.gather() instead of aiotools.TaskGroup
             # to keep methods of other plugins running when a plugin raises an error
