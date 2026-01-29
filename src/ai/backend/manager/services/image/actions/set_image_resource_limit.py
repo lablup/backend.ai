d8689253254f5926dcc1a1d52817d969aeddb900
Gyubong <gbl@lablup.com>
2026-01-29 15:02:48 +0900

    fix: Use ImageID type

--- src/ai/backend/manager/services/image/actions/set_image_resource_limit.py
+++ src/ai/backend/manager/services/image/actions/set_image_resource_limit.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional, override
-from uuid import UUID
 
+from ai.backend.common.types import ImageID
 from ai.backend.manager.actions.action import BaseActionResult
 from ai.backend.manager.data.image.types import ImageData, ResourceLimitInput
 from ai.backend.manager.services.image.actions.base import ImageAction
@@ -9,7 +9,7 @@ from ai.backend.manager.services.image.actions.base import ImageAction
 
 @dataclass
 class SetImageResourceLimitByIdAction(ImageAction):
-    image_id: UUID
+    image_id: ImageID
     resource_limit: ResourceLimitInput
 
     @override
