d22e77ec901b22e80f7a8a90daaa837f237b667c
jopemachine <gbl@lablup.com>
2026-02-04 04:43:07 +0000

    fix: Resolve conflict

--- src/ai/backend/manager/models/alembic/versions/35dfab3b0662_add_artifact_storages_common_table.py
+++ src/ai/backend/manager/models/alembic/versions/35dfab3b0662_add_artifact_storages_common_table.py
@@ -1,7 +1,7 @@
 """Add artifact_storages common table
 
 Revision ID: 35dfab3b0662
-Revises: d0a3c0716970
+Revises: bc4e0e948300
 Create Date: 2025-12-02 09:24:21.050932
 
 """
@@ -13,7 +13,7 @@ from ai.backend.manager.models.base import GUID
 
 # revision identifiers, used by Alembic.
 revision = "35dfab3b0662"
-down_revision = "d0a3c0716970"
+down_revision = "bc4e0e948300"
 branch_labels = None
 depends_on = None
 
