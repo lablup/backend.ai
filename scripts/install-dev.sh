a1cb5e3952847328f6cfc857fac21f617fcd83d2
Yubin Kim <yubin@lablup.com>
2026-03-24 16:09:13 +0900

    fix: update editable webui static_path from build/rollup to build/web

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -1144,7 +1144,7 @@ configure_backendai() {
   # install and configure webui
   if [ $EDITABLE_WEBUI -eq 1 ]; then
     install_editable_webui
-    sed_inplace "s@\(#\)\{0,1\}static_path = .*@static_path = "'"src/ai/backend/webui/build/rollup"'"@" ./webserver.conf
+    sed_inplace "s@\(#\)\{0,1\}static_path = .*@static_path = "'"src/ai/backend/webui/build/web"'"@" ./webserver.conf
   else
     webui_version=$(jq -r '.package + " (built at " + .buildDate + ", rev " + .revision + ")"' src/ai/backend/web/static/version.json)
     show_note "The currently embedded webui version: $webui_version"
