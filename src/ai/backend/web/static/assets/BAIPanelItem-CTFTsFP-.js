b903037d6164cd4abb7aa165abcdcb0bee28b035
Sanghun Lee <oper6909@gmail.com>
2026-08-10 16:48:46 +0900

    chore: update webui to 26.8.1

---
+++ src/ai/backend/web/static/assets/BAIPanelItem-CTFTsFP-.js
@@ -0,0 +1,6 @@
+import{t as g,ah as h,j as s,B as c,bd as p,T as o,bi as x,cN as f,aF as j}from"./index-CrFvxZIN.js";const S=j(({css:e})=>({progressSteps:e`
+    .ant-progress-steps-item {
+      border-radius: 100px;
+    }
+  `})),b=({title:e,value:r,unit:a,percent:i,color:n,progressProps:m,...l})=>{const{token:t}=g.useToken(),{styles:y}=S(),d=h();return s.jsxs(c,{...l,direction:"column",style:{minWidth:80,...l.style},justify:"between",align:"start",wrap:"wrap",children:[p(e)?s.jsx(o.Text,{style:{fontSize:t.fontSizeHeading5,wordBreak:"keep-all",textAlign:"left"},children:e}):e,s.jsxs(c,{children:[p(r)||x(r)?s.jsx(o.Text,{style:{fontSize:t.fontSizeHeading1,color:n??d.primary5},children:r}):r,a&&s.jsx(o.Text,{children:a})]}),x(i)&&s.jsx(f,{percent:i,strokeColor:n??t.colorPrimary,showInfo:!1,steps:12,size:[5,12],className:y.progressSteps,...m})]})};export{b as B};
+//# sourceMappingURL=BAIPanelItem-CTFTsFP-.js.map
---
+++ src/ai/backend/web/static/assets/chunk-BGahzRSW.js
@@ -0,0 +1,2 @@
+import{d0 as u,gG as d}from"./index-CrFvxZIN.js";var h=Math.ceil,i=Math.max;function g(n,e,c){e===void 0?e=1:e=i(u(e),0);var t=n==null?0:n.length;if(!t||e<1)return[];for(var r=0,l=0,a=Array(h(t/e));r<t;)a[l++]=d(n,r,r+=e);return a}export{g as c};
+//# sourceMappingURL=chunk-BGahzRSW.js.map
