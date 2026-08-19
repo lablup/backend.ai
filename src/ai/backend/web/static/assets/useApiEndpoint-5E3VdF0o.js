e24c83fc12429526f8a3b9c1de0034b67b03213b
Sanghun Lee <oper6909@gmail.com>
2026-08-19 21:00:07 +0900

    chore: update webui to 26.8.1

---
+++ src/ai/backend/web/static/assets/useApiEndpoint-5E3VdF0o.js
@@ -0,0 +1,5 @@
+import{jw as r,jx as n,r as o}from"./index-CrFvxZIN.js";/**
+ @license
+ Copyright (c) 2015-2026 Lablup Inc. All rights reserved.
+ */const i=()=>{const e=r(n);return o.useMemo(()=>{const t=localStorage.getItem("backendaiwebui.api_endpoint");return t?t.replace(/^"+|"+$/g,""):e!=null&&e.api_endpoint?e.api_endpoint:""},[e])};export{i as u};
+//# sourceMappingURL=useApiEndpoint-5E3VdF0o.js.map
