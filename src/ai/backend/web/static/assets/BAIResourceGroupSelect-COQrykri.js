c92c719796794ddf9564ca3da48e226c6acf9c3e
HyeockJinKim <hyeokjin@lablup.com>
2026-09-17 14:15:13 +0900

    chore: update webui to 26.9.0-rc.5

--- src/ai/backend/web/static/assets/BAIResourceGroupSelect-COQrykri.js
+++
@@ -1,7 +0,0 @@
-import{aA as t,r as o,j as l,H as c,al as s,ff as i}from"./index-DPebpL40.js";const n=(function(){var a=[{alias:null,args:null,concreteType:"ScalingGroup",kind:"LinkedField",name:"scaling_groups",plural:!0,selections:[{alias:null,args:null,kind:"ScalarField",name:"name",storageKey:null}],storageKey:null}];return{fragment:{argumentDefinitions:[],kind:"Fragment",metadata:null,name:"BAIResourceGroupSelectQuery",selections:a,type:"Query",abstractKey:null},kind:"Request",operation:{argumentDefinitions:[],kind:"Operation",name:"BAIResourceGroupSelectQuery",selections:a},params:{cacheID:"e0e2315cadb2e8aa35586ebe588cd9d1",id:null,metadata:{},name:"BAIResourceGroupSelectQuery",operationKind:"query",text:`query BAIResourceGroupSelectQuery {
-  scaling_groups {
-    name
-  }
-}
-`}}})();n.hash="835aef9b1b8293b5cb6fa2e775e8945c";const m=({...a})=>{const{t:r}=t(),{scaling_groups:u}=o.useLazyLoadQuery(n,{},{});return l.jsx(c,{options:s(i(u,"name"),e=>({label:e==null?void 0:e.name,value:e==null?void 0:e.name,resourceGroup:e==null?void 0:e.name})),showSearch:!0,placeholder:r("comp:BAIResourceGroupSelect.SelectResourceGroup"),...a})};export{m as B};
-//# sourceMappingURL=BAIResourceGroupSelect-COQrykri.js.map
--- src/ai/backend/web/static/assets/useApiEndpoint-CYqsKyP2.js
+++
@@ -1,5 +0,0 @@
-import{aJ as a,hS as n,l as o}from"./index-DPebpL40.js";/**
- @license
- Copyright (c) 2015-2026 Lablup Inc. All rights reserved.
- */const i=()=>{const e=a(n);return o.useMemo(()=>{const t=localStorage.getItem("backendaiwebui.api_endpoint");return t?t.replace(/^"+|"+$/g,""):e!=null&&e.api_endpoint?e.api_endpoint:""},[e])};export{i as u};
-//# sourceMappingURL=useApiEndpoint-CYqsKyP2.js.map
