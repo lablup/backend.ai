70d6d64144bc71d67e38da12e31d5ac34d27ebf8
HyeockJinKim <hyeokjin@lablup.com>
2026-09-23 11:29:24 +0900

    chore: update webui to 26.9.0-rc.7

---
+++ src/ai/backend/web/static/assets/ProgressBarMarkTooltip-BBcxVigR.js
@@ -0,0 +1,2 @@
+import{j as t,q as e}from"./index-Bg9dLa8r.js";function s({content:o,children:r}){return t.jsx(e,{content:o,placement:"above",focusTrigger:"always",children:r})}export{s as default};
+//# sourceMappingURL=ProgressBarMarkTooltip-BBcxVigR.js.map
---
+++ src/ai/backend/web/static/assets/upperCase-BLomuTlE.js
@@ -0,0 +1,2 @@
+import{gV as p}from"./index-Bg9dLa8r.js";var t=p(function(r,e,o){return r+(o?" ":"")+e.toUpperCase()});export{t as u};
+//# sourceMappingURL=upperCase-BLomuTlE.js.map
